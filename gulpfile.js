8ac414c6544e7932fce26f8611c3513c094bc8f9
Travis McChesney <travis@travismcchesney.com>
2015-11-17 14:06:02 -0700

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+ 
+gulp.task('default', function () {
+    return gulp.src(['./test/notifications/*.js'], {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it 
+        .pipe(mocha({reporter: 'spec'}));
+});
