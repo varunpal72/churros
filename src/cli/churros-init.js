428c8c724adf11d0ba8a08f49d11765c4b47a400
jjwyse <josh@cloud-elements.com>
2016-01-08 16:36:11 -0700

    prompts working properly during churros init

--- src/cli/churros-init.js
+++ src/cli/churros-init.js
@@ -2,11 +2,11 @@
 
 const prompt = require('prompt');
 const optimist = require('optimist');
+const fs = require('fs');
 
 prompt.message = "   ";
 prompt.delimiter = "";
-
-prompt.override = optimist.argv
+prompt.override = optimist.argv // allows for properties to be sent as command-line args
 
 console.log('Initializing churros...\n');
 
@@ -24,15 +24,9 @@ const schema = {
     url: {
       default: 'api.cloud-elements.com',
       description: 'url: ',
-      before: function (value) {
-        if (value.startsWith('localhost')) {
-          value = 'http://' + value;
-        }
-
-        if (!value.startsWith('http')) {
-          value = 'https://' + value;
-        }
-
+      before: (value) => {
+        if (value.startsWith('localhost')) value = 'http://' + value;
+        if (!value.startsWith('http')) value = 'https://' + value;
         return value + '/elements/api-v2';
       }
     }
@@ -40,11 +34,26 @@ const schema = {
 };
 
 prompt.start();
+prompt.get(schema, (err, result) => {
+  const propsDir = process.env.HOME + '/.churros/';
+  const output = {
+    username: result.username,
+    password: result.password,
+    url: result.url
+  }
 
-prompt.get(schema, function (err, result) {
-  console.log('  username: ' + result.username);
-  console.log('  password: ' + result.password);
-  console.log('  url: ' + result.url);
-
-  console.log('\nFinished initialization of churros');
+  if (!fs.existsSync(propsDir)) {
+    fs.mkdirSync(propsDir);
+  }
+  
+  writeProperties(propsDir, output);
 });
+
+function writeProperties(dir, output) {
+  fs.writeFile(dir + '/churros.json', JSON.stringify(output, null, 2), (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('\nFinished initialization of churros');
+  });
+}
