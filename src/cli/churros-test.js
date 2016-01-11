2fbe1dcd01bc78e21b892b9a5901998795260f32
jjwyse <josh@cloud-elements.com>
2016-01-11 16:39:47 -0700

    sexy...ish

--- src/cli/churros-test.js
+++ src/cli/churros-test.js
@@ -1,25 +1,59 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const commander = require('commander');
 const util = require('util');
 const config = require(process.env.HOME + '/.churros/churros.json');
 const shell = require('shelljs');
 
+const collect = function collect(val, list) {
+  list.push(val);
+  return list;
+}
+
+const runTests = function runTests(resource, options) {
+  const user = options.user || config.user;
+  const password = options.password || config.password;
+  const url = options.url || config.url;
+  const suite = options.suite;
+  const mochaPaths = [];
+
+  const rootTestDir = path.dirname(require.main.filename) + '/../test';
+
+  // always pass the setup file first
+  mochaPaths.push(rootTestDir + '/setup');
+
+  // validate the root resource path before continuing
+  var testPath = util.format('%s/%s', rootTestDir, resource);
+  if (!fs.existsSync(testPath)) {
+    console.log('Invalid resource: %s', resource);
+    process.exit(1);
+  }
+
+  // add the resource next, unless a specific suite was passed
+  if (!suite || suite.length < 1) {
+    mochaPaths.push(testPath);
+  } else {
+    suite.forEach((s) => {
+      var suitePath = testPath + '/' + s;
+      if (!fs.existsSync(suitePath + '.js')) {
+        console.log('Invalid suite: %s', s);
+        process.exit(1);
+      }
+      mochaPaths.push(suitePath);
+    });
+  }
+
+  const args = util.format('--user %s --password %s --url %s --timeout 20000 --reporter spec --ui bdd', user, password, url);
+  var cmd = util.format('mocha %s %s', mochaPaths.join(' '), args);
+  shell.exec(cmd);
+}
+
 commander
   .command('resource', 'The resource to test (formulas, notifications, elements/box, etc.)')
-  .action((resource, options) => {
-    const user = options.user || config.user;
-    const password = options.password || config.password;
-    const url = options.url || config.url;
-
-    const setup = path.dirname(require.main.filename) + '/../test/setup';
-    const suite = path.dirname(require.main.filename) + '/../test/' + resource;
-    const args = util.format('--user %s --password %s --url %s --timeout 20000 --reporter spec --ui bdd', user, password, url);
-    const cmd = util.format('mocha %s %s %s', setup, suite, args);
-    shell.exec(cmd);
-  })
-  .option('-s, --suite <suite>', 'The suite(s) of tests to run')
+  .action((resource, options) => runTests(resource, options))
+  .option('-s, --suite <suite>', 'The suite(s) of tests to run', collect, [])
   .option('-t, --test <test>', 'The specific test(s) to run', '')
   .option('-u, --user <user>', '', '')
   .option('-p, --password <password>', '', '')
