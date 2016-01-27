5b1369c0210d064391ba520b26686b58d1b33102
jjwyse <josh@cloud-elements.com>
2016-01-27 14:16:46 -0700

    running some tests

--- src/test/elements/abbyy/tasks.js
+++ src/test/elements/abbyy/tasks.js
@@ -5,9 +5,7 @@ const schema = require('./assets/tasks.schema');
 
 tester.for('ocr', 'tasks', () => {
   // checkout functions available under tester.test which provide a lot of pre-canned tests
-  // i.e.  tester.test.cruds(api, payload, schema)
-  //       tester.test.badGet404(api);
-  //       tester.test.badPost400(api);
+  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
 
   it('jjwyse should insert some tests here :)', () => true);
 });
