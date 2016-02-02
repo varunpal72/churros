889ded76e7c261228be4f583e023f9c07fc22bab
jjwyse <josh@cloud-elements.com>
2016-02-02 11:30:29 -0700

    working on dropbox selenium...what a b

--- src/core/oauth.js
+++ src/core/oauth.js
@@ -2,71 +2,109 @@
 
 const webdriver = require('selenium-webdriver');
 
-const manipulateDom = (element, driver, r, username, password) => {
+const manipulateDom = (element, browser, r, username, password) => {
   switch (element) {
   case 'desk':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id('user_session_email')).sendKeys(username);
-    driver.findElement(webdriver.By.id('user_session_password')).sendKeys(password);
-    driver.findElement(webdriver.By.id('user_session_submit')).click();
-    driver.findElement(webdriver.By.name('commit')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id('user_session_email')).sendKeys(username);
+    browser.findElement(webdriver.By.id('user_session_password')).sendKeys(password);
+    browser.findElement(webdriver.By.id('user_session_submit')).click();
+    browser.findElement(webdriver.By.name('commit')).click();
+    return browser.getCurrentUrl();
+  case 'dropbox':
+    browser.get(r.body.oauthUrl);
+    const findLoginEmail = () => {
+      return browser.findElements(webdriver.By.name('login_email'))
+        .then(r => {
+          r.forEach(element => {
+            if (element.getTagName() === 'input') {
+              element.sendKeys(username);
+              return true;
+            }
+          })
+        });
+    };
+    const findLoginPassword = () => {
+      return browser.findElements(webdriver.By.name('login_password'))
+        .then(r => {
+          r.forEach(element => {
+            if (element.getTagName() === 'input') {
+              r.sendKeys(password);
+              return true;
+            }
+          });
+        });
+    };
+    const findBtn = () => {
+      return browser.findElement(webdriver.By.css('.login-button'))
+        .then(r => {
+          console.log('clicking on %s', r);
+          r.click();
+          return true;
+        });
+    };
+    return browser.wait(() => {
+      return findLoginEmail()
+        .then(findLoginPassword())
+        .then(findBtn())
+        .thenCatch(() => false);
+    }, 5000);
   case 'sfdc':
   case 'sfdcservicecloud':
   case 'sfdcmarketingcloud':
   case 'sfdcdocuments':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id("username")).clear();
-    driver.findElement(webdriver.By.id("username")).sendKeys(username);
-    driver.findElement(webdriver.By.id("password")).clear();
-    driver.findElement(webdriver.By.id("password")).sendKeys(password);
-    driver.findElement(webdriver.By.id("Login")).click();
-    driver.get(driver.getCurrentUrl()); // have to actually go to it and then it redirects you to your callback
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id("username")).clear();
+    browser.findElement(webdriver.By.id("username")).sendKeys(username);
+    browser.findElement(webdriver.By.id("password")).clear();
+    browser.findElement(webdriver.By.id("password")).sendKeys(password);
+    browser.findElement(webdriver.By.id("Login")).click();
+    browser.get(browser.getCurrentUrl()); // have to actually go to it and then it redirects you to your callback
+    return browser.getCurrentUrl();
   case 'etsy':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id('username-existing')).sendKeys(username);
-    driver.findElement(webdriver.By.id('password-existing')).sendKeys(password);
-    driver.findElement(webdriver.By.id('signin_button')).click();
-    driver.findElement(webdriver.By.xpath('//*[@id="oauth-submit"]/span/input')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id('username-existing')).sendKeys(username);
+    browser.findElement(webdriver.By.id('password-existing')).sendKeys(password);
+    browser.findElement(webdriver.By.id('signin_button')).click();
+    browser.findElement(webdriver.By.xpath('//*[@id="oauth-submit"]/span/input')).click();
+    return browser.getCurrentUrl();
   case 'acton':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id('authorizeLink')).click();
-    driver.findElement(webdriver.By.id('oauth_user_name')).sendKeys(username);
-    driver.findElement(webdriver.By.id('oauth_user_password')).sendKeys(password);
-    driver.findElement(webdriver.By.id('loginBtn')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id('authorizeLink')).click();
+    browser.findElement(webdriver.By.id('oauth_user_name')).sendKeys(username);
+    browser.findElement(webdriver.By.id('oauth_user_password')).sendKeys(password);
+    browser.findElement(webdriver.By.id('loginBtn')).click();
+    return browser.getCurrentUrl();
   case 'box':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.name('login')).sendKeys(username);
-    driver.findElement(webdriver.By.name('password')).sendKeys(password);
-    driver.findElement(webdriver.By.name('login_submit')).click();
-    driver.findElement(webdriver.By.name('consent_accept')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.name('login')).sendKeys(username);
+    browser.findElement(webdriver.By.name('password')).sendKeys(password);
+    browser.findElement(webdriver.By.name('login_submit')).click();
+    browser.findElement(webdriver.By.name('consent_accept')).click();
+    return browser.getCurrentUrl();
   case 'facebooksocial':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id('email')).clear();
-    driver.findElement(webdriver.By.id('email')).sendKeys(username);
-    driver.findElement(webdriver.By.id('pass')).clear();
-    driver.findElement(webdriver.By.id('pass')).sendKeys(password);
-    driver.findElement(webdriver.By.id('loginbutton')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id('email')).clear();
+    browser.findElement(webdriver.By.id('email')).sendKeys(username);
+    browser.findElement(webdriver.By.id('pass')).clear();
+    browser.findElement(webdriver.By.id('pass')).sendKeys(password);
+    browser.findElement(webdriver.By.id('loginbutton')).click();
+    return browser.getCurrentUrl();
   case 'instagram':
-    driver.get(r.body.oauthUrl);
-    driver.findElement(webdriver.By.id('id_username')).clear();
-    driver.findElement(webdriver.By.id('id_username')).sendKeys(username);
-    driver.findElement(webdriver.By.id('id_password')).clear();
-    driver.findElement(webdriver.By.id('id_password')).sendKeys(password);
-    driver.findElement(webdriver.By.className('button-green')).click();
-    return driver.getCurrentUrl();
+    browser.get(r.body.oauthUrl);
+    browser.findElement(webdriver.By.id('id_username')).clear();
+    browser.findElement(webdriver.By.id('id_username')).sendKeys(username);
+    browser.findElement(webdriver.By.id('id_password')).clear();
+    browser.findElement(webdriver.By.id('id_password')).sendKeys(password);
+    browser.findElement(webdriver.By.className('button-green')).click();
+    return browser.getCurrentUrl();
   case 'zendesk':
-    driver.get(r.body.oauthUrl);
+    browser.get(r.body.oauthUrl);
     const iframe = webdriver.By.tagName('iframe')[0];
-    driver.switchTo().frame(iframe);
-    driver.findElement(webdriver.By.id('user_email')).sendKeys(username);
-    driver.findElement(webdriver.By.id('user_password')).sendKeys(password);
-    return driver.getCurrentUrl();
+    browser.switchTo().frame(iframe);
+    browser.findElement(webdriver.By.id('user_email')).sendKeys(username);
+    browser.findElement(webdriver.By.id('user_password')).sendKeys(password);
+    return browser.getCurrentUrl();
   default:
     console.log('No OAuth callback found for element %s.  Please implement that callback in core/oauth so %s can be provisioned', element, element);
     process.exit(1);
@@ -74,10 +112,10 @@ const manipulateDom = (element, driver, r, username, password) => {
 };
 
 module.exports = (element, r, username, password) => {
-  const driver = new webdriver.Builder()
+  const browser = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
-  const url = manipulateDom(element, driver, r, username, password);
-  driver.close();
+  const url = manipulateDom(element, browser, r, username, password);
+  browser.close();
   return url;
 };
