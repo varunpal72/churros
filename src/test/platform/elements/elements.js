318fcf82f943e7c1aad1ee0aca466efb7a91f215
Brian Rothhaar <brian@cloud-elements.com>
2016-02-03 13:45:01 -0500

    Added more element metadata tests. Removed elements module.

--- src/test/platform/elements/elements.js
+++ src/test/platform/elements/elements.js
@@ -1,19 +1,42 @@
 'use strict';
 
 const util = require('util');
-const chakram = require('chakram');
-const expect = chakram.expect;
-const tester = require('core/tester')();
-const elements = require('core/elements');
+const expect = require('chakram').expect;
+const tester = require('core/tester');
 const metadataSchema = require('./assets/element.metadata.schema.json');
 
 tester.for(null, 'elements', (api) => {
 
-  it('metadata should contain element metadata)', () => {
-    return elements.get('sfdc')
-      .then(r => {
-      const url = util.format('%s/%s/metadata', api, r.body.id);
-      return tester.get(url, metadataSchema);
+  it('should return element metadata', () => {
+    return tester.get('elements/sfdc')
+    .then(r => {
+      return tester.get(util.format('%s/%s/metadata', api, r.body.id), metadataSchema);
+    });
+  });
+
+  it('should return polling event metadata for polling element', () => {
+    return tester.get('elements/netsuitecrm')
+    .then(r => {
+      return tester.get(util.format('%s/%s/metadata', api, r.body.id), (r) => {
+        expect(r).to.have.statusCode(200);
+        expect(r.body).to.not.be.empty;
+        expect(r.body.events.supported).to.equal(true);
+        expect(r.body.events.methods).to.contain('polling');
+        expect(r.body.events.polling).to.not.be.empty;
+      });
+    });
+  });
+
+  it('should return webhook event metadata for webhook element', () => {
+    return tester.get('elements/dropbox')
+    .then(r => {
+      return tester.get(util.format('%s/%s/metadata', api, r.body.id), (r) => {
+        expect(r).to.have.statusCode(200);
+        expect(r.body).to.not.be.empty;
+        expect(r.body.events.supported).to.equal(true);
+        expect(r.body.events.methods).to.contain('webhook');
+        expect(r.body.events.webhook).to.not.be.empty;
+      });
     });
   });
 });
