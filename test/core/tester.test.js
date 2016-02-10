df4a2efd36c4d0880294bc8729ac12048e091e75
jjwyse <josh@cloud-elements.com>
2016-02-10 09:19:09 -0700

    100% :)

--- test/core/tester.test.js
+++ test/core/tester.test.js
@@ -58,7 +58,11 @@ beforeEach(() => {
       return { message: 'Invalid JSON body' };
     })
     .post('/foo/file')
-    .reply(200, (uri, requestBody) => genPayload({ id: 123 }));
+    .reply(200, (uri, requestBody) => genPayload({ id: 123 }))
+    .post('/foo/bad/file')
+    .reply(404, (uri, requestBody) => {
+      return { message: 'No resource found at /foo/bad/file' };
+    });
 
   nock(baseUrl, eventHeaders())
     .post('/events/myelement')
@@ -71,6 +75,10 @@ beforeEach(() => {
     .get('/foo/456')
     .reply(404, () => {
       return { message: 'No foo found with the given ID' };
+    })
+    .get('/foo/bad')
+    .reply(404, () => {
+      return { message: 'No resource found with name /foo/bad' };
     })
     .get('/foo')
     .reply(200, (uri, requestBody) => [genPayload({ id: 123 }), genPayload({ id: 456 })])
@@ -114,22 +122,64 @@ describe('tester', () => {
   it('should support post', () => tester.post('/foo', genPayload(), genSchema()));
   it('should support post with custom validation', () => tester.post('/foo/bad', {}, (r) => expect(r).to.have.statusCode(400)));
   it('should support post with no schema or custom validation', () => tester.post('/foo', genPayload()));
+  it('should throw an error if post validation fails', () => {
+    return tester.post('/foo/bad', {}, (r) => expect(r).to.have.statusCode(200))
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support get', () => tester.get('/foo/123', genSchema()));
   it('should support get with custom validation', () => tester.get('/foo/456', (r) => expect(r).to.have.statusCode(404)));
   it('should support get with no schema or custom validation', () => tester.get('/foo/123'));
+  it('should throw an error if get validation fails', () => {
+    return tester.get('/foo/456', (r) => expect(r).to.have.statusCode(200))
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support put', () => tester.put('/foo/123', genPayload(), genSchema()));
   it('should support put with custom validation', () => tester.put('/foo/456', genPayload(), (r) => expect(r).to.have.statusCode(404)));
   it('should support put with no schema or custom validation', () => tester.put('/foo/123', genPayload()));
+  it('should throw an error if put validation fails', () => {
+    return tester.put('/foo/456', genPayload(), (r) => expect(r).to.have.statusCode(200))
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support patch', () => tester.patch('/foo/123', genPayload({ id: 123 }), genSchema()));
   it('should support patch with custom validation', () => tester.patch('/foo/456', genPayload(), (r) => expect(r).to.have.statusCode(404)));
   it('should support patch with no schema or custom validation', () => tester.put('/foo/123', genPayload()));
+  it('should throw an error if patch validation fails', () => {
+    return tester.patch('/foo/456', genPayload(), (r) => expect(r).to.have.statusCode(200))
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support delete', () => tester.delete('/foo/123'));
+  it('should throw an error if delete validation fails', () => {
+    return tester.delete('/foo/456')
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support find', () => tester.find('/foo', genSchema()));
+  it('should throw an error if find validation fails', () => {
+    return tester.find('/foo/bad', (r) => expect(r).to.have.statusCode(200))
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => true);
+  });
 
   it('should support crd', () => tester.crd('/foo', genPayload(), genSchema()));
   it('should support crds', () => tester.crds('/foo', genPayload(), genSchema()));
@@ -152,10 +202,21 @@ describe('tester', () => {
     // should really NOT depend on the file system here :/
     const filePath = '.tmp';
     fs.closeSync(fs.openSync(filePath, 'w'));
-    tester.postFile('/foo/file', filePath, null, genSchema());
-    fs.unlink(filePath);
+    return tester.postFile('/foo/file', filePath, null, genSchema())
+      .then(r => fs.unlink(filePath));
+  });
 
-    return true;
+  it('should throw an error if post file validation fails', () => {
+    const filePath = '.tmp';
+    fs.closeSync(fs.openSync(filePath, 'w'));
+    return tester.postFile('/foo/bad/file', filePath, null, genSchema())
+      .then(r => {
+        throw Error('Where my error at?');
+      })
+      .catch(r => {
+        fs.unlink(filePath);
+        return true;
+      });
   });
 
   tester.test.badPost400('/foo/bad', genPayload());
