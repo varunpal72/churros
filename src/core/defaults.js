574d200d97633f51ce3365cc0596fb0c91411eac
jjwyse <josh@cloud-elements.com>
2016-02-11 18:00:18 -0700

    adding user-agent to all requests

--- src/core/defaults.js
+++ src/core/defaults.js
@@ -7,34 +7,47 @@ const logger = require('winston');
 let baseUrl = null;
 let userSecret = null;
 let orgSecret = null;
+let username = null;
 
-const setDefaults = (b, u, o) => {
+const setDefaults = (b, u, o, un) => {
   logger.debug('Modifying the chakram default request headers and base url');
   chakram.setRequestDefaults({
     baseUrl: b,
-    headers: { Authorization: util.format('User %s, Organization %s', userSecret, orgSecret) }
+    headers: {
+      Authorization: util.format('User %s, Organization %s', userSecret, orgSecret),
+      'User-Agent': util.format('Churrosbot/%s', un)
+    }
   });
 };
 
-var exports = module.exports = (b, u, o) => {
+const validate = (b, u, o, un) => {
+  if (!b) throw Error('The base URL was never set. Cannot reset until properly initialized');
+  if (!u) throw Error('The user secret was never set. Cannot reset until properly initialized');
+  if (!o) throw Error('The org secret was never set. Cannot reset until properly initialized');
+  if (!un) throw Error('The username was never set. Cannot reset until properly initialized');
+};
+
+var exports = module.exports = (b, u, o, un) => {
+  validate(b, u, o, un);
   baseUrl = b;
   userSecret = u;
   orgSecret = o;
-  setDefaults(b, u, o);
+  username = un;
+  setDefaults(b, u, o, un);
 };
 
 exports.reset = () => {
-  if (!baseUrl) throw Error('The base URL was never set. Cannot reset until properly initialized');
-  if (!userSecret) throw Error('The user secret was never set. Cannot reset until properly initialized');
-  if (!orgSecret) throw Error('The org secret was never set. Cannot reset until properly initialized');
-
-  setDefaults(baseUrl, userSecret, orgSecret);
+  validate(baseUrl, userSecret, orgSecret, username);
+  setDefaults(baseUrl, userSecret, orgSecret, username);
 };
 
 exports.token = (token) => {
   logger.debug('Adding token to our default request headers');
   chakram.setRequestDefaults({
     baseUrl: baseUrl,
-    headers: { Authorization: util.format('User %s, Organization %s, Element %s', userSecret, orgSecret, token) }
+    headers: {
+      Authorization: util.format('User %s, Organization %s, Element %s', userSecret, orgSecret, token),
+      'User-Agent': util.format('Churrosbot/%s', username)
+    }
   });
 };
