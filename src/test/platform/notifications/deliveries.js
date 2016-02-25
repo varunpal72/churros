a320dc41a86bbb3639917bec577df221c0adcf2c
Brian Rothhaar <brian@cloud-elements.com>
2016-02-25 14:04:49 -0700

    Refactored notification subscription delivery tests

--- src/test/platform/notifications/deliveries.js
+++ src/test/platform/notifications/deliveries.js
@@ -24,7 +24,7 @@ const genSub = (opts) => new Object({
 });
 
 suite.forPlatform('notifications/subscriptions/deliveries', genSub({}), schema, (test) => {
-  it('should return notification/subscription delivery status', () => {
+  it('should return notification subscription delivery status', () => {
     const topic = 'churros-topic-' + tools.random();
     const n = genNotif({ topic: topic });
     const s = genSub({ topics: [ topic ] });
@@ -34,37 +34,42 @@ suite.forPlatform('notifications/subscriptions/deliveries', genSub({}), schema, 
       promises.push(cloud.post("notifications/subscriptions", s));
     }
     let subs = [];
+    let nId;
+    let sId;
 
     return chakram.all(promises)
       .then(r => {
         r.forEach(sr => subs.push(sr));
         return cloud.post("notifications", n)
-          .then(r => {
-            let nId = r.body.id;
-            let sId = subs[ 0 ].body.id;
-
-            // test get by notification id and subscription id
-            return cloud.withOptions({ qs: { hydrate: true } }).get(util.format('notifications/%s/subscriptions/%s/deliveries', nId, sId),
-              (r) => {
-                expect(r).to.have.schemaAnd200(schema);
-                expect(r.body).to.not.be.empty;
-                expect(r.body.notificationId).to.equal(nId);
-                expect(r.body.subscriptionId).to.equal(sId);
-                expect(r.body.notification).to.not.be.empty;
-                expect(r.body.subscription).to.not.be.empty;
-              })
-              // test get all by notification id
-              .then(r => cloud.get(util.format('notifications/%s/subscriptions/deliveries', nId), (r) => {
-                expect(r).to.have.schemaAnd200(listSchema);
-                expect(r.body).to.not.be.empty;
-                expect(r.body.length).to.equal(load);
-              }))
-              // test search
-              .then(r => cloud.withOptions({ qs: { hydrate: true, where: "channel='webhook'" } }).get('notifications/subscriptions/deliveries', (r) => {
-                expect(r).to.have.schemaAnd200(listSchema);
-                r.body.forEach(s => expect(s.subscription.channel).to.equal('webhook'));
-              }));
-          });
+      })
+      .then(r => {
+        nId = r.body.id;
+        sId = subs[ 0 ].body.id;
+        // test get by notification id and subscription id
+        return cloud.withOptions({ qs: { hydrate: true } }).get(util.format('notifications/%s/subscriptions/%s/deliveries', nId, sId),
+          (r) => {
+            expect(r).to.have.schemaAnd200(schema);
+            expect(r.body).to.not.be.empty;
+            expect(r.body.notificationId).to.equal(nId);
+            expect(r.body.subscriptionId).to.equal(sId);
+            expect(r.body.notification).to.not.be.empty;
+            expect(r.body.subscription).to.not.be.empty;
+          })
+      })
+      .then(r => {
+        // test get all by notification id
+        return cloud.get(util.format('notifications/%s/subscriptions/deliveries', nId), (r) => {
+          expect(r).to.have.schemaAnd200(listSchema);
+          expect(r.body).to.not.be.empty;
+          expect(r.body.length).to.equal(load);
+        })
+      })
+      // test search
+      .then(r => {
+        return cloud.withOptions({ qs: { hydrate: true, where: "channel='webhook'" } }).get('notifications/subscriptions/deliveries', (r) => {
+          expect(r).to.have.schemaAnd200(listSchema);
+          r.body.forEach(s => expect(s.subscription.channel).to.equal('webhook'));
+        })
       })
       .then(r => subs.forEach(sub => cloud.delete(util.format('/notifications/subscriptions/%s', sub.body.id))));
   });
