edf2b7416314738a1e549ba3da70a923220e5645
CENate <nate@cloud-elements.com>
2016-02-29 17:00:38 -0700

    added tests for attachments along with the files. Had to add several objects to the object.definition file.

--- src/test/elements/sfdc/attachments.js
+++ src/test/elements/sfdc/attachments.js
@@ -1,11 +1,37 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
+const tools = require('core/tools');
 const payload = require('./assets/attachments');
+const fs = require('fs');
+let path = __dirname + '/assets/attach';
+
+const contact = () => ({
+  FirstName: 'Conan',
+  LastName: 'Barbarian',
+  Email: tools.randomEmail()
+});
+
+const attachPath = ('./assets/attach.txt');
+const updatePath = ('./assets/update.txt');
 
 suite.forElement('crm', 'attachments', payload, (test) => {
-  // checkout functions available under test.should which provide a lot of pre-canned tests
-  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
-
-  it('natedoyle should insert some tests here :)', () => true);
+  it('should allow CRUDS for ' + test.api, () => {
+    let contactId;
+    let attachmentId;
+    return cloud.post('/hubs/crm/contacts', contact())
+      .then(r => contactId = r.body.id)
+      .then(r => {
+      	fs.closeSync(fs.openSync(attachPath, 'w'));
+      	return cloud.postFile('/hubs/crm/Contact/' + contactId + '/attachments', path)
+      })
+      .then(r => fs.unlink(attachPath))
+      .then(r => attachmentId = r.body.id)
+      .then(r => cloud.get(test.api + '/' + attachmentId))
+      .then(r => cloud.get(test.api + '/' + attachmentId + '/' + 'data'))
+      .then(r => cloud.update(test.api + '/' + r.body.id, update_attachment))
+      .then(r => cloud.delete('/hubs/crm/attachments/' + attachmentId))
+      .then(r => cloud.delete('/hubs/crm/contacts/' + contactId));
+  });
 });
