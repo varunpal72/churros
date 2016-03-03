a8574b7de71a96867df6e4fd6fa6860fa9c883b8
CENate <nate@cloud-elements.com>
2016-03-03 13:20:36 -0700

    added files for resources not configured during stubbing process.

---
+++ src/test/elements/sugar/tasks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/tasks');
+
+suite.forElement('crm', 'tasks', payload, (test) => {
+  // checkout functions available under test.should which provide a lot of pre-canned tests
+  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
+
+  it('natedoyle should insert some tests here :)', () => true);
+});
---
+++ src/test/elements/sugar/users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/users');
+
+suite.forElement('crm', 'users', payload, (test) => {
+  // checkout functions available under test.should which provide a lot of pre-canned tests
+  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
+
+  it('natedoyle should insert some tests here :)', () => true);
+});
