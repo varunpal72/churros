6e0d0361de99629d099f0c78903c9e21666742fd
Brian Rothhaar <brian@cloud-elements.com>
2016-03-04 12:06:11 -0500

    Added elements API tests for base- and all sub-resources

---
+++ src/test/platform/elements/resourceModels.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const util = require('util');
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const props = require('core/props');
+const provisioner = require('core/provisioner');
+const common = require('./assets/common.js');
+const schema = require('./assets/element.model.schema.json');
+
+
+
+suite.forPlatform('elements/resources/models', common.genParameter({}), schema, (test) => {
+  let element, resource, keyUrl, idUrl;
+  before(done => cloud.post('elements', common.genElement({}))
+    .then(r => element = r.body)
+    .then(r => cloud.post('elements/' + element.key + '/resources', common.genResource({}))
+    .then(r => resource = r.body))
+    .then(r => keyUrl = 'elements/' + element.key + '/resources/' + resource.id + '/models')
+    .then(r => idUrl = 'elements/' + element.id + '/resources/' + resource.id + '/models')
+    .then(r => done()));
+
+  it('should support CRUD by key', () => {
+      let model;
+      return cloud.post(keyUrl, common.genModel({}), schema)
+        .then(r => model = r.body)
+        .then(r => cloud.get(keyUrl, schema))
+        .then(r => cloud.put(keyUrl, common.genModel({name: 'updatedName'}), schema))
+        .then(r => cloud.delete(keyUrl));
+
+  });
+  //common.crudSubResource(keyUrl, schema, listSchema, common.genModel({}), common.genModel({ description: "An updated Churros model" })));
+  //it('should support CRUD by ID', () => common.crudSubResource(idUrl, schema, listSchema, common.genModel({}), common.genModel({ description: "An updated Churros model" })));
+
+  after(done => { cloud.delete('elements/' + element.key).then(() => done()) });
+});
---
+++ src/test/platform/elements/resources.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const util = require('util');
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const props = require('core/props');
+const provisioner = require('core/provisioner');
+const common = require('./assets/common.js');
+const schema = require('./assets/element.resource.schema.json');
+const listSchema = require('./assets/element.resources.schema.json');
+
+suite.forPlatform('elements/resources', common.genResource({}), schema, (test) => {
+  let element, keyUrl, idUrl;
+  before(done => cloud.post('elements', common.genElement({}))
+  .then(r => element = r.body)
+  .then(r => keyUrl = 'elements/' + element.key + '/resources')
+  .then(r => keyUrl = 'elements/' + element.id + '/resources')
+  .then(done()));
+
+  it('should support CRUD by key', () => common.crudSubResource(keyUrl, schema, listSchema, common.genResource({}), common.genResource({ description: "An updated Churros resource" })));
+
+  after(done => { cloud.delete('elements/' + element.key).then(() => done()) });
+
+});
