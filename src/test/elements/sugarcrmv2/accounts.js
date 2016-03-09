459d65cc6e9de5768a09afd04bc2a5b70a2951e1
CENate <nate@cloud-elements.com>
2016-03-09 11:32:56 -0700

    added tests to all resources on Sugar. Users was set to Sr instead of CRUDS as I think that is more in keeping with the use model for the users resource.

--- src/test/elements/sugarcrmv2/accounts.js
+++ src/test/elements/sugarcrmv2/accounts.js
@@ -4,5 +4,5 @@ const suite = require('core/suite');
 const payload = require('./assets/accounts');
 
 suite.forElement('crm', 'accounts', payload, (test) => {
-	test.should.supportCruds();
+  test.should.supportCruds();
 });
--- src/test/elements/sugarcrmv2/opportunities.js
+++ src/test/elements/sugarcrmv2/opportunities.js
@@ -4,8 +4,5 @@ const suite = require('core/suite');
 const payload = require('./assets/opportunities');
 
 suite.forElement('crm', 'opportunities', payload, (test) => {
-  // checkout functions available under test.should which provide a lot of pre-canned tests
-  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
-
-  it('natedoyle should insert some tests here :)', () => true);
+  test.should.supportCruds();
 });
