3e61e47a242e51c8bbd46413d92120455a243a17
CENate <nate@cloud-elements.com>
2016-03-10 13:40:01 -0700

    stubbed out tests for hubspot.

---
+++ src/test/elements/hubspotcrm/owners.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/owners');
+
+suite.forElement('crm', 'owners', payload, (test) => {
+  // checkout functions available under test.should which provide a lot of pre-canned tests
+  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
+
+  it('natedoyle should insert some tests here :)', () => true);
+});
