260a3909d9a377a6ee915df93070325c6a3c8ce8
Martin Brandt <martinfbrandt@gmail.com>
2016-03-15 15:05:50 -0600

    added tests to check if file locking/unlocking is working

--- src/test/elements/box/files.js
+++ src/test/elements/box/files.js
@@ -7,7 +7,7 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('documents', 'files', null, (test) => {
-  it('should allow uploading and downloading a file', () => {
+  it('should allow uploading, downloading, and locking a file', () => {
     let fileId = -1;
     let query = { path: util.format('/brady-%s.jpg', tools.random()) };
     let path = __dirname + '/assets/brady.jpg';
@@ -15,6 +15,8 @@ suite.forElement('documents', 'files', null, (test) => {
     return cloud.postFile(test.api, path, { qs: query })
       .then(r => fileId = r.body.id)
       .then(r => cloud.get(test.api + '/' + fileId, (r) => expect(r).to.have.statusCode(200)))
+      .then(r => cloud.patch(test.api + '/' + fileId + '/lock', (r) => expect(r).to.have.statusCode(200)))
+      .then(r => cloud.patch(test.api + '/' + fileId + '/unlock', (r) => expect(r).to.have.statusCode(200)))
       .then(r => cloud.delete('/hubs/documents/files/' + fileId));
   });
 });
