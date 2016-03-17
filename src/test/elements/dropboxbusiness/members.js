8066371dc9890fb1c6c9abfbc0914954fcc6c4d0
CENate <nate@cloud-elements.com>
2016-03-17 13:45:25 -0600

    Fixed id call in members so it returns something other than undefined.

--- src/test/elements/dropboxbusiness/members.js
+++ src/test/elements/dropboxbusiness/members.js
@@ -3,9 +3,6 @@
 const suite = require('core/suite');
 const payload = require('./assets/members');
 const cloud = require('core/cloud');
-// function getMemberId(memberId){
-// 	return memberId.split(":")[1]
-// }
 
 let memberId;
 // member is unsuspended twice to make sure it's in a state where it can be tested next time.
@@ -13,16 +10,14 @@ suite.forElement('documents', 'members', payload, (test) => {
   it('should support CRUDS for members', () =>{
     return cloud.post(test.api, payload)
     .then(r => {
-    	let memberStr = r.body.id;
-    	console.log(memberStr);
-    	console.log(r.body.id);
+    	let memberStr = r.body.complete[0].profile.team_member_id;
     	memberId = memberStr.split(":")[1];
       })
     .then(r => cloud.delete(`${test.api}/${memberId}`))
-    .then(r => cloud.get(test.api))
+    .then (r => cloud.get(test.api)
     .then(r => {
-    	let record = r[r.length-1];
-    	memberId = record.body.id.split(":")[1];
+    	let record = r.body[r.body.length-1];
+    	memberId = record.profile.team_member_id.split(":")[1];
     })
     .then(r => cloud.get(`${test.api}/${memberId}`))
     .then(r => cloud.patch(`${test.api}/${memberId}/suspend/false`))
