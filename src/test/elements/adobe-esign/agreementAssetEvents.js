d6c598f7e2ad10782a36f03c978868d7902c65b1
shraddhajain <shraddha@cloud-elements.com>
2016-03-18 17:24:05 -0500

    CE-3676 Made the start date dynamic in /agreementAssetEvents.js

--- src/test/elements/adobe-esign/agreementAssetEvents.js
+++ src/test/elements/adobe-esign/agreementAssetEvents.js
@@ -12,10 +12,14 @@ suite.forElement('esignature', 'agreementAssetEvents', null, (test) => {
   let currentDate;	
   var date = new Date();
   currentDate = date.toISOString().substring(0,19); 
+  startDate = date.setDate(date.getDate()-10);
+  var oldDate = new Date(startDate);
+  var startDate = oldDate.toISOString().substring(0,19);
+  
   it('should allow GET for ' + test.api, () => {
     return cloud.withOptions({
       qs: {
-        where: util.format("startDate='2016-01-01T00:00:00' and endDate='"+currentDate+"'")
+        where: util.format("startDate='"+startDate+"' and endDate='"+currentDate+"'")
       }
     }).get(test.api);
   });
