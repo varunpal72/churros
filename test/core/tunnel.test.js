2f90a7884b981ca5fcc71199eb9b86092aafe7ff
jjwyse <josh@cloud-elements.com>
2016-04-06 14:40:42 -0600

    not my favorite unit test...

--- test/core/tunnel.test.js
+++ test/core/tunnel.test.js
@@ -12,6 +12,7 @@ mockery.enable({ warnOnReplace: false, warnOnUnregistered: false });
 
 const tunnel = require('core/tunnel');
 const expect = require('chakram').expect;
+const fs = require('fs');
 
 describe('tunnel', () => {
   const port = 8999;
@@ -24,5 +25,37 @@ describe('tunnel', () => {
   it('should support starting up a publicly available HTTP tunnel without an auth token', () => {
     return tunnel.start(port)
       .then(url => expect(url).to.be.a('string'));
+  });
+
+  it('should preserve an ngrok2 config file if present', () => {
+    const baseDir = `${process.env.HOME}/.ngrok2`;
+    const path = `${baseDir}/ngrok.yml`;
+    if (fs.existsSync(path)) return; // only run if this machine does NOT have an ngrok config file
+
+
+    // create folder, if not exists
+    var didCreateDir = false;
+    if (!fs.existsSync(baseDir)) {
+      fs.mkdirSync(baseDir);
+      didCreateDir = true;
+    }
+
+    // create tmp file
+    const contents = 'authtoken: fake';
+    fs.writeFileSync(path, contents);
+
+    return tunnel.start(port, 'fakeauthtoken')
+      .then(url => expect(url).to.be.a('string'))
+      .then(() => {
+        // make sure it's the same as it was before running tunnel.start
+        const config = fs.readFileSync(path).toString('utf8');
+        expect(config).to.be.a('string');
+        expect(config).to.equal(contents);
+      })
+      .then(() => {
+        // cleanup
+        fs.unlink(path);
+        if (didCreateDir) fs.rmdirSync(baseDir);
+      });
   });
 });
