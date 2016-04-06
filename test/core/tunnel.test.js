456b0796876d321868c287218b2e507fffd66c47
jjwyse <josh@cloud-elements.com>
2016-04-06 14:15:32 -0600

    mocking ngrok to test out the tunnel code

--- test/core/tunnel.test.js
+++ test/core/tunnel.test.js
@@ -1,11 +1,27 @@
 'use strict';
 
+/* MOCKING OUT THE ngrok module here */
+const mockery = require('mockery');
+mockery.registerMock('ngrok', {
+  connect: (opts, cb) => {
+    cb(null, 'http://fakeurl.ngrok.io');
+  }
+});
+mockery.enable({ warnOnReplace: false, warnOnUnregistered: false });
+/****************************************/
+
 const tunnel = require('core/tunnel');
 const expect = require('chakram').expect;
 
 describe('tunnel', () => {
-  it('should support starting up a publicly available HTTP tunnel', () => {
-    const port = 8999;
+  const port = 8999;
+
+  it('should support starting up a publicly available HTTP tunnel with an auth token', () => {
+    return tunnel.start(port, 'fakeauthtoken')
+      .then(url => expect(url).to.be.a('string'));
+  });
+
+  it('should support starting up a publicly available HTTP tunnel without an auth token', () => {
     return tunnel.start(port)
       .then(url => expect(url).to.be.a('string'));
   });
