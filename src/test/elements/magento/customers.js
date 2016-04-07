a7549c45571051a7b186c2a619bce1b3bbb1686e
Brad Scharmann <bscharm@gmail.com>
2016-04-06 19:09:17 -0600

    add CEQL search tests to products and customers

--- src/test/elements/magento/customers.js
+++ src/test/elements/magento/customers.js
@@ -2,8 +2,12 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/customers');
+const tools = require('core/tools');
+payload.email = tools.randomEmail();
+
 suite.forElement('ecommerce', 'customers', payload, (test) => {
   test.should.return200OnGet();
   test.should.supportPagination();
   test.should.supportCruds();
+  test.should.supportCeqlSearch("email");
 });
--- src/test/elements/magento/products.js
+++ src/test/elements/magento/products.js
@@ -2,9 +2,12 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/products');
+const tools = require('core/tools');
+payload.sku = tools.random();
 
 suite.forElement('ecommerce', 'products', payload, (test) => {
   test.should.return200OnGet();
   test.should.supportPagination();
   test.should.supportCruds();
+  test.should.supportCeqlSearch("sku");
 });
