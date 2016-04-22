5161de8619a50a6f1840fde4f1bac233bc86bcf8
Venkat Ramana Lashkar <ramana.kathek@gmail.com>
2016-04-21 22:50:09 -0500

    CE-4294 Churros tests for MySql element

---
+++ src/test/elements/mysql/query.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const chakram = require('chakram');
+
+const groupQuery = () => {
+  return "select count(*), state from contact group by state order by 1 desc limit 5";
+};
+
+const joinQuery = () => {
+  return "select c.first_name, c.last_name, cp.name from contact c inner join company cp on cp.company_id = c.company_id";
+};
+
+suite.forElement('db', 'query', {}, (test) => {
+  it('should allow POST for ' + test.api, () => {
+    return cloud.withOptions({json:false}).post('/hubs/db/query', groupQuery())
+    .then(r => cloud.withOptions({json:false}).post('/hubs/db/query', joinQuery()));
+  });
+});
