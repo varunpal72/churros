7cdc047aaef5fafedacecfcf9c190784d37bc197
Venkat Ramana Lashkar <ramana.kathek@gmail.com>
2016-05-06 14:23:00 -0500

    fix circleci error

--- src/test/elements/sqlserver/query.js
+++ src/test/elements/sqlserver/query.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 const groupQuery = () => ({
   script: "select top 5 count(*), Email from Contact group by Email order by 1 desc"
