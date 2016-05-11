35278f83412eb84c014a8e79c311af971a1d9cdf
jainshraddha20@gmail.com <shraddha123>
2016-05-11 12:18:00 -0500

    Chargify Churros Tests

---
+++ src/test/elements/chargify/transactions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('payment', 'transactions', (test) => {
+  test.should.return200OnGet();
+  it(`should allow GET for ${test.api}/{transactionId}`, () => {
+    let subscriptionId = 12697293;
+    let transactionId;
+    return cloud.get(`${test.api}`)
+      .then(r => transactionId = r.body[0].transaction.id)
+      .then(r => cloud.get(`${test.api}/${transactionId}`))
+      .then(r => cloud.get(`/hubs/payment/subscriptions/${subscriptionId}/transactions`));
+  });
+  test.should.supportPagination();
+});
