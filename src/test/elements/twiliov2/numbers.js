84fec737aec33bd632c7611aef5717f5fd4a1ede
Dan Harris <danharris@elements-floater.local>
2016-05-11 14:51:15 -0600

    add status code check and change vars from lets

--- src/test/elements/twiliov2/numbers.js
+++ src/test/elements/twiliov2/numbers.js
@@ -9,29 +9,30 @@ var chakram = require('chakram'),
 suite.forElement('messaging', 'numbers', {payload: payload}, (test) => {
   it('should allow Read', () => {
     let query = { countryCode: 'US', type: 'Local' };
-    return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers');
+    return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers')
+    .then(r => expect(r).to.have.statusCode(200));
   });
 
   it('should allow query by countryCode', () => {
-    var query = { countryCode: 'US', type: 'Local'};
+    let query = { countryCode: 'US', type: 'Local'};
     return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers')
     .then(r => expect(r.body[0].iso_country).to.eq('US'));
   });
 
   it('should allow query by MmsEnabled', () => {
-    var query = { countryCode: 'US', type: 'Local', where: "MmsEnabled='true'"};
+    let query = { countryCode: 'US', type: 'Local', where: "MmsEnabled='true'"};
     return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers')
     .then(r => expect(r.body[0].capabilities.MMS).to.eq(true));
   });
 
   it('should allow query by SmsEnabled', () => {
-    var query = { countryCode: 'US', type: 'Local', where: "SmsEnabled='true'"};
+    let query = { countryCode: 'US', type: 'Local', where: "SmsEnabled='true'"};
     return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers')
     .then(r => expect(r.body[0].capabilities.SMS).to.eq(true));
   });
 
   it('should allow Pagination', () => {
-    var query = { countryCode: 'US', type: 'Local', page: 1, pageSize: 2 };
+    let query = { countryCode: 'US', type: 'Local', page: 1, pageSize: 2 };
     return cloud.withOptions({ qs: query }).get('/hubs/messaging/numbers')
     .then(r => expect(r.body.length).to.eq(2));
   });
