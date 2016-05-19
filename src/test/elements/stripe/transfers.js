8bd065d7b3a000d6ac5951bc95f7616e4e4349c0
jainshraddha20@gmail.com <shraddha123>
2016-05-19 15:46:00 -0500

    Stripe enhancements for Reveel Churros Tests

---
+++ src/test/elements/stripe/transfers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+
+const updateTransfers = () => ({
+  "description": tools.random()
+});
+
+suite.forElement('payment', 'transfers', (test) => {
+  test.should.supportSr();
+  it(`should allow Patch for ${test.api}`, () => {
+    let transferId;
+    return cloud.get(`${test.api}`)
+      .then(r => transferId = r.body[0].id)
+      .then(r => cloud.patch(`${test.api}/${transferId}`,updateTransfers()));
+  });
+});
