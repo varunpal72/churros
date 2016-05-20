355c36fdc81462ead49bfa575d7bb18eb907dd95
Tyler Toth <tyler@cloud-elements.com>
2016-05-19 22:37:18 -0500

    Tests for the Magento Soap Element. Changed key to magentosoapv19

--- src/test/elements/magentosoapv19/invoices.js
+++ src/test/elements/magentosoapv19/invoices.js
@@ -6,9 +6,6 @@ const payload = require('./assets/invoices');
 suite.forElement('ecommerce', 'invoices', { payload: payload }, (test) => {
   test.should.supportSr();
   test.should.supportPagination();
-  //haven't added POST b/c it requires an order with status pending & I can't create orders
-  //invoices/{id}/capture is not working as expected...
   test.withOptions({ qs: { where: 'created_at=\'2013-05-29 08:38:43\'' } }).should.return200OnGet();
-  test.withOptions({ qs: { where: 'created_at>=\'2013-05-29 08:38:43\'', pageSize: 5, page: 1} }).should.return200OnGet();
-
+  test.withOptions({ qs: { where: 'created_at>=\'2013-05-29 08:38:43\'', pageSize: 5, page: 1 } }).should.return200OnGet();
 });
