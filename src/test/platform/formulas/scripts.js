ead6ad88e62acec4eff5434cb544184accaab4d9
jjwyse <josh@cloud-elements.com>
2016-06-07 14:10:28 -0600

    re-factoring

--- src/test/platform/formulas/scripts.js
+++ src/test/platform/formulas/scripts.js
@@ -40,11 +40,26 @@ const gen = (genStep) => (genScript) => {
   return f;
 };
 
+const versionlessValidator = (r) => {
+  expect(r).to.have.statusCode(200);
+  expect(r.body).to.not.be.null;
+  expect(r.body.properties.scriptEngine).to.be.undefined;
+  return r;
+};
+
 /**
  * Handles validating that formula script steps that are trying to be created have the right error handling capabilities
  * around them
  */
 suite.forPlatform('formulas', { name: 'formula script steps', schema: schema }, (test) => {
+  const wrap = (formula, step, validator) => {
+    let formulaId;
+    return cloud.post(test.api, formula)
+      .then(r => formulaId = r.body.id)
+      .then(r => cloud.post(`${test.api}/${formulaId}/steps`, step, validator))
+      .then(r => cloud.delete(`${test.api}/${formulaId}`));
+  };
+
   test
     .withName('should allow creating a script step with the v1 engine')
     .withJson(gen(genV1Step)(genGoodV1Script))
@@ -77,12 +92,8 @@ suite.forPlatform('formulas', { name: 'formula script steps', schema: schema }, 
       expect(res.body.message).to.contain('done()'); // should warn about NOT calling the done() callback
       return res;
     };
-
-    let formulaId;
-    return cloud.post(test.api, gen()())
-      .then(r => formulaId = r.body.id)
-      .then(r => cloud.post(`${test.api}/${formulaId}/steps`, genV2Step(genBadV2Script), validator))
-      .then(r => cloud.delete(`${test.api}/${formulaId}`));
+    
+    return wrap(gen()(), genV2Step(genBadV2Script), validator);
   });
 
   it('should set the script engine to v1 when adding a step to a formula that contains all v1 steps', () => {
@@ -92,24 +103,18 @@ suite.forPlatform('formulas', { name: 'formula script steps', schema: schema }, 
       expect(r.body.properties.scriptEngine).to.equal('v1');
     };
 
-    let formulaId;
-    return cloud.post(test.api, gen(genV1Step)(genGoodV1Script))
-      .then(r => formulaId = r.body.id)
-      .then(r => cloud.post(`${test.api}/${formulaId}/steps`, genVersionlessStep(genGoodV1Script), validator))
-      .then(r => cloud.delete(`${test.api}/${formulaId}`));
+    return wrap(gen(genV1Step)(genGoodV1Script), genVersionlessStep(genGoodV1Script), validator);
   });
 
   it('should not set the script engine property when adding a step to a formula that does not contain all v1 steps', () => {
-    const validator = (r) => {
-      expect(r).to.have.statusCode(200);
-      expect(r.body).to.not.be.null;
-      expect(r.body.properties.scriptEngine).to.be.undefined;
-    };
+    return wrap(gen(genVersionlessStep)(genGoodV2Script), genVersionlessStep(genGoodV2Script), versionlessValidator);
+  });
 
-    let formulaId;
-    return cloud.post(test.api, gen(genVersionlessStep)(genGoodV2Script))
-      .then(r => formulaId = r.body.id)
-      .then(r => cloud.post(`${test.api}/${formulaId}/steps`, genVersionlessStep(genGoodV2Script), validator))
-      .then(r => cloud.delete(`${test.api}/${formulaId}`));
+  it('should not set the script engine property when adding a step to a formula that contains some v1 steps and some v2 steps', () => {
+    // formula with one v2 step and one v1 step
+    const f = gen(genV2Step)(genGoodV2Script);
+    f.steps.push(genV1Step(genGoodV1Script));
+
+    return wrap(f, genVersionlessStep(genGoodV2Script), versionlessValidator);
   });
 });
