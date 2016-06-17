b8aca906ba1b73beb3f58af2c68bb2d5bfeffef9
wyselm <laura@cloud-elements.com>
2016-06-17 15:38:23 -0600

    change type of id

--- src/test/elements/hireright/background-checks.js
+++ src/test/elements/hireright/background-checks.js
@@ -9,7 +9,7 @@ suite.forElement('screening', 'background-checks', { payload: payload }, (test) 
   test.should.return200OnPost();
 
   it('should allow GET for /hubs/screening/background-checks/{id}/reports and /hubs/screening/background-checks/{id}/statuses', () => {
-    let backgroundCheckId = payload.clientApplicantId + ":" + payload.clientRequestId;
+    const backgroundCheckId = payload.clientApplicantId + ":" + payload.clientRequestId;
     return cloud.post('/hubs/screening/background-checks', payload)
       .then(r => cloud.get(`/hubs/screening/background-checks/${backgroundCheckId}/reports`))
       .then(r => cloud.get(`/hubs/screening/background-checks/${backgroundCheckId}/statuses`));
