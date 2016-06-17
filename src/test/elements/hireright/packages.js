48fd8114e8b94f49d5a81872c2efb985d324092b
wyselm <laura@cloud-elements.com>
2016-06-17 15:40:24 -0600

    removing unneeded stuff

--- src/test/elements/hireright/packages.js
+++ src/test/elements/hireright/packages.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 suite.forElement('screening', 'packages', (test) => {
   test.should.return200OnGet();
--- src/test/elements/hireright/products.js
+++ src/test/elements/hireright/products.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 suite.forElement('screening', 'products', (test) => {
   test.should.return200OnGet();
