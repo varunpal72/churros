7c925f8d0047736d16f384c9bfa3d339756c9045
wyselm <laura@cloud-elements.com>
2016-06-22 10:09:12 -0600

    adding test for Wrike to copy folders

--- src/test/elements/wrike/folders.js
+++ src/test/elements/wrike/folders.js
@@ -7,17 +7,20 @@ const expect = require('chakram').expect;
 suite.forElement('helpdesk', 'folders', (test) => {
   test.should.return200OnGet();
 
-  it('should CRUD for folders', () => {
-    let rootFolderId, folderId;
+  it('should CRUD and copy for folders', () => {
+    let rootFolderId, folderId, copyFolderId;
     let temp = { "title": "update folder title" };
 
     return cloud.get('/hubs/helpdesk/folders')
       .then(r => rootFolderId = r.body.filter(i => i.title === 'Root')[0].id)
-      .then(r => cloud.post('/hubs/helpdesk/folders/' + rootFolderId + '/folders', { "title": "Test Folder" }))
+      .then(r => cloud.post(`/hubs/helpdesk/folders/${rootFolderId}/folders`, { "title": "Test Folder" }))
       .then(r => folderId = r.body.id)
       .then(r => cloud.get(`/hubs/helpdesk/folders/${folderId}`))
       .then(r => cloud.put(`/hubs/helpdesk/folders/${folderId}`, temp))
       .then(r => expect(r.body.title).to.eq(temp.title))
+      .then(r => cloud.post(`/hubs/helpdesk/folders/${folderId}/copy`, { "title": "Copy Folder", "parent":  folderId}))
+      .then(r => copyFolderId = r.body.id)
+      .then(r => cloud.delete(`/hubs/helpdesk/folders/${copyFolderId}`))
       .then(r => cloud.delete(`/hubs/helpdesk/folders/${folderId}`));
   });
 });
