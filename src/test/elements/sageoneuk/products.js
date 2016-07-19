1480c33fa919319825df2b840f39ab4b3726b6a4
Tyler Toth <tyler@cloud-elements.com>
2016-07-19 15:38:34 -0500

    Update Sage One UK to Sage Accounting Hub

--- src/test/elements/sageoneuk/products.js
+++ src/test/elements/sageoneuk/products.js
@@ -4,7 +4,7 @@ const suite = require('core/suite');
 const payload = require('./assets/products');
 const chakram = require('chakram');
 
-suite.forElement('accounting', 'products', { payload: payload }, (test) => {
+suite.forElement('sageaccounting', 'products', { payload: payload }, (test) => {
   test.should.supportCruds(chakram.put);
   test.should.supportPagination();
   test.withOptions({ qs: { where: 'search =\'product\'' } }).should.return200OnGet();
