d2129ea2924c6bc2bb43b2a9c9b6b0baf5c4c48a
jjwyse <josh@cloud-elements.com>
2016-07-25 15:06:48 -0600

    adding test to ensure that the "user" section is returned when retrieving element instances

--- src/test/platform/elements/instances.js
+++ src/test/platform/elements/instances.js
@@ -5,7 +5,8 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const props = require('core/props');
 const provisioner = require('core/provisioner');
-const instanceSchema = require('./assets/element.instance.schema.json');
+const instanceSchema = require('./assets/element.instance.schema');
+const instancesSchema = require('./assets/element.instances.schema');
 
 const genInstance = (element, o) => ({
   name: (o.name || 'churros-instance'),
@@ -14,16 +15,18 @@ const genInstance = (element, o) => ({
   configuration: props.all(element)
 });
 
-const crudInstance = (baseUrl, schema) => {
+const crudsInstance = (baseUrl) => {
   let id;
   return provisioner.create('jira', undefined, baseUrl)
     .then(r => id = r.body.id)
     .then(r => cloud.get(`${baseUrl}/${id}`, (r) => {
-      expect(r).to.have.schemaAnd200(schema);
+      expect(r).to.have.schemaAnd200(instanceSchema);
       expect(r.body.configuration).to.not.be.empty;
       expect(r.body.configuration.password).to.equal("********");
       expect(r.body.configuration.username).to.equal(props.getForKey('jira', 'username'));
     }))
+    .then(r => cloud.get(`${baseUrl}`, instancesSchema))
+    .then(r => expect(r.body.length).to.be.above(0))
     .then(r => cloud.put(`${baseUrl}/${id}`, genInstance('jira', { name: 'updated-instance' })))
     .then(r => provisioner.delete(id, baseUrl));
 };
@@ -38,45 +41,35 @@ const updateInstanceWithReprovision = (baseUrl, schema) => {
       expect(r.body.configuration.password).to.equal("********");
       expect(r.body.configuration.username).to.equal(props.getForKey('shopify', 'username'));
     }))
-    .then(r => {
-      return provisioner.partialOauth('shopify');
-    })
+    .then(r => provisioner.partialOauth('shopify'))
     .then(code =>
       cloud.put(`${baseUrl}/${id}`, genInstance('shopify', { name: 'updated-instance', providerData: { code: code } }), r => {
-        expect(r).to.have.statusCode(200);
         expect(r.body.configuration).to.not.be.empty;
         expect(r.body.configuration.password).to.equal("********");
         expect(r.body.configuration.username).to.equal(props.getForKey('shopify', 'username'));
         expect(r.body).to.not.have.key('providerData');
       }))
     .then(r => cloud.get(`/hubs/ecommerce/orders`))
-    .then(r => {
-      expect(r).to.have.statusCode(200);
-      expect(r.body).to.be.instanceof(Array);
-    })
+    .then(r => expect(r.body).to.be.instanceof(Array))
     .then(r => provisioner.partialOauth('shopify'))
     .then(code => cloud.patch(`${baseUrl}/${id}`, { name: 'updated-instance', providerData: { code: code } }, r => {
-        expect(r).to.have.statusCode(200);
-        expect(r.body.configuration).to.not.be.empty;
-        expect(r.body.configuration.password).to.equal("********");
-        expect(r.body.configuration.username).to.equal(props.getForKey('shopify', 'username'));
-        expect(r.body).to.not.have.key('providerData');
+      expect(r.body.configuration).to.not.be.empty;
+      expect(r.body.configuration.password).to.equal("********");
+      expect(r.body.configuration.username).to.equal(props.getForKey('shopify', 'username'));
+      expect(r.body).to.not.have.key('providerData');
     }))
     .then(r => cloud.get(`/hubs/ecommerce/orders`))
-    .then(r => {
-      expect(r).to.have.statusCode(200);
-      expect(r.body).to.be.instanceof(Array);
-    })
+    .then(r => expect(r.body).to.be.instanceof(Array))
     .then(r => provisioner.delete(id, baseUrl));
 };
 
 const opts = { schema: instanceSchema };
 
 suite.forPlatform('elements/instances', opts, (test) => {
-  it('should support CRUD by key', () => crudInstance('elements/jira/instances', instanceSchema));
+  it('should support CRUD by key', () => crudsInstance('elements/jira/instances'));
   it('should support CRUD by ID', () => {
     return cloud.get('elements/jira')
-      .then(r => crudInstance(`elements/${r.body.id}/instances`, instanceSchema));
+      .then(r => crudsInstance(`elements/${r.body.id}/instances`));
   });
   it('should support update with reprovision by key', () => updateInstanceWithReprovision('/instances', instanceSchema));
 });
