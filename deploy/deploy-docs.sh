58bc33677fd77432321bd2c1e7e4a5a0c59469e9
jjwyse <josh@cloud-elements.com>
2016-08-08 16:41:33 -0600

    ...oh dear

--- deploy/deploy-docs.sh
+++ deploy/deploy-docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# hmm....
+git config user.name jjwyse
+git config user.email josh@cloud-elements.com
+
 git branch -D gh-pages
 git push origin :gh-pages
 git checkout -b gh-pages
