1c0681ea9781b25b80feee0e01c2a263cc9c3117
jjwyse <josh@cloud-elements.com>
2016-08-08 15:57:05 -0600

    working on fixing deploy docs

--- deploy/deploy-docs.sh
+++ deploy/deploy-docs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+git push origin :gh-pages
+git co -b gh-pages
 npm run docs
 git commit -am 'New docs release'
-git push origin :gh-pages
 git subtree push --prefix docs origin gh-pages
