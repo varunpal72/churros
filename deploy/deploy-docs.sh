937b55186a158bc3493f6c7bfd17ed8484aead8d
jjwyse <josh@cloud-elements.com>
2016-08-08 16:00:12 -0600

    silly me

--- deploy/deploy-docs.sh
+++ deploy/deploy-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git push origin :gh-pages
-git co -b gh-pages
+git checkout -b gh-pages
 npm run docs
 git commit -am 'New docs release'
 git subtree push --prefix docs origin gh-pages
