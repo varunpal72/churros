36e4c9cc60c6342af828aaad07bf61cf32debb70
mjhirsch <mike.hirsch@cloud-elements.com>
2016-08-09 17:51:42 -0600

    added etsy test ported from robot

---
+++ src/test/elements/etsy/orders.js
@@ -0,0 +1,13 @@
+//Orders API call NOT working >>> getting Access Denied response
+
+'use strict';
+
+const suite = require('core/suite');
+// const payload = require('./assets/orders');
+const cloud = require('core/cloud');
+
+suite.forElement('ecommerce', 'orders', {}, (test) => {
+  // checkout functions available under test.should which provide a lot of pre-canned tests
+  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
+ 
+});
