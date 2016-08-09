ccc4fce1cad0c40c3c455c01f41a6cd7ef3e4f5d
Charlie Ozinga <ozchaz@gmail.com>
2016-08-09 15:25:17 -0600

    Add zendesk element test

---
+++ src/test/elements/zendesk/agents.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/agents');
+
+const options = {
+  churros: {
+    updatePayload: {
+      "name": "Zendesk2",
+      "email": "test_user2@cloud-elements.com"
+    }
+  }
+};
+
+suite.forElement('helpdesk', 'agents', { payload }, (test) => {
+  test.should.supportPagination();
+  test.withOptions(options).should.supportCruds();
+});
