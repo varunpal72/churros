b83180305075cc8103ee5e73149d5238ed907027
ndkerby <ndkerby@gmail.com>
2016-08-09 14:44:58 -0600

    Ecwid tests for churros

---
+++ src/test/elements/ecwid/orders.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/orders');
+const cloud = require('core/cloud');
+
+suite.forElement('ecommerce', 'orders', { payload: payload }, (test) => {
+  test.should.supportSr();
+
+  it('it should support PATCH', () => {
+    return cloud.get('/hubs/ecommerce/orders')
+    .then(r => r.body.filter(r => r.id))
+    .then(filteredOrders => cloud.patch(`/hubs/ecommerce/orders/${filteredOrders[0].id}`, payload));
+  });
+
+  it('it should support GET payments by order id', () => {
+    return cloud.get('/hubs/ecommerce/orders')
+    .then(r => r.body.filter(r => r.id))
+    .then(filteredOrders => cloud.get(`/hubs/ecommerce/orders/${filteredOrders[0].id}/payments`));
+  });
+
+  it('it should support GET refunds by order id', () => {
+    return cloud.get('/hubs/ecommerce/orders')
+    .then(r => r.body.filter(r => r.id))
+    .then(filteredOrders => cloud.get(`/hubs/ecommerce/orders/${filteredOrders[0].id}/refunds`));
+  });
+});
