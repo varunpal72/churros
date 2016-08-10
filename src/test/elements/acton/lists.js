5d2c48f56e358d8c7fa0936df34ccec8e1b7a7fc
ndkerby <ndkerby@gmail.com>
2016-08-09 19:18:40 -0600

    rearranged tests

--- src/test/elements/acton/lists.js
+++ src/test/elements/acton/lists.js
@@ -8,6 +8,8 @@ const cloud = require('core/cloud');
 const tools = require('core/tools');
 payload.listname = tools.random();
 suite.forElement('marketing', 'lists', { payload: payload }, (test) => {
+  test.should.supportCruds();
+
   it('it should support GET all contacts inside a list', () => {
     return cloud.get('/hubs/marketing/lists')
     .then(r => r.body.filter(r => r.id))
@@ -18,16 +20,6 @@ suite.forElement('marketing', 'lists', { payload: payload }, (test) => {
     return cloud.get('/hubs/marketing/lists')
     .then(r => r.body.filter(r => r.id))
     .then(filteredLists => cloud.post(`/hubs/marketing/lists/${filteredLists[0].id}/contacts`, contactPayload));
-  });
-
-  it('it should support DELETE a contact inside a list', () => {
-    let filteredLists;
-
-    return cloud.get('/hubs/marketing/lists')
-    .then(r => r.body.filter(r => r.id))
-    .then(fi => filteredLists = fi)
-    .then(() => cloud.post(`/hubs/marketing/lists/${filteredLists[0].id}/contacts`, contactPayload))
-    .then(contacts => cloud.delete(`/hubs/marketing/lists/${filteredLists[0].id}/contacts/${contacts.body[0].id}`));
   });
 
   it('it should support GET a contact inside a list', () => {
@@ -50,8 +42,14 @@ suite.forElement('marketing', 'lists', { payload: payload }, (test) => {
     .then(contacts => cloud.patch(`/hubs/marketing/lists/${filteredLists[0].id}/contacts/${contacts.body[0].id}`, contactUpdatePayload));
   });
 
+  it('it should support DELETE a contact inside a list', () => {
+    let filteredLists;
 
-
-  test.should.supportCruds();
+    return cloud.get('/hubs/marketing/lists')
+    .then(r => r.body.filter(r => r.id))
+    .then(fi => filteredLists = fi)
+    .then(() => cloud.post(`/hubs/marketing/lists/${filteredLists[0].id}/contacts`, contactPayload))
+    .then(contacts => cloud.delete(`/hubs/marketing/lists/${filteredLists[0].id}/contacts/${contacts.body[0].id}`));
+  });
 
 });
