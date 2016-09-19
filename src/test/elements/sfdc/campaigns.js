5593be1b5dcc7fbc8ba3a01948d0b39c3eebedc4
Atul Barve <atul@cloud-elements.com>
2016-09-19 12:52:25 -0500

    Formatting and adding another Test

--- src/test/elements/sfdc/campaigns.js
+++ src/test/elements/sfdc/campaigns.js
@@ -5,5 +5,6 @@ const payload = require('./assets/campaigns');
 
 suite.forElement('crm', 'campaigns', { payload: payload }, (test) => {
   test.should.supportCruds();
+  test.should.supportCeqlSearch('id');
   test.should.supportPagination();
 });
