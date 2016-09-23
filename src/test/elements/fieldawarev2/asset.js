7f24b5e9707466f88592dfc0311dc951e75ade0d
Atul Barve <atul@cloud-elements.com>
2016-09-23 11:36:50 -0500

    Fixed Circle CI issue

--- src/test/elements/fieldawarev2/asset.js
+++ src/test/elements/fieldawarev2/asset.js
@@ -2,7 +2,6 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const tools = require('core/tools');
 const customerPayload = require('./assets/customers');
 
 const assetPayload = (locationId) => (
