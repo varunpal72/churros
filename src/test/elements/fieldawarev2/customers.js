d3cd7e55d94c2e8d57fcff60c552e0b68d217856
Atul Barve <atul@cloud-elements.com>
2016-09-24 14:01:21 -0500

    Fixed Extra semicolon

--- src/test/elements/fieldawarev2/customers.js
+++ src/test/elements/fieldawarev2/customers.js
@@ -5,7 +5,7 @@ const payload = require('./assets/customers');
 const tools = require('core/tools');
 
 suite.forElement('fsa', 'customers', { payload: payload }, (test) => {
-  payload.name = tools.random();;
+  payload.name = tools.random();
   test.should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
   test.should.supportCeqlSearch('name');
