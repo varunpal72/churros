82d3e2ce155e1a5e8cef9f415282b67e9526db4f
Taylor Clark <taylor@cloud-elements.com>
2016-09-29 17:14:34 -0600

    Shopify smart-collections Resources

--- src/test/elements/shopify/smart-collections.js
+++ src/test/elements/shopify/smart-collections.js
@@ -3,20 +3,22 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const payload = require('./assets/smart-collection.json');
-const smartCollectionUpdate = { "title": "newSmartCollection" };
+const smartCollectionUpdate = {"smart_collection" : {"title": "MyCollection"}};
 
 suite.forElement('shopify', 'smart-collections', { payload: payload }, (test) => {
-
+    let smartCollectionId;
   it('should allow CS for /smart-collections', () => {
     return cloud.post('/hubs/ecommerce/smart-collections', payload)
+      .then(r => smartCollectionId = r.body.smart_collection.id)
       .then(r => cloud.get('/hubs/ecommerce/smart-collections'))
+      .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
   });
 
   it('should allow CRUDS for /smart-collections/:id', () => {
-    let collectionId;
+    let smartCollectionId;
 
     return cloud.post('/hubs/ecommerce/smart-collections', payload)
-      .then(r => smartCollectionId = r.body.id)
+      .then(r => smartCollectionId = r.body.smart_collection.id)
       .then(r => cloud.patch('/hubs/ecommerce/smart-collections/' + smartCollectionId, smartCollectionUpdate))
       .then(r => cloud.get('/hubs/ecommerce/smart-collections/' + smartCollectionId))
       .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
