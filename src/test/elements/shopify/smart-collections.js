9510371178d0663122d5fed8b08c7ccc9c8dfcab
Taylor Clark <taylor@cloud-elements.com>
2016-09-29 17:30:32 -0600

    Smart Collections Resources

---
+++ src/test/elements/shopify/smart-collections.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const payload = require('./assets/smart-collection.json');
+const smartCollectionUpdate = {"smart_collection" : {"title": "MyCollection"}};
+
+suite.forElement('shopify', 'smart-collections', { payload: payload }, (test) => {
+    let smartCollectionId;
+  it('should allow CS for /smart-collections', () => {
+    return cloud.post('/hubs/ecommerce/smart-collections', payload)
+      .then(r => smartCollectionId = r.body.smart_collection.id)
+      .then(r => cloud.get('/hubs/ecommerce/smart-collections'))
+      .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
+  });
+
+  it('should allow CRUDS for /smart-collections/:id', () => {
+    let smartCollectionId;
+
+    return cloud.post('/hubs/ecommerce/smart-collections', payload)
+      .then(r => smartCollectionId = r.body.smart_collection.id)
+      .then(r => cloud.patch('/hubs/ecommerce/smart-collections/' + smartCollectionId, smartCollectionUpdate))
+      .then(r => cloud.get('/hubs/ecommerce/smart-collections/' + smartCollectionId))
+      .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
+  });
+});
