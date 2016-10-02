b1d63574e0ee7976611135be8630f26d3406b908
Taylor Clark <taylor@cloud-elements.com>
2016-10-02 08:32:04 -0600

    Replaced test with pre-defined test functions

--- src/test/elements/shopify/smart-collections.js
+++ src/test/elements/shopify/smart-collections.js
@@ -5,22 +5,6 @@ const cloud = require('core/cloud');
 const payload = require('./assets/smart-collection.json');
 const smartCollectionUpdate = {"title": "MyCollection"};
 
-suite.forElement('shopify', 'smart-collections', { payload: payload }, (test) => {
-    let smartCollectionId;
-  it('should allow CS for /smart-collections', () => {
-    return cloud.post('/hubs/ecommerce/smart-collections', payload)
-      .then(r => smartCollectionId = r.body.id)
-      .then(r => cloud.get('/hubs/ecommerce/smart-collections'))
-      .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
-  });
-
-  it('should allow CRUDS for /smart-collections/:id', () => {
-    let smartCollectionId;
-
-    return cloud.post('/hubs/ecommerce/smart-collections', payload)
-      .then(r => smartCollectionId = r.body.id)
-      .then(r => cloud.patch('/hubs/ecommerce/smart-collections/' + smartCollectionId, smartCollectionUpdate))
-      .then(r => cloud.get('/hubs/ecommerce/smart-collections/' + smartCollectionId))
-      .then(r => cloud.delete('/hubs/ecommerce/smart-collections/' + smartCollectionId));
-  });
+suite.forElement('ecommerce', 'smart-collections', { payload: payload }, (test) => {
+    test.should.supportCruds();
 });
