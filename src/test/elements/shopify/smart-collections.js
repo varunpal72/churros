2d746685933333c447b0495fd9b849eec5f0f535
Taylor Clark <taylor@cloud-elements.com>
2016-10-02 08:50:49 -0600

    removed unused packages/variables

--- src/test/elements/shopify/smart-collections.js
+++ src/test/elements/shopify/smart-collections.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 const payload = require('./assets/smart-collection.json');
-const smartCollectionUpdate = {"title": "MyCollection"};
 
 suite.forElement('ecommerce', 'smart-collections', { payload: payload }, (test) => {
     test.should.supportCruds();
