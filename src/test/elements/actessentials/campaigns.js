1fcca492e637ddc32d81d37397f9c2a924a58595
Luke Vance <lukevance90@gmail.com>
2016-10-06 09:54:38 -0600

    updated basic tests add specialty tests to activities and campaigns

--- src/test/elements/actessentials/campaigns.js
+++ src/test/elements/actessentials/campaigns.js
@@ -5,15 +5,18 @@ const payload = require('./assets/campaigns');
 const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'campaigns', {payload: payload}, (test) => {
-  test.should.supportCruds();
   test.should.supportPagination();
+  test.should.supportCrs();
+  // get /campaigns
+  // test.should.ret  urn200OnGet();
+  // post campaigns
+  // test.should.return200OnPost();
 
-  it('should only GET for campaigns/{id}', () => {
-    return cloud.post(test.api, payload)
-    .then(function(returnVal){
-      console.log(returnVal);
-    });
-    
-  });
+  // it('should RETRIEVE for /campaigns/{id}', () => {
+  //   let campaignId = null;
+  //   return cloud.get(test.api)
+  //   .then(r => campaignId = r.body[0].campaignId)
+  //   .then(r => cloud.get(`${test.api}/${campaignId}`));
+  // });
 
 });
