4be1d5960d9d7f611450ea573d37ec72cc2d7e3f
wyselm <laura@cloud-elements.com>
2016-10-13 17:41:07 -0600

    skiping 1 jira test where we don't delete

--- src/test/elements/jira/fields.js
+++ src/test/elements/jira/fields.js
@@ -4,7 +4,7 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const payload = require('./assets/field');
 
-suite.forElement('helpdesk', 'fields', {payload:payload}, (test) => {
+suite.forElement('helpdesk', 'fields', {skip: true, payload:payload}, (test) => {
   it('should allow create and get of fields', () => {
     cloud.post("/hubs/helpdesk/fields", payload)
     .then(cloud.get("/hubs/helpdesk/fields"));
