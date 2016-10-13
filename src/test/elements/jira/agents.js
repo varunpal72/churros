8fc5b67ed4c20442f5a4b7eab6a227dc79ae404b
Martin Brandt <martinfbrandt@gmail.com>
2016-10-13 17:08:44 -0600

    Jira tests....finally

---
+++ src/test/elements/jira/agents.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('helpdesk', 'agents', (test) => {
+  let agentId;
+  it('should allow get for /agents', () => {
+    cloud.withOptions({qs:{where:`username='test'`}}).get('/hubs/helpdesk/agents')
+    .then(r => agentId = r.body[0].key)
+    .then(r => cloud.get("/hubs/helpdesk/agents/" + agentId))
+  }
+);
+}
+);
---
+++ src/test/elements/jira/incidentTypes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const payload = require('./assets/issuetype');
+const uri = '/hubs/helpdesk/incident-types';
+const updatePayload = {};
+updatePayload.name = payload.name;
+
+suite.forElement('helpdesk', 'incidentTypes', {payload:payload}, (test) => {
+  it('should allow CRUDS for /incidents-types', () => {
+  let incidentTypeId;
+  cloud.post(uri, payload)
+  .then(r => incidentTypeId = r.body.id)
+  .then(r => cloud.get(uri + "/" + incidentTypeId))
+  .then(r => cloud.get(uri))
+  .then(r => cloud.update(uri + '/' + incidentTypeId, updatePayload))
+  .then(r => cloud.delete(uri + '/' + incidentTypeId));
+});
+});
