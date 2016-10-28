5138d3409874b055eef86b38820ee0118962a3b2
charuhas.vidwans <charuhas.vidwans@gslab.com>
2016-10-29 00:03:06 +0530

    JSON formatting changes

--- src/test/elements/marketo/folders.js
+++ src/test/elements/marketo/folders.js
@@ -5,17 +5,17 @@ const suite = require('core/suite');
 const payload = require('./assets/folders');
 const cloud = require('core/cloud');
 const updatePayload = {
-  			"name" : "Test Folder Updated",
-  			"description" : tools.randomStr()
-			};
+  "name": "Test Folder Updated",
+  "description": tools.randomStr()
+};
 
 suite.forElement('marketing', 'folders', { payload: payload }, (test) => {
   it('It should perform CRUS for /folders', () => {
-	let id;
-	return cloud.post(test.api, payload)
-		.then(r => id = r.body.id)
-		.then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
-		.then(r => cloud.get(test.api))
-		.then(r => cloud.get(`${test.api}/${id}`));
+    let id;
+    return cloud.post(test.api, payload)
+      .then(r => id = r.body.id)
+      .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.get(`${test.api}/${id}`));
   });
 });
--- src/test/elements/marketo/lists.js
+++ src/test/elements/marketo/lists.js
@@ -4,7 +4,6 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'lists', (test) => {
-  //test.withApi(test.api + '/${tListId}/leads/${leadId}/isMember').should.return200OnGet();
   it('should create a customer and then CRDS for an attachment', () => {
     let listId = 1001;
     let leadId = 42293;
