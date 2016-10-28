660c44adc98ec90b6da95b861f1d0c7b325ac7c5
Tyler Toth <tyler@cloud-elements.com>
2016-10-28 09:42:51 -0500

    QuickBase contacts/attachments update

--- src/test/elements/quickbase/contacts.js
+++ src/test/elements/quickbase/contacts.js
@@ -12,7 +12,7 @@ suite.forElement('db', 'contacts', { payload: payload }, (test) => {
     let contactId;
     return cloud.post(test.api, payload)
       .then(r => contactId = r.body.id)
-      .then(r => cloud.withOptions({ qs: { fieldName: 'file' } }).postFile(`${test.api}/${contactId}/attachments`, __dirname + '/assets/attach.txt'))
+      .then(r => cloud.withOptions({ qs: { fieldName : 'file' } }).postFile(`${test.api}/${contactId}/attachments`, __dirname + '/assets/attach.txt'))
       .then(r => cloud.get(`${test.api}/${contactId}/attachments/22`))
       .then(r => cloud.delete(`${test.api}/${contactId}`));
   });
