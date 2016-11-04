08bb7657a3bd6bbe08fd4ddf2d3cff81e12d621c
charuhas.vidwans <charuhas.vidwans@gslab.com>
2016-11-04 15:14:54 +0530

    Changes based on review comments

--- src/test/elements/twitter/friendships.js
+++ src/test/elements/twitter/friendships.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const suite = require('core/suite');
+const payload = require('./assets/friendships');
 
-suite.forElement('social', 'friendships', null, (test) => {
+suite.forElement('social', 'friendships', { payload: payload }, (test) => {
   test.withOptions({ qs: { where: `sourceScreenName = 'madhuri_ce' and targetScreenName = 'dingbat_27'` } }).should.return200OnGet();
   test.withApi(`${test.api}/incoming`).should.supportPagination();
   test.withApi(`${test.api}/incoming`).should.return200OnGet();
   test.withApi(`${test.api}/outgoing`).should.supportPagination();
   test.withApi(`${test.api}/outgoing`).should.return200OnGet();
-  test.withOptions({ qs: { id: '140688569', follow: true } }).should.return200OnPost();
+  test.should.return200OnPost();
 });
