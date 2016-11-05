0d6eeab12ac4e0abbac4dfe48f7f2230ad93b8fc
charuhas.vidwans <charuhas.vidwans@gslab.com>
2016-11-05 18:24:19 +0530

    changing tweets to statuses

--- src/test/elements/twitter/statuses.js
+++ src/test/elements/twitter/statuses.js
@@ -1,20 +1,18 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/tweets');
+const payload = require('./assets/statuses');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const tweetsPayload = build({ status: tools.random() });
 
-suite.forElement('social', 'tweets', { payload: tweetsPayload }, (test) => {
-  test.withOptions({ qs: { where: 'q = \'Jurgen Klopp\'', page: 1, pageSize: 1 } }).should.return200OnGet();
-  it('should create and get a tweet and then patch for variant', () => {
+suite.forElement('social', 'statuses', { payload: tweetsPayload }, (test) => {
+  it('should create and get a statuses and then patch for variant', () => {
     let tweetId;
-    return cloud.post(test.api, tweetsPayload)
+    return cloud.post(test.api, tweetsPayload);
       .then(r => tweetId = r.body.id_str)
       .then(r => cloud.get(`${test.api}/${tweetId}`), null)
       .then(r => cloud.delete(`${test.api}/${tweetId}`), null);
   });
-
 });
