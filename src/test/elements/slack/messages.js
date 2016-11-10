8151dc800a5db6efc36f003c85e71694497f4bd4
Luke Vance <lukevance90@gmail.com>
2016-11-10 16:41:53 -0700

    fixed tests for messages

--- src/test/elements/slack/messages.js
+++ src/test/elements/slack/messages.js
@@ -1,34 +1,10 @@
-// 'use strict';
-//
-// const suite = require('core/suite');
-// // const payload = require('./assets/channels');
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/messages');
 // const cloud = require('core/cloud');
-//
-// suite.forElement('collaboration', 'messages', { payload: 'blah' }, (test) => {
-//   // check channel create (generally skip this one)
-//   test.withOptions({skip:true}).should.return200OnPost();
-//   test.should.supportSr();
-//
-//   it('should allow PATCH on public channels for all actions', () => {
-//     return cloud.get(test.api)
-//     .then(response => response.body.filter(channel => channel.name === 'churros-test-channel'))
-//     .then(channels => {
-//       // console.log(channels);
-//       if (channels.length > 0) {return channels[0];}
-//       else {return cloud.post(test.api, createPayload);}
-//     })
-//     .then(r => {
-//       console.log(r);
-//       cloud.update(`${test.api}/${r.id}/actions`, {action: 'rename', name: 'churros-check'});
-//     })
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'purpose', purpose: 'eat yummy churros'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'topic', name: 'tasty desserts'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'archive'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'unarchive'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'leave'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'join', name: 'churros-check'}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'read', ts: 1412341512.123410}))
-//     .then(r => cloud.update(`${test.api}/${r.id}/actions`, {action: 'rename', name: 'churros-test-channel'}));
-//   });
-//
-// });
+
+suite.forElement('collaboration', 'messages', payload, (test) => {
+  test.should.supportCruds();
+
+});
