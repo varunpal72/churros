af8f3811e2674bae41b7613552d5105ed8d7633f
Tyler Toth <tyler@cloud-elements.com>
2016-11-15 12:15:31 -0600

    Adding suite level skip for orders/products

--- src/test/elements/infusionsoftecommerce/orders.js
+++ src/test/elements/infusionsoftecommerce/orders.js
@@ -2,7 +2,7 @@
 
 const suite = require('core/suite');
 
-suite.forElement('ecommerce', 'orders', null, (test) => {
+suite.forElement('ecommerce', 'orders', {skip: true}, (test) => {
   test.should.supportSr();
   test.withOptions({ qs: { page: 1, pageSize: 1}}).should.return200OnGet();
 });
