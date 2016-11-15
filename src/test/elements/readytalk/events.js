7678fd18ea221a326810cac849b2d60e68d21b6e
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2016-11-15 09:09:36 +0000

    Remove events.json

--- src/test/elements/readytalk/events.js
+++ src/test/elements/readytalk/events.js
@@ -2,10 +2,9 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const payload = require('./assets/events');
 
-suite.forElement('general', 'events', { payload: payload }, (test) => {
-it('should allow Sr /events ,Sr /events/attendees   ', () => {
+suite.forElement('general', 'events', { payload: null}, (test) => {
+it('should allow Sr /events ,Sr /events/attendees ', () => {
     let eventId,attendeesId;
     return cloud.get(test.api)
       .then(r => eventId = r.body[0].id)
