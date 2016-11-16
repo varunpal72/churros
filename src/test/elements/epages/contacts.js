ec36ea4e45dbe218476bf6e7bdd95ed3b85beaa7
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2016-11-16 08:24:05 +0000

    Did all changes as per review comment

--- src/test/elements/epages/contacts.js
+++ src/test/elements/epages/contacts.js
@@ -3,15 +3,12 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
-const payload  =  {
-  "company": tools.randomStr,
-  "contactPerson": tools.randomStr,
-  "contactPersonJobTitle": tools.randomStr,
-  "email": tools.randomEmail()
-};
+const payload = require('./assets/contacts');
+const build = (overrides) => Object.assign({}, payload, overrides);
+const contactsPayload = build({ company: tools.randomStr(), contactPerson: tools.randomStr(), contactPersonJobTitle: tools.randomStr(), email : tools.randomEmail()}); 
 
-suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
-     it('should allow RU for /contacts', () => {
+suite.forElement('crm', 'contacts', { payload: contactsPayload }, (test) => {
+     it('should allow RU for /hub/crm/contacts', () => {
         return cloud.get(test.api)
           .then(r => cloud.withOptions({ qs: {locale:'en_US' }}).put(test.api,payload));
       });
--- src/test/elements/epages/orders.js
+++ src/test/elements/epages/orders.js
@@ -2,18 +2,16 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const payload = [
-  {
-    "op": "add",
-    "path": "/customerComment",
-    "value": "Please deliver asap."
-  }
-];
+const tools = require('core/tools');
+const payload =require('./assets/orders');
+const build = (overrides) => Object.assign({}, payload, overrides);
+const orderPayload = build({value: tools.randomStr()});
 
-suite.forElement('crm', 'orders', { payload: payload }, (test) => {	
+suite.forElement('crm', 'orders', { payload: orderPayload }, (test) => {	
  let id;
- it('should allow SRU for /orders', () => {
-	return cloud.get(test.api)
+ it('should allow SRU for /hub/crm/orders', () => {
+	 test.should.supportPagination();
+	 return cloud.withOptions({ qs: { where: 'locale = \'en_US\'' } }).get(test.api)
 	  .then(r => id =r.body[0].id)
 	  .then(r => cloud.get(`${test.api}/${id}`))
 	  .then(r => cloud.patch(`${test.api}/${id}`,payload));
