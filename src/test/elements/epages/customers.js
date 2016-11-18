24e52e66743784787eb45e8233271d68226c1384
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2016-11-18 17:04:08 +0000

    Updated Churros for order and added for Customers

--- src/test/elements/epages/customers.js
+++ src/test/elements/epages/customers.js
@@ -4,24 +4,24 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
 const payload = require('./assets/customers');
-const build = (overrides) => Object.assign(}, payload, overrides);
-const customersPayload = build({ customerNumber : tools.randomInt(),billingAddress.firstName: tools.random(),  billingAddress.lastName: tools.random()});
+const build = (overrides) => Object.assign({}, payload, overrides);
+const customersPayload = build({ customerNumber : tools.randomInt()});
 const updatePayload = {
       "customers": [
         {
           "op": "add",
           "path": "/customerNumber",
-          "value": "0812"
+          "value": tools.randomInt()
         }
       ]
 };
 suite.forElement('crm', 'customers', { payload: customersPayload }, (test) => {
  test.should.supportPagination();
- it('should allow SRU for /hub/crm/orders', () => {
+ it('should allow SRU for /hub/crm/customers', () => {
    let id;
 	 return cloud.post(test.api, customersPayload)
-	  .then(r => id =r.body.customerId)
+	  .then(r => id =r.body.id)
 	  .then(r => cloud.get(`${test.api}/${id}`))
 	  .then(r => cloud.patch(`${test.api}/${id}`,updatePayload));
-  });*/
+  });
 });
