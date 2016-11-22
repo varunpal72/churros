befeb6e5b88ae80c2c0448685390f778a455ced1
Luke Vance <lukevance90@gmail.com>
2016-11-21 22:13:46 -0700

    added tests for project subresources

--- src/test/elements/gooddata/projects.js
+++ src/test/elements/gooddata/projects.js
@@ -5,24 +5,91 @@ const payload = require('./assets/projects');
 const cloud = require('core/cloud');
 
 suite.forElement('db', 'projects', { payload: payload }, (test) => {
+  test.withOptions({skip:true}).should.supportCruds();
   // get authenticated user info
   test.should.return200OnGet();
 
-  it('should support GET for all reports in a project', () => {
+  it('should allow POST for /projects/{projectId}/report-definitions', () => {
+    let reportDefinition = {
+
+    };
+    return cloud.get(test.api)
+    .then(r => r.body[0].project.links.self.substr(14))
+    .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, reportDefinition));
+  });
+
+  it('should allow POST for /projects/{projectId}/reports', () => {
+    let report = {};
+    return cloud.get(test.api)
+    .then(r => r.body[0].project.links.self.substr(14))
+    .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, report));
+  });
+
+  it('should allow GET for /projects/{projectId}/reports', () => {
     // let projectId = '';
     return cloud.get(test.api)
     .then(r => r.body[0].project.links.self.substr(14))
     .then(projectId => cloud.get(`${test.api}/${projectId}/reports`));
   });
 
-  it('should support POST for /projects/{projectId}/reports-execute-json', () => {
+  it('should allow PATCH for /projects/{projectId}/reports/json', () => {
     let projectId = '';
-    let report_request = {};
+    let report_request = {
+      "report_req": {
+        "report": "/gdc/md/{project_id}/obj/{object_id}"
+      }
+    };
     return cloud.get(test.api)
     .then(r => projectId = r.body[0].project.links.self.substr(14))
     .then(() => cloud.get(`${test.api}/${projectId}/reports`))
-    .then(r => r.body[0].link)
-    .then(reportId => cloud.post(`${test.api}/${projectId}/reports-execute-json`, report_request));
+    .then(r => report_request.report_req.report = r.body[0].link)
+    .then(() => cloud.patch(`${test.api}/${projectId}/reports/json`, report_request));
   });
+
+  // it('should allow GET for /projects/{projectId}/reports/{reportId}/export-json', () => {
+  //   let projectId = '';
+  //   let report_request = {
+  //     "report_req": {
+  //       "reportDefinition": "/gdc/md/{project_id}/obj/{object_id}"
+  //     }
+  //   };
+  //   return cloud.get(test.api)
+  //   .then(r => projectId = r.body[0].project.links.self.substr(14))
+  //   .then(() => cloud.get(`${test.api}/${projectId}/reports`))
+  //   .then(r => report_request = r.body[0].link)
+  //   .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-json`, report_request))
+  //   .then(r => r.uri.substr(r.uri.indexOf('?')))
+  //   .then(reportId => cloud.get(`${test.api}/${projectId}/reports/${reportId}/export-json`));
+  // });
+
+  it('should allow PATCH for /projects/{projectId}/reports-execute-csv', () => {
+    let projectId = '';
+    let report_request = {
+      "report_req": {
+        "report": "/gdc/md/{project_id}/obj/{object_id}"
+      }
+    };
+    return cloud.get(test.api)
+    .then(r => projectId = r.body[0].project.links.self.substr(14))
+    .then(() => cloud.get(`${test.api}/${projectId}/reports`))
+    .then(r => report_request.report_req.report = r.body[0].link)
+    .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-csv`, report_request));
+  });
+
+  // it('should allow GET for /projects/{projectId}/reports/{reportId}/export-csv', () => {
+  //   let projectId = '';
+  //   let report_request = {
+  //     "report_req": {
+  //       "reportDefinition": "/gdc/md/{project_id}/obj/{object_id}"
+  //     }
+  //   };
+  //   return cloud.get(test.api)
+  //   .then(r => projectId = r.body[0].project.links.self.substr(14))
+  //   .then(() => cloud.get(`${test.api}/${projectId}/reports`))
+  //   .then(r => report_request = r.body[0].link)
+  //   .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-csv`, report_request))
+  //   .then(r => r.execResult.dataResult.substr(r.execResult.dataResult.indexOf('dataResult/')))
+  //   .then(reportId => cloud.get(`${test.api}/${projectId}/reports/${reportId}/export-csv`));
+  // });
 
 });
