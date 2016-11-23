f07b1946b02d018d90c02ee440b6665a4191f51d
Luke Vance <lukevance90@gmail.com>
2016-11-23 10:33:27 -0700

    updated tests for report file calls

--- src/test/elements/gooddata/projects.js
+++ src/test/elements/gooddata/projects.js
@@ -2,94 +2,83 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/projects');
+const reportDefinitionPayload = require('./assets/reports').reportDefinition;
+const reportPayload = require('./assets/reports').report;
 const cloud = require('core/cloud');
 
 suite.forElement('db', 'projects', { payload: payload }, (test) => {
   test.withOptions({skip:true}).should.supportCruds();
-  // get authenticated user info
+  // get projects for authenticated user
   test.should.return200OnGet();
 
-  it('should allow POST for /projects/{projectId}/report-definitions', () => {
-    let reportDefinition = {
+  // it('should allow POST for /projects/{projectId}/report-definitions', () => {
+  //   return cloud.get(test.api)
+  //   .then(r => r.body[0].project.links.self.substr(14))
+  //   .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, reportDefinitionPayload));
+  // });
 
-    };
-    return cloud.get(test.api)
-    .then(r => r.body[0].project.links.self.substr(14))
-    .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, reportDefinition));
-  });
+  // it('should allow POST for /projects/{projectId}/reports', () => {
+  //   return cloud.get(test.api)
+  //   .then(r => r.body[0].project.links.self.substr(14))
+  //   .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, reportPayload));
+  // });
 
-  it('should allow POST for /projects/{projectId}/reports', () => {
-    let report = {};
-    return cloud.get(test.api)
-    .then(r => r.body[0].project.links.self.substr(14))
-    .then(projectId => cloud.post(`${test.api}/${projectId}/reports`, report));
-  });
-
-  it('should allow GET for /projects/{projectId}/reports', () => {
-    // let projectId = '';
+  it('should allow GET for /projects/{id}/reports', () => {
     return cloud.get(test.api)
     .then(r => r.body[0].project.links.self.substr(14))
     .then(projectId => cloud.get(`${test.api}/${projectId}/reports`));
   });
 
-  it('should allow PATCH for /projects/{projectId}/reports/json', () => {
+  it('should allow PATCH for /projects/{id}/reports/json', () => {
     let projectId = '';
     let report_request = {
-      "report_req": {
-        "report": "/gdc/md/{project_id}/obj/{object_id}"
-      }
+      "report": "{object_id}"
     };
     return cloud.get(test.api)
     .then(r => projectId = r.body[0].project.links.self.substr(14))
     .then(() => cloud.get(`${test.api}/${projectId}/reports`))
-    .then(r => report_request.report_req.report = r.body[0].link)
+    .then(r => report_request.report = r.body[0].link.substr(r.body[0].link.indexOf("/obj/") + 5))
     .then(() => cloud.patch(`${test.api}/${projectId}/reports/json`, report_request));
   });
 
-  // it('should allow GET for /projects/{projectId}/reports/{reportId}/export-json', () => {
-  //   let projectId = '';
-  //   let report_request = {
-  //     "report_req": {
-  //       "reportDefinition": "/gdc/md/{project_id}/obj/{object_id}"
-  //     }
-  //   };
-  //   return cloud.get(test.api)
-  //   .then(r => projectId = r.body[0].project.links.self.substr(14))
-  //   .then(() => cloud.get(`${test.api}/${projectId}/reports`))
-  //   .then(r => report_request = r.body[0].link)
-  //   .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-json`, report_request))
-  //   .then(r => r.uri.substr(r.uri.indexOf('?')))
-  //   .then(reportId => cloud.get(`${test.api}/${projectId}/reports/${reportId}/export-json`));
-  // });
-
-  it('should allow PATCH for /projects/{projectId}/reports-execute-csv', () => {
+  it('should allow GET for /projects/{id}/reports/json', () => {
     let projectId = '';
     let report_request = {
-      "report_req": {
-        "report": "/gdc/md/{project_id}/obj/{object_id}"
-      }
+      "report": "{object_id}"
     };
     return cloud.get(test.api)
     .then(r => projectId = r.body[0].project.links.self.substr(14))
     .then(() => cloud.get(`${test.api}/${projectId}/reports`))
-    .then(r => report_request.report_req.report = r.body[0].link)
-    .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-csv`, report_request));
+    .then(r => report_request.report = r.body[0].link.substr(r.body[0].link.indexOf("/obj/") + 5))
+    .then(() => cloud.patch(`${test.api}/${projectId}/reports/json`, report_request))
+    .then(r => r.body.execResult.dataResult.substr(r.body.execResult.dataResult.indexOf("dataResult/") + 11))
+    .then(reportId => cloud.withOptions({qs:{reportId:reportId}}).get(`${test.api}/${projectId}/reports/json`));
   });
 
-  // it('should allow GET for /projects/{projectId}/reports/{reportId}/export-csv', () => {
-  //   let projectId = '';
-  //   let report_request = {
-  //     "report_req": {
-  //       "reportDefinition": "/gdc/md/{project_id}/obj/{object_id}"
-  //     }
-  //   };
-  //   return cloud.get(test.api)
-  //   .then(r => projectId = r.body[0].project.links.self.substr(14))
-  //   .then(() => cloud.get(`${test.api}/${projectId}/reports`))
-  //   .then(r => report_request = r.body[0].link)
-  //   .then(() => cloud.patch(`${test.api}/${projectId}/reports-execute-csv`, report_request))
-  //   .then(r => r.execResult.dataResult.substr(r.execResult.dataResult.indexOf('dataResult/')))
-  //   .then(reportId => cloud.get(`${test.api}/${projectId}/reports/${reportId}/export-csv`));
-  // });
+  it('should allow PATCH for /projects/{id}/reports/csv', () => {
+    let projectId = '';
+    let report_request = {
+      "report": "{object_id}"
+    };
+    return cloud.get(test.api)
+    .then(r => projectId = r.body[0].project.links.self.substr(14))
+    .then(() => cloud.get(`${test.api}/${projectId}/reports`))
+    .then(r => report_request.report = r.body[0].link.substr(r.body[0].link.indexOf("/obj/") + 5))
+    .then(() => cloud.patch(`${test.api}/${projectId}/reports/csv`, report_request));
+  });
+
+  it('should allow GET for /projects/{id}/reports/csv', () => {
+    let projectId = '';
+    let report_request = {
+      "report": "{object_id}"
+    };
+    return cloud.get(test.api)
+    .then(r => projectId = r.body[0].project.links.self.substr(14))
+    .then(() => cloud.get(`${test.api}/${projectId}/reports`))
+    .then(r => report_request.report = r.body[0].link.substr(r.body[0].link.indexOf("/obj/") + 5))
+    .then(() => cloud.patch(`${test.api}/${projectId}/reports/csv`, report_request))
+    .then(r => r.body.uri.substr(r.body.uri.indexOf('raw/') + 4))
+    .then(reportHash => cloud.withOptions({qs:{reportHash:reportHash}}).get(`${test.api}/${projectId}/reports/csv`));
+  });
 
 });
