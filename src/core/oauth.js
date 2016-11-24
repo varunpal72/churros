0e072aaebd3c6659ca14fe9bc914430709217a81
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2016-11-24 08:33:59 +0000

    Added Churros for Onedrivev2

--- src/core/oauth.js
+++ src/core/oauth.js
@@ -242,16 +242,11 @@ const manipulateDom = (element, browser, r, username, password, config) => {
       return browser.getCurrentUrl();
     case 'onedrivev2':
       browser.get(r.body.oauthUrl);
-      browser.isElementPresent(webdriver.By.id('i0116'));
       browser.findElement(webdriver.By.id('i0116')).sendKeys(username);
+      browser.findElement(webdriver.By.id('idSIButton9')).click();
+      browser.sleep(3000);
       browser.findElement(webdriver.By.id('i0118')).sendKeys(password);
       browser.findElement(webdriver.By.id('idSIButton9')).click();
-      browser.findElement(webdriver.By.id('idBtn_Accept'))
-        .then((element) => element.click(),
-          (err) => {
-            if (err.state && err.state === 'no such element') { // ignore this
-            } else { webdriver.promise.rejected(err); }
-          });
       return browser.getCurrentUrl();
     case 'quickbooks':
       browser.get(r.body.oauthUrl);
@@ -395,6 +390,7 @@ const manipulateDom = (element, browser, r, username, password, config) => {
       browser.findElement(webdriver.By.id('password')).sendKeys(password);
       browser.findElement(webdriver.By.id('allow')).click();
       return browser.getCurrentUrl();
+
     default:
       throw 'No OAuth function found for element ' + element + '.  Please implement function in core/oauth so ' + element + ' can be provisioned';
   }
