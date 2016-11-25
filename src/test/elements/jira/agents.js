485159adc5db2d4bb827e086ff6b127f49d361c0
root <hanumant.shinde@gslab.com>
2016-11-25 11:15:59 +0000

    added churros test  cases for pagination of jira

--- src/test/elements/jira/agents.js
+++ src/test/elements/jira/agents.js
@@ -6,10 +6,9 @@ const cloud = require('core/cloud');
 suite.forElement('helpdesk', 'agents', (test) => {
   let agentId;
   it('should allow get for /agents', () => {
-    cloud.withOptions({qs:{where:`username='test'`}}).get('/hubs/helpdesk/agents')
+    cloud.withOptions({qs:{where:`username='test'`}}).get('/hubs/helpdesk/agents') 
+    .then(r => cloud.withOptions({qs:{where:`username='test'`, page: 1, pageSize: 1 }}).get('/hubs/helpdesk/agents'))
     .then(r => agentId = r.body[0].key)
     .then(r => cloud.get("/hubs/helpdesk/agents/" + agentId));
-  }
-);
-}
-);
+  });
+});
