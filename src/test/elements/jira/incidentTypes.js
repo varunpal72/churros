7755faed33a3919691cb2875595dd85b752f9cab
root <hanumant.shinde@gslab.com>
2016-11-28 03:48:45 +0000

    changed as per review comment

--- src/test/elements/jira/incidentTypes.js
+++ src/test/elements/jira/incidentTypes.js
@@ -15,7 +15,7 @@ suite.forElement('helpdesk', 'incidentTypes', {payload:payload}, (test) => {
   cloud.post(uri, payload)
   .then(r => incidentTypeId = r.body.id)
   .then(r => cloud.withOptions({qs:{ page: 1, pageSize: 1 }}).get('/hubs/helpdesk/incident-types'))
-  .then(r => cloud.withOptions({qs:{ id: `${incidentTypeId}` }}).get('/hubs/helpdesk/incident-types'))
+  .then(r => cloud.withOptions({qs:{ where: `id='${incidentTypeId}'` }}).get('/hubs/helpdesk/incident-types'))
   .then(r => cloud.get(uri + "/" + incidentTypeId))
   .then(r => cloud.get(uri))
   .then(r => cloud.update(uri + '/' + incidentTypeId, updatePayload))
