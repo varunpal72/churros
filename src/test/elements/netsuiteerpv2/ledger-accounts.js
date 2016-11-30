2540b0f2ed3fb39dbffcf9672464b6d081c54863
Tyler Toth <tyler@cloud-elements.com>
2016-11-30 16:16:34 -0600

    Formatting updates

--- src/test/elements/netsuiteerpv2/ledger-accounts.js
+++ src/test/elements/netsuiteerpv2/ledger-accounts.js
@@ -4,12 +4,12 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('erp', 'ledger-accounts', null, (test) => {
-        it('should allow SR for /hubs/erp/ledger-accounts ', () => {
-        let ledgerAccountId;
-        return cloud.get(test.api)
-          .then(r => ledgerAccountId = r.body[0].internalId)
-          .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 5}}).get(`${test.api}`))
-          .then(r => cloud.withOptions({ qs: { where: `internalId ='${ledgerAccountId}'`}}).get(`${test.api}`))
-          .then(r => cloud.get(`${test.api}/${ledgerAccountId}`));
-        });
+  it('should allow SR for /hubs/erp/ledger-accounts ', () => {
+    let ledgerAccountId;
+    return cloud.get(test.api)
+      .then(r => ledgerAccountId = r.body[0].internalId)
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 5 } }).get(`${test.api}`))
+      .then(r => cloud.withOptions({ qs: { where: `internalId ='${ledgerAccountId}'` } }).get(`${test.api}`))
+      .then(r => cloud.get(`${test.api}/${ledgerAccountId}`));
+  });
 });
