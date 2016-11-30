64703aa9f819c937cd71ad5a828771ffaf6414db
Luke Vance <lukevance90@gmail.com>
2016-11-29 22:48:42 -0700

    cleaned up for linter

--- src/test/elements/gooddata/users.js
+++ src/test/elements/gooddata/users.js
@@ -2,21 +2,10 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/users');
-const cloud = require('core/cloud');
 
 suite.forElement('db', 'users', { payload: payload }, (test) => {
-
   test.withOptions({skip:true}).should.supportCruds();
   // get authenticated user info
   test.withApi(`${test.api}/{userId}`).should.return200OnGet();
-  // test.should.supportSr();
-  // it('should allow PATCH for ' + test.api, () => {
-  //   let userBody = {
-  //     "firstName": 'Jack',
-  //     "lastName": 'Churro Master'
-  //   };
-  //   return cloud.patch(`${test.api}/{userId}`, userBody);
-  // });
-
 
 });
