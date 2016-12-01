81a3c6da142c2e53cfc83bdbc1454462c5f0c7a4
Tyler Toth <tyler@cloud-elements.com>
2016-12-01 12:05:36 -0600

    Updated typeform formatting

--- src/test/elements/typeform/designs.js
+++ src/test/elements/typeform/designs.js
@@ -5,10 +5,10 @@ const payload = require('./assets/designs');
 const cloud = require('core/cloud');
 
 suite.forElement('general', 'designs', { payload: payload }, (test) => {
-    it(`should allow CR for /hubs/general/desings`, () => {
+  it(`should allow CR for /hubs/general/desings`, () => {
     let designId;
     return cloud.post(`${test.api}`, payload)
       .then(r => designId = r.body.id)
       .then(r => cloud.get(`${test.api}/${designId}`));
-    });
+  });
 });
--- src/test/elements/typeform/images.js
+++ src/test/elements/typeform/images.js
@@ -5,10 +5,10 @@ const payload = require('./assets/images');
 const cloud = require('core/cloud');
 
 suite.forElement('general', 'images', { payload: payload }, (test) => {
-    it(`should allow CR for /hubs/general/images`, () => {
+  it(`should allow CR for /hubs/general/images`, () => {
     let imageId;
     return cloud.post(`${test.api}`, payload)
       .then(r => imageId = r.body.id)
       .then(r => cloud.get(`${test.api}/${imageId}`));
-    });
+  });
 });
