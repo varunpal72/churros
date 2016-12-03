5d0c947c879ad600c1ae21690d4ca1a30d01f779
Tyler Toth <tyler@cloud-elements.com>
2016-12-02 18:39:46 -0600

    corrected naming

--- src/test/elements/epages/contacts.js
+++ src/test/elements/epages/contacts.js
@@ -6,7 +6,7 @@ const cloud = require('core/cloud');
 const payload = require('./assets/contacts');
 
 suite.forElement('ecommerce', 'contacts', { payload: payload }, (test) => {
-  it(`should allow RU for ${test.api}`, () => {
+  it(`should allow SU for ${test.api}`, () => {
     return cloud.get(test.api)
       .then(r => cloud.withOptions({ qs: { locale: 'en_US' } }).put(test.api, payload));
   });
