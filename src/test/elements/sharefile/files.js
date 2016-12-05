ff34f23f1cf2351134ace7d156fce081d7e92d2a
Martin Brandt <martinfbrandt@gmail.com>
2016-12-05 15:43:58 -0700

    modifications to Sharefile files and folders to make the tests more reproducible.

--- src/test/elements/sharefile/files.js
+++ src/test/elements/sharefile/files.js
@@ -7,33 +7,40 @@ const rootFolder = '/My Files & Folders';
 suite.forElement('documents', 'files', (test) => {
   let query = { path: rootFolder + `/file-${tools.random()}.txt` };
   let metadataPatch = { path: rootFolder + `/file-${tools.random()}.txt` };
-
+  console.log('query: ', query, 'metadatapatch: ',metadataPatch);
   let fileId;
   let copyId1;
   let copyPath = { path: rootFolder + `/churros-${tools.random()}.txt`};
 
-  it('Testing file uploading/downloading/metadata', () => {
+  it('Testing file uploading/getting/deleting', () => {
     let path = __dirname + '/assets/file.txt';
     return cloud.postFile('/hubs/documents/files', path, { qs:query})
     .then(r => fileId = r.body.id)
     .then(r => cloud.get('/hubs/documents/files'+ '/' + fileId))
-    .then(r => cloud.get('/hubs/documents/files'+ '/' + fileId + '/metadata'))
-    .then(r => cloud.patch('hubs/documents/files/' + fileId + '/metadata', metadataPatch))
-    .then(r => cloud.post('/hubs/documents/files'+ '/' + fileId + '/copy', copyPath))
-    .then(r => copyId1 = r.body.id);
+    .then(r => cloud.get('/hubs/documents/files'+ '/' + fileId + '/metadata'));
+    cloud.withOptions({ qs: query }).withApi('/hubs/documents/files').should.return200OnGet();
+    cloud.withOptions({qs: query}).delete('/hubs/documents/files');
 
   });
-  test.withOptions({qs:metadataPatch}).withApi('/hubs/documents/files').should.return200OnGet();
-//  test.withOptions({qs:query}).withApi('/hubs/documents/files/metadata').should.return200OnGet();
+  it('Testing file metadataing', () => {
+    let path = __dirname + '/assets/file.txt';
+    return cloud.postFile('/hubs/documents/files', path, { qs:query})
+    .then(r => fileId = r.body.id)
+    .then(r => cloud.patch('hubs/documents/files/' + fileId + '/metadata', metadataPatch));
+    cloud.withOptions({qs: query}).delete('/hubs/documents/files');
 
-/*  THESE ARE UNRELIABLE AND SOMETIMES THROW 500s
+  });
+
   it('Testing file copy and delete', () => {
-    return cloud.get('/hubs/documents/files/' + copyId1 + '/metadata')
-    .then(r =>   cloud.withOptions({ qs: {path : r.body['path'] } }).post('/hubs/documents/files/copy', {"path":"/My Files & Folders"}))
-    .then(r => copyPath = r.body['path'])
-    .then(r => cloud.withOptions({qs: {path: copyPath}}).delete('/hubs/documents/files'));
-  })
-  */
+    let path = __dirname + '/assets/file.txt';
+    return cloud.postFile('/hubs/documents/files', path, { qs:query})
+    .then(r => fileId = r.body.id)
+    .then(r => cloud.get('/hubs/documents/files'+ '/' + fileId + '/metadata'))
+    .then(r => cloud.withOptions({ qs: {path : r.body.path } }).post('/hubs/documents/files/copy', copyPath))
+    .then(r => copyPath = r.body.path);
+    cloud.withOptions({qs: {path: copyPath}}).delete('/hubs/documents/files');
+  });
+
 
 
   it('Testing file deleting', () => {
@@ -41,8 +48,7 @@ suite.forElement('documents', 'files', (test) => {
     return cloud.postFile('/hubs/documents/files', path, { qs:query})
     .then(r => fileId = r.body.id)
     .then(r => cloud.delete('/hubs/documents/files/' + fileId))
-    .then(r => cloud.withOptions({qs:metadataPatch}).delete('/hubs/documents/files'))
-    .then(r => cloud.withOptions({qs:copyPath}).delete('/hubs/documents/files'));
+
 
 
 
