0413d162ab12b87d760d7b87eb22978a2088e69c
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2016-12-05 14:37:54 +0530

    Added Churros for Salesforce

--- src/test/elements/sfdc/contacts.js
+++ src/test/elements/sfdc/contacts.js
@@ -4,17 +4,47 @@ const suite = require('core/suite');
 const payload = require('./assets/contacts');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
-
+const activities = require('./assets/activities');
+const notes = require('./assets/notes');
+const tasksUpdate = require('./assets/tasksUpdate');
+const tasks = require('./assets/tasks');
 const contact = () => ({
   FirstName: 'Conan',
   LastName: 'Barbarian',
   Email: tools.randomEmail()
 });
-
-
 suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportCeqlSearch('id');
+
+  it('should allow CRUDS for /hubs/crm/contacts', () => {
+    let contactId;
+    let activityId;
+    let noteId;
+    let taskId;
+    return cloud.post(`${test.api}`, payload)
+        .then(r => contactId = r.body.id)
+        .then(r => cloud.get(`${test.api}/${contactId}`))
+        .then(r => cloud.get(`${test.api}`))
+        .then(r => cloud.patch(`${test.api}/${contactId}`, payload))
+        .then(r => cloud.get(`${test.api}/${contactId}/activities`))
+        .then(r => cloud.post(`${test.api}/${contactId}/activities`,activities))
+        .then(r => activityId = r.body.Id)
+        .then(r => cloud.get(`${test.api}/${contactId}/activities/${activityId}`))
+        .then(r => cloud.patch(`${test.api}/${contactId}/activities/${activityId}`,activities))
+        .then(r => cloud.delete(`${test.api}/${contactId}/activities/${activityId}`))
+        .then(r => cloud.get(`${test.api}/${contactId}/notes`))
+        .then(r => cloud.post(`${test.api}/${contactId}/notes`,notes))
+        .then(r => noteId = r.body.Id)
+        .then(r => cloud.get(`${test.api}/${contactId}/notes/${noteId}`))
+        .then(r => cloud.delete(`${test.api}/${contactId}/notes/${noteId}`))
+        .then(r => cloud.post(`${test.api}/${contactId}/tasks`,tasks))
+        .then(r => taskId = r.body.Id)
+        .then(r => cloud.get(`${test.api}/${contactId}/tasks/${taskId}`))
+        .then(r => cloud.patch(`${test.api}/${contactId}/tasks/${taskId}`,tasksUpdate))
+        .then(r => cloud.delete(`${test.api}/${contactId}/tasks/${taskId}`))
+        .then(r => cloud.delete(`${test.api}/${contactId}`));
+  });
 
   it.skip('should allow CRUDS for /contacts/:id/attachments', () => {
     let contactId;
