fca6ee9abfe0ba6104880a0fa386d5dfb0eb0f89
root <hanumant.shinde@gslab.com>
2016-12-05 05:17:31 +0000

    formatting files

--- src/test/elements/sfdcdocuments/folders.js
+++ src/test/elements/sfdcdocuments/folders.js
@@ -4,7 +4,7 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('documents', 'folders', null, (test) => {
-    it('should allow GET for hubs/documents/folders  by path', () => {
-    return cloud.withOptions({ qs: { path:`/` } }).get(`${test.api}/contents`);
-    });
+  it('should allow GET for hubs/documents/folders  by path', () => {
+    return cloud.withOptions({ qs: { path: `/` } }).get(`${test.api}/contents`);
+  });
 });
