78cd9523fd7d0afdfad0d1fac612f771e5c407c3
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2016-12-06 17:08:55 +0530

    Incorporated review comments and changed /events to /meetings

--- src/test/elements/readytalk/events.js
+++ src/test/elements/readytalk/events.js
@@ -3,15 +3,17 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
-suite.forElement('general', 'events', { payload: null}, (test) => {
-it('should allow get for events and attendees ', () => {
-    let eventId,attendeesId;
+suite.forElement('general', 'meetings', null, (test) => {
+it('should allow get for meetings and attendees ', () => {
+    let meetingId,attendeesId;
+    test.should.supportPagination();
+    test.should.supportCeqlSearch('id');
     return cloud.withOptions({ qs:{where : "status ='ENDED'"}}).get(`${test.api}`)
-      .then(r => eventId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${eventId}`))
-      .then(r => cloud.get(`${test.api}/${eventId}/attendees`))
+      .then(r => meetingId = r.body[0].id)
+      .then(r => cloud.get(`${test.api}/${meetingId}`))
+      .then(r => cloud.get(`${test.api}/${meetingId}/attendees`))
       .then(r => attendeesId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${eventId}/attendees/${attendeesId}/chats`))
-      .then(r => cloud.get(`${test.api}/${eventId}/attendees/${attendeesId}`));
- });  
+      .then(r => cloud.get(`${test.api}/${meetingId}/attendees/${attendeesId}/chats`))
+      .then(r => cloud.get(`${test.api}/${meetingId}/attendees/${attendeesId}`));
+ });
 });
