db09412989bb45a0b3cc9fec5b1c7213c09407c9
Mithila Garud <mithila.garud@gslab.com>
2016-12-07 16:30:41 +0530

    Removed commented script

--- src/test/elements/readytalk/meetings.js
+++ src/test/elements/readytalk/meetings.js
@@ -4,13 +4,8 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'meetings', null, (test) => {
-  // checkout functions available under test.should which provide a lot of pre-canned tests
-  //   more information here: https://github.com/cloud-elements/churros/blob/master/CONTRIBUTING.md#adding-tests-to-an-existing-suite
-  //test.should.supportPagination();
   test.should.supportSr();
   it('it should support POST a contact inside a list', () => {
     return cloud.get(`${test.api}?pageSize=5&page=1`);
-//    .then(r => id = r.body.id)
-//    .then(r => cloud.get(`${test.api}?pageSize=5&page=1`));
   });
 });
