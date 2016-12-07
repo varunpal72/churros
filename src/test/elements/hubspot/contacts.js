24a51112ed913c680a192e315c05a1df505c9706
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2016-12-07 13:43:53 +0530

    Incorporated review comments

--- src/test/elements/hubspot/contacts.js
+++ src/test/elements/hubspot/contacts.js
@@ -13,40 +13,30 @@ const options = {
     }
   }
 };
-const propertygroups = {
-    "displayName": "test_churros_1",
-    "displayOrder": 0,
-    "name": "test12"
-  };
-  const updatePropertygroups = {
-  "displayName": "test_churros1",
-  "displayOrder": 0,
-  "name": "test12"
-  };
-  const fieldsUpdate = {
-  "favoritedOrder": -1,
-  "hidden": false,
-  "mutableDefinitionNotDeletable": false,
-  "displayOrder": -1,
-  "description": "The country reported by a contact's IP address. This is automatically set by HubSpot and can be used for segmentation and reporting.",
-  "label": "IP Country",
-  "type": "string",
-  "readOnlyDefinition": false,
-  "formField": false,
-  "displayMode": "current_value",
-  "groupName": "conversioninformation",
-  "name": "chuors_temp_1",
-  "options": [],
-  "fieldType": "text",
-  "calculated": false,
-  "externalOptions": false,
-  "favorited": false
-};
 suite.forElement('marketing', 'contacts', { payload: payload }, (test) => {
   test.withOptions(options).should.supportCruds();
   test.should.supportPagination();
   it('should allow CRUD for hubs/marketing/contacts/properties', () => {
     let id;
+    const fieldsUpdate = {
+    "favoritedOrder": -1,
+    "hidden": false,
+    "mutableDefinitionNotDeletable": false,
+    "displayOrder": -1,
+    "description": "The country reported by a contact's IP address. This is automatically set by HubSpot and can be used for segmentation and reporting.",
+    "label": "IP Country",
+    "type": "string",
+    "readOnlyDefinition": false,
+    "formField": false,
+    "displayMode": "current_value",
+    "groupName": "conversioninformation",
+    "name": "chuors_temp_1",
+    "options": [],
+    "fieldType": "text",
+    "calculated": false,
+    "externalOptions": false,
+    "favorited": false
+  };
     return cloud.post(`${test.api}/properties`,propertiesPayload)
      .then(r => id = r.body.name)
      .then(r => cloud.get(`${test.api}/properties`))
@@ -56,6 +46,16 @@ suite.forElement('marketing', 'contacts', { payload: payload }, (test) => {
    });
   it('should allow CRUD for hubs/marketing/contacts/propertygroups', () => {
   let id;
+  const propertygroups = {
+      "displayName": "test_churros_1",
+      "displayOrder": 0,
+      "name": "test12"
+    };
+    const updatePropertygroups = {
+    "displayName": "test_churros1",
+    "displayOrder": 0,
+    "name": "test12"
+    };
   return cloud.post(`${test.api}/propertygroups`,propertygroups)
    .then(r => id = r.body.name)
    .then(r => cloud.get(`${test.api}/propertygroups`))
--- src/test/elements/hubspot/files.js
+++ src/test/elements/hubspot/files.js
@@ -1,22 +1,19 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/files');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
 
-const fieldsUpdate = () => ({
-  "name": "cloud_elements",
-  "folder_path":  "test_CE"
-});
-
-suite.forElement('marketing', 'files', { payload: payload }, (test) => {
-
+suite.forElement('marketing', 'files', null , (test) => {
 it('should allow CRUD for hubs/marketing/files', () => {
+  const fieldsUpdate = () => ({
+    "name": "cloud_elements",
+    "folder_path":  "test_CE"
+  });
    let path = __dirname + `/assets/test.txt`;
    let id;
    return cloud.withOptions({ qs: { path:`/${tools.random()}` } })
-   .postFile(`${test.api}`, path)
+   .postFile(test.api, path)
    .then(r => id = r.body.id)
    .then(r => cloud.get(`${test.api}/${id}`))
    .then(r => cloud.patch(`${test.api}/${id}`,fieldsUpdate()))
