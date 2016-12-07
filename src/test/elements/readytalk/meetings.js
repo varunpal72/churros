3d0ffc0a5ac81d3806252a3b1af92818fcdde0a8
Mithila Garud <mithila.garud@gslab.com>
2016-12-07 17:59:18 +0530

    incorporated review comments

--- src/test/elements/readytalk/meetings.js
+++ src/test/elements/readytalk/meetings.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'meetings', null, (test) => {
   test.should.supportSr();
-  it('it should support GET meetings with pagination', () => {
-    return cloud.get(`${test.api}?pageSize=5&page=1`);
-  });
+  test.withOptions({ qs: { page: 1, pageSize: 5}}).should.return200OnGet();
 });
