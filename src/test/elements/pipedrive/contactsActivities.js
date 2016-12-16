db5e55cf868420b810685d762c26f0a399f4ea4e
Madhuri Kadam <GS-1259@GSLAB.COM>
2016-12-16 18:38:00 +0530

    formatted the files

--- src/test/elements/pipedrive/contactsActivities.js
+++ src/test/elements/pipedrive/contactsActivities.js
@@ -4,37 +4,32 @@ const cloud = require('core/cloud');
 const payload = require('./assets/contacts');
 const tools = require('core/tools');
 
-
-suite.forElement('crm', 'contacts', { payload:payload}, (test) => {
-   let contactId,activitiesId; 
-   const updatePayload= {
-        "subject": tools.random()
+suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
+  let contactId, activitiesId;
+  const updatePayload = {
+    "subject": tools.random()
   };
-const activitiesPayload = {
-  "subject": tools.random(),
-  "type": "call",
-  "due_date": "2015-04-24",
-  "due_time": "12:30"
-};
-it('should support CRUDS for contacts/activities', () =>{
-  return cloud.post(test.api, payload)
-	.then(r => contactId = r.body.id)
-	.then(r => cloud.get(`${test.api}/${contactId}/activities`))
-        .then(r => cloud.post(`${test.api}/${contactId}/activities`,activitiesPayload))
-	.then(r => activitiesId=r.body.id)
-        .then(r => cloud.get(`${test.api}/${contactId}/activities/${activitiesId}`))
-        .then(r => cloud.put(`${test.api}/${contactId}/activities/${activitiesId}`,updatePayload))
-        .then(r => cloud.delete(`${test.api}/${contactId}/activities/${activitiesId}`));
+  const activitiesPayload = {
+    "subject": tools.random(),
+    "type": "call",
+    "due_date": "2015-04-24",
+    "due_time": "12:30"
+  };
+  it('should support CRUDS for contacts/activities', () => {
+    return cloud.post(test.api, payload)
+      .then(r => contactId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${contactId}/activities`))
+      .then(r => cloud.post(`${test.api}/${contactId}/activities`, activitiesPayload))
+      .then(r => activitiesId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${contactId}/activities/${activitiesId}`))
+      .then(r => cloud.put(`${test.api}/${contactId}/activities/${activitiesId}`, updatePayload))
+      .then(r => cloud.delete(`${test.api}/${contactId}/activities/${activitiesId}`));
+  });
 
+  it('should GET /contacts emails and mails', () => {
+    return cloud.get(`${test.api}/${contactId}/emails`)
+      .then(r => cloud.get(`${test.api}/${contactId}/mails`))
+      .then(r => cloud.delete(`${test.api}/${contactId}`));
 
+  });
 });
-
-it('should GET /contacts emails and mails', () =>{
-  return cloud.get(`${test.api}/${contactId}/emails`)
-	.then(r => cloud.get(`${test.api}/${contactId}/mails`))
-        .then(r => cloud.delete(`${test.api}/${contactId}`));
-
-});
-});
-
-
