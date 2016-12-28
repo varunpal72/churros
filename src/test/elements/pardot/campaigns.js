aa6e2e5dbbb0051cb84d2d292b47bab78de4980d
root <hanumant.shinde@gslab.com>
2016-12-28 08:29:00 +0000

    Updated churros test cases for pardot

--- src/test/elements/pardot/campaigns.js
+++ src/test/elements/pardot/campaigns.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const payload = require('./assets/campaigns');
+const contactsPayload = require('./assets/contacts');
 
 suite.forElement('marketing', 'campaigns', { payload: payload }, (test) => {
   test.should.supportCrus();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
   test.withName('should support searching campaigns by created_after').withOptions({ qs: { where: 'created_after=\'2015-01-01\'' } }).should.return200OnGet();
+
+  it('should allow PUT for /hubs/marketing/campaigns/:id/contacts', () => {
+    let campaignId, contactId, contactCampaignPayload;
+    return cloud.get(test.api)
+      .then(r => campaignId = r.body[0].id)
+      .then(r => cloud.post(`/hubs/marketing/contacts`, contactsPayload))
+      .then(r => contactId = r.body.id)
+      .then(r => contactCampaignPayload = [{ "id": contactId }])
+      .then(r => cloud.put(`${test.api}/${campaignId}/contacts`, contactCampaignPayload))
+      .then(r => cloud.delete(`/hubs/marketing/contacts/${contactId}`));
+  });
 });
