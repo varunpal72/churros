006f3ce3adb42f4f1b1fad48c316d8cb9197fe51
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2016-12-28 17:45:48 +0530

    Incorporated review comments

--- src/test/elements/mysql/bulky.js
+++ src/test/elements/mysql/bulky.js
@@ -2,11 +2,13 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/bulky');
-const update = () => ({
-  "name": "test-churros"
-});
-
-const options = { churros: { updatePayload: update() } };
+const options = {
+  churros: {
+    updatePayload: {
+      "name": "test-churros"
+    }
+  }
+};
 suite.forElement('db', 'bulky', { payload: payload }, (test) => {
   test.withOptions(options).should.supportCruds();
   test.should.supportCeqlSearch('id');
