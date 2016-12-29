c59a2ced2199680d869a0ad6c38fcc4a0c0acef0
Bhargav Gandhi [BhargavGandhi-GSLab] <bhargav.gandhi@gslab.com>
2016-12-29 18:25:25 +0530

    CE-4082: Add missing cases for DocuSign * GET  /ping * GET  /templates * GET  /templates/{templateId} * GET  /envelopes/{envelopeId}/custom_fields (commented because there is a problem with Cloud Elements Hub API) * GET  /envelopes/{envelopeId}/documents/{documentId} * GET  /envelopes/{envelopeId}/documents/certificates * POST /envelopes/{envelopeId}/recipients * POST /envelopes/{envelopeId}/recipients/{recipientId}/tabs

--- src/test/elements/docusign/envelopes.js
+++ src/test/elements/docusign/envelopes.js
@@ -8,9 +8,11 @@ const updatePayload = require('./assets/envelopes.update.json');
 const schema = require('./assets/envelope.schema.json');
 const envelopesSchema = require('./assets/envelopes.schema.json');
 const documentsSchema = require('./assets/documents.schema.json');
+const recipient = require('./assets/envelopes.recipient.json');
+const tab = require('./assets/recipients.tab.json');
 
 suite.forElement('esignature', 'envelopes', (test) => {
-  it('should allow creating an envelope, retrieving the created envelope and updating the envelope status', () => {
+  it('should allow creating an envelope, retrieving the created envelope, updating the envelope status and retrieving the custom fields of the envelope', () => {
     let envelopeId = "-1";
     let path = __dirname + '/assets/MrRobotPdf.pdf';
 
@@ -19,18 +21,43 @@ suite.forElement('esignature', 'envelopes', (test) => {
     return cloud.withOptions(opts).postFile(test.api, path)
       .then(r => envelopeId = r.body.envelopeId)
       .then(r => cloud.get(test.api + '/' + envelopeId, (r) => expect(r).to.have.schemaAnd200(schema)))
-      .then(r => cloud.patch(test.api + '/' + envelopeId, updatePayload, (r) => expect(r).to.have.statusCode(200)));
+      .then(r => cloud.patch(test.api + '/' + envelopeId, updatePayload, (r) => expect(r).to.have.statusCode(200)))
+      //.then(r => cloud.get(`${test.api}/${envelopeId}/custom_fields`));
   });
 
   it('should allow creating an envelope, retrieving the created envelope\'s document(s)', () => {
     let envelopeId = "-1";
     let path = __dirname + '/assets/MrRobotPdf.pdf';
     const opts = { formData: { envelope: JSON.stringify(createPayload) } };
+    let documentId;
 
     return cloud.withOptions(opts).postFile(test.api, path)
       .then(r => envelopeId = r.body.envelopeId)
       .then(r => cloud.get(test.api + '/' + envelopeId + '/documents',
-        (r) => expect(r).to.have.schemaAnd200(documentsSchema)));
+        (r) => expect(r).to.have.schemaAnd200(documentsSchema)))
+      .then(r => documentId = r.body[0].documentId)
+      .then(r => cloud.get(`${test.api}/${envelopeId}/documents/${documentId}`));
+  });
+
+  it('should get all envelopes, find one which is sent and then find its certifiates', () => {
+    let envelopeId;
+
+    return cloud.withOptions({ qs: { where: 'from_date=\'2015-01-01T00:00:00Z\'' } }).get(test.api)
+      .then(r => envelopeId = r.body.filter(function(el) {
+        return el.status === 'sent';
+      })[0].envelopeId)
+      .then(r => cloud.get(`${test.api}/${envelopeId}/documents/certificates`));
+  });
+
+  it('should create recipients for envelopes and also tabs for recipients', () => {
+    let envelopeId = "-1";
+    let path = __dirname + '/assets/MrRobotPdf.pdf';
+    const opts = { formData: { envelope: JSON.stringify(createPayload) } };
+
+    return cloud.withOptions(opts).postFile(test.api, path)
+      .then(r => envelopeId = r.body.envelopeId)
+      .then(r => cloud.post(`${test.api}/${envelopeId}/recipients`, recipient))
+      .then(r => cloud.post(`${test.api}/${envelopeId}/recipients/${recipient.agents[0].recipientId}/tabs`, tab));
   });
 
   test.withValidation(envelopesSchema)
