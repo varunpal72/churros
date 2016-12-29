cb8fe0c60e08cbde47b546f17991b39a99bb1961
yugesh <yugesh.bhosale@gslab.com>
2016-12-29 12:14:07 +0530

    updated changes in box churros

--- src/test/elements/box/foldersCollaborations.js
+++ src/test/elements/box/foldersCollaborations.js
@@ -1,14 +1,15 @@
 'use strict';
+
 const cloud = require('core/cloud');
 const suite = require('core/suite');
 const folderPayload = require('./assets/folders');
 
 suite.forElement('documents', 'folders', { payload: folderPayload }, (test) => {
-  it('should allow CD for ${test.api} and GET collaborations', () => {
-    let folder;
+  it(`should allow CD for ${test.api} and GET collaborations`, () => {
+    let folderId;
     return cloud.post(test.api, folderPayload)
-      .then(r => folder = r.body)
-      .then(r => cloud.get(`${test.api}/${folder.id}/collaborations`))
-      .then(() => cloud.delete(`${test.api}/${folder.id}`));
+      .then(r => folderId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${folderId}/collaborations`))
+      .then(() => cloud.delete(`${test.api}/${folderId}`));
   });
 });
