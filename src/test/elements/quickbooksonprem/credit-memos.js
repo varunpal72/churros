af3527b56536d611493119aca897adb476a9c6ba
root <hanumant.shinde@gslab.com>
2017-01-05 14:48:43 +0000

    added tests for quickbooks enterprise

---
+++ src/test/elements/quickbooksonprem/credit-memos.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/credit-memos');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const updatePayload ={ "Subtotal":"25600.00" };
+
+suite.forElement('finance', 'credit-memos', { payload: payload}, (test) => {
+    it('should support CRUDS,Pagination for /hubs/finance/credit-memos ', () => {
+    let id;
+    return cloud.post(test.api, payload)
+      .then(r => id = r.body.TxnID)
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => updatePayload.EditSequence = r.body.EditSequence)
+      .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
+      .then(r => cloud.delete(`${test.api}/${id}`));      
+  });
+});
---
+++ src/test/elements/quickbooksonprem/item-receipts.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/item-receipts');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const updatePayload ={ "Memo": tools.random() };
+
+suite.forElement('finance', 'item-receipts', { payload: payload}, (test) => {
+    it('should support CRUDS,Pagination for /hubs/finance/item-receipts ', () => {
+    let id;
+    return cloud.post(test.api, payload)
+      .then(r => id = r.body.TxnID)
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
+      .then(r => cloud.withOptions({ qs: { where: `TxnID='${id}'` } }).get(test.api))
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => updatePayload.EditSequence = r.body.EditSequence)
+      .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
+      .then(r => cloud.delete(`${test.api}/${id}`));      
+  });
+});
