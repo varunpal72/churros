7a04620fa89253d1b816c0fecb6b4fd2d5abc124
Bhargav Gandhi [BhargavGandhi-GSLab] <bhargav.gandhi@gslab.com>
2017-01-05 15:13:47 +0530

    CE-4093: Autotask Helpdesk.
    Add missing APIs:
    
    GET   /incidents/{incidentId}/comments
    PATCH /incidents/{incidentId}/comments
    GET   /incidents/{incidentId}/comments/{commentId}
    PATCH /incidents/{incidentId}/comments/{commentId}

--- src/test/elements/autotaskhelpdesk/incidents.js
+++ src/test/elements/autotaskhelpdesk/incidents.js
@@ -7,6 +7,8 @@ const payload = require('./assets/incidents');
 const tools = require('core/tools');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const incidentPayload = build({ title: tools.random(), description: tools.random() });
+const comment = require('./assets/comments');
+const updatedComment = require('./assets/updatedComments');
 
 const updatePayload = {
   "title": tools.random(),
@@ -17,8 +19,8 @@ const updatePayload = {
 suite.forElement('helpdesk', 'incidents', { payload: incidentPayload }, (test) => {
   const build = (overrides) => Object.assign({}, incidentPayload, overrides);
   const payload = build({ title: tools.random(), description: tools.random() });
-  it(`should support paging, Ceql search and Crus for ${test.api} and attachments`, () => {
-    let incidentId, attachmentId;
+  it(`should support paging, Ceql search and Crus for ${test.api}, comments and attachments`, () => {
+    let incidentId, attachmentId, commentId;
     let query = { fileName: "attach.txt" };
     return cloud.post(test.api, payload)
       .then(r => incidentId = r.body.id)
@@ -26,6 +28,11 @@ suite.forElement('helpdesk', 'incidents', { payload: incidentPayload }, (test) =
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
       .then(r => cloud.withOptions({ qs: { where: 'priority = 1' } }).get(test.api))
       .then(r => cloud.patch(`${test.api}/${incidentId}`, updatePayload))
+      .then(r => cloud.put(`${test.api}/${incidentId}/comments`, comment))
+      .then(r => cloud.get(`${test.api}/${incidentId}/comments`))
+      .then(r => commentId = r.body[0].id)
+      .then(r => cloud.patch(`${test.api}/${incidentId}/comments/${commentId}`, updatedComment))
+      .then(r => cloud.get(`${test.api}/${incidentId}/comments/${commentId}`))
       .then(r => cloud.withOptions({ qs: query }).postFile(`${test.api}/${incidentId}/attachments`, __dirname + '/assets/attach.txt'))
       .then(r => cloud.get(`${test.api}/${incidentId}/attachments`))
       .then(r => cloud.withOptions({
