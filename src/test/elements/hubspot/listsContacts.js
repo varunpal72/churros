e01f80a18eaf6e6d4a8104f2ac2eb9efdc2e2987
Tyler Toth <tyler@cloud-elements.com>
2017-01-07 17:40:32 -0600

    Small updates

--- src/test/elements/hubspot/listsContacts.js
+++ src/test/elements/hubspot/listsContacts.js
@@ -3,16 +3,12 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
+const contactsApi = '/hubs/marketing/contacts';
 
 suite.forElement('marketing', 'lists', null, (test) => {
-  it('should create a contact then a list, and then add the contact to the list, get it, then remove it from list. Finally remove the list and contact', () => {
-    const contactsApi = '/hubs/marketing/contacts';
+  it(`should support CD for ${test.api} and ${contactsApi}, then CS ${test.api}/:listId/contacts`, () => {
     const email = tools.randomEmail();
-
-    let listId;
-    let contactId;
-    let updatedPayload;
-
+    let listId, contactId, updatedPayload;
     return cloud.post(test.api, { name: tools.random() })
       .then(r => listId = r.body.id)
       .then(r => cloud.post(contactsApi, { email: email, firstName: tools.random(), lastName: tools.random() }))
@@ -20,7 +16,7 @@ suite.forElement('marketing', 'lists', null, (test) => {
       .then(r => updatedPayload = [{ properties: { email: email }, vid: contactId }])
       .then(r => cloud.post(`${test.api}/${listId}/contacts`, updatedPayload))
       .then(r => cloud.get(`${test.api}/${listId}/contacts`))
-      //.then(r => cloud.delete(`${test.api}/${listId}/contacts/${contactId}`))
+      //.then(r => cloud.delete(`${test.api}/${listId}/contacts/${contactId}`)) comment b/c error in hubspot ZH #4138
       .then(r => cloud.delete(`${test.api}/${listId}`))
       .then(r => cloud.delete(`${contactsApi}/${contactId}`));
   });
