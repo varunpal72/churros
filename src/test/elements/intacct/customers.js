e9acde260de26ad106469bbba836ff15d592f2ee
Tyler Toth <tyler@cloud-elements.com>
2017-01-09 15:48:52 -0600

    adding skips and formatting

--- src/test/elements/intacct/customers.js
+++ src/test/elements/intacct/customers.js
@@ -2,7 +2,8 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/customers');
-suite.forElement('finance', 'customers',{ payload: payload }, (test) => {
+suite.forElement('finance', 'customers', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
+  test.withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' }, skip: true }).should.return200OnGet();
 });
