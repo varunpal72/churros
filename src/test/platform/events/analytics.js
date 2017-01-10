d2c7a0ea9fa6a65dccfb0005ab13be0b2254fc52
Travis McChesney <travis@travismcchesney.com>
2017-01-10 16:52:09 -0700

    New event analytics tests.

--- src/test/platform/events/analytics.js
+++ src/test/platform/events/analytics.js
@@ -10,6 +10,7 @@ const provisioner = require('core/provisioner');
 const suite = require('core/suite');
 const analyticsSchema = require('./assets/analytics.schema.json');
 const instanceAnalyticsSchema = require('./assets/instanceAnalytics.schema.json');
+const accountAnalyticsSchema = require('./assets/accountAnalytics.schema.json');
 
 const signatureKey = '1234efgh5678dcba';
 
@@ -34,7 +35,7 @@ suite.forPlatform('events', { name: 'event analytics' }, (test) => {
         .then(() => {
         })
         // Wait up to 10 seconds to see if we've gotten all events in the analytics results.
-        .then(r => tools.wait.upTo(10000).for(() => {
+        .then(r => tools.wait.upTo(90000).for(() => {
           const from = new Date();
           from.setHours(from.getHours() - 1);
           const to = new Date();
@@ -60,6 +61,11 @@ suite.forPlatform('events', { name: 'event analytics' }, (test) => {
         .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']));
     });
 
+    it('should return an error for an invalid account ID for event analytics', () =>
+      cloud.get(`/instances/events/analytics?accountIds[]=0`, r => expect(r).to.have.statusCode(401))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']))
+    );
+
     it('should return instance analytics for x events for an element instance', () => {
       const payload = common.loadEventRequest(element);
       const load = props.getForKey('events', 'load');
@@ -71,7 +77,7 @@ suite.forPlatform('events', { name: 'event analytics' }, (test) => {
         .then(() => {
         })
         // Wait up to 10 seconds to see if we've gotten all events in the instance analytics results.
-        .then(r => tools.wait.upTo(10000).for(() => {
+        .then(r => tools.wait.upTo(90000).for(() => {
           const from = new Date();
           from.setHours(from.getHours() - 1);
           const to = new Date();
@@ -80,8 +86,77 @@ suite.forPlatform('events', { name: 'event analytics' }, (test) => {
               expect(r.body).to.have.length(61) &&
                 r.body.map(s => expect(s).to.have.contain.keys(['records', 'total', 'timestamp'])) &&
                 expect(r.body.reduce((accum, curr) =>
-                  accum + curr.records[0] ? curr.records.reduce((a, c) => a + c.count, 0) : 0, 0)).to.be.at.least(3)
+                  accum + curr.records[0] ? curr.records.reduce((a, c) => a + c.count, 0) : 0, 0)).to.be.at.least(load)
             );
         }));
     });
+
+    it('should return an error for an invalid date range for event instance analytics', () => {
+      const from = new Date();
+      from.setHours(from.getHours() - 1);
+      const to = new Date();
+      to.setHours(to.getHours() - 2);
+      return cloud.get(`/instances/events/analytics/instances?from=${from.toJSON()}&to=${to.toJSON()}&interval=minute`, r => expect(r).to.have.statusCode(400))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']));
+    });
+
+    it('should return an error for an invalid interval for instance analytics', () => {
+      const from = new Date();
+      from.setHours(from.getHours() - 24);
+      const to = new Date();
+      return cloud.get(`/instances/events/analytics/instances?from=${from.toJSON()}&to=${to.toJSON()}&interval=second`, r => expect(r).to.have.statusCode(400))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']));
+    });
+
+    it('should return an error for an invalid account ID for instance analytics', () =>
+      cloud.get(`/instances/events/analytics/instances?accountIds[]=0`, r => expect(r).to.have.statusCode(401))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']))
+    );
+
+    it('should return account analytics for x events for an element instance', () => {
+      const payload = common.loadEventRequest(element);
+      const load = props.getForKey('events', 'load');
+
+      return cloud.createEvents(element, { '<elementInstanceId>': instanceId }, payload, load)
+        .then(sentEvents => sentEvents.filter(event => event.error).length)
+        .then(f => { if (f > 0) logger.warn("Failed to POST %d events", f); })
+        .then(() => tools.wait.upTo(10000).for(() => cloud.get(`instances/events/analytics/accounts`, accountAnalyticsSchema)))
+        .then(() => {
+        })
+        // Wait up to 10 seconds to see if we've gotten all events in the account analytics results.
+        .then(r => tools.wait.upTo(90000).for(() => {
+          const from = new Date();
+          from.setHours(from.getHours() - 1);
+          const to = new Date();
+          return cloud.get(`/instances/events/analytics/accounts?from=${from.toJSON()}&to=${to.toJSON()}&interval=minute`)
+            .then(r =>
+              expect(r.body).to.have.length(61) &&
+                r.body.map(s => expect(s).to.have.contain.keys(['records', 'total', 'timestamp'])) &&
+                expect(r.body.reduce((accum, curr) =>
+                  accum + curr.records[0] ? curr.records.reduce((a, c) => a + c.count, 0) : 0, 0)).to.be.at.least(load)
+            );
+        }));
+    });
+
+    it('should return an error for an invalid date range for event account analytics', () => {
+      const from = new Date();
+      from.setHours(from.getHours() - 1);
+      const to = new Date();
+      to.setHours(to.getHours() - 2);
+      return cloud.get(`/instances/events/analytics/accounts?from=${from.toJSON()}&to=${to.toJSON()}&interval=minute`, r => expect(r).to.have.statusCode(400))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']));
+    });
+
+    it('should return an error for an invalid interval for account analytics', () => {
+      const from = new Date();
+      from.setHours(from.getHours() - 24);
+      const to = new Date();
+      return cloud.get(`/instances/events/analytics/accounts?from=${from.toJSON()}&to=${to.toJSON()}&interval=second`, r => expect(r).to.have.statusCode(400))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']));
+    });
+
+    it('should return an error for an invalid account ID for account analytics', () =>
+      cloud.get(`/instances/events/analytics/accounts?accountIds[]=0`, r => expect(r).to.have.statusCode(401))
+        .then(r => expect(r.body).to.contain.all.keys(['requestId', 'message']))
+    );
 });
