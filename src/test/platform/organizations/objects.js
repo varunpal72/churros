d1686abb3ce1bfa0e7b16e3bc6a9b6ef5d353820
cassiedbrown <cassie@cloud-elements.com>
2017-01-11 11:11:39 -0700

    new test for delete /organizations/objects/definitions
    After discussion and help with Josh, adjusted test to be self contained
    and to check for 409 when running the delete
    /organizations/objects/definitions if a transformation exists.

--- src/test/platform/organizations/objects.js
+++ src/test/platform/organizations/objects.js
@@ -21,6 +21,46 @@ const objects = {
   }
 };
 
+const transformation = {
+  "ChurrosOrgObjectTestContact1235678": {
+    "level": "organization",
+    "vendorName": "Contact",
+    "startDate": "2017-01-03 18:15:08.496059",
+    "fields": [
+      {
+        "path": "MyID",
+        "vendorPath": "AccountId"
+      },
+      {
+        "path": "MyEmail",
+        "vendorPath": "Email"
+      },
+      {
+        "path": "MyFirstName",
+        "vendorPath": "FirstName"
+      },
+      {
+        "path": "MyLastName",
+        "vendorPath": "LastName"
+      },
+      {
+        "path": "MyPhone",
+        "vendorPath": "Phone"
+      }
+    ],
+    "configuration": [
+      {
+        "type": "passThrough",
+        "properties": {
+          "fromVendor": false,
+          "toVendor": false
+        }
+      }
+    ],
+    "isLegacy": false
+  }
+};
+
 
 suite.forPlatform('organizations/objects/definitions', { payload: objects }, (test) => {
   /** * Will delete all object definitions that are definied in the "objects" object above */
@@ -33,13 +73,21 @@ suite.forPlatform('organizations/objects/definitions', { payload: objects }, (te
   /** * Safety first... */
   before(() => deleteEm());
 
-  it('Should support creating object definitions', () => {
+  it('Should throw a 409 when associated transformations exist on one or more object definitions', () => {
     return cloud.post('/organizations/objects/definitions', objects)
-      // create a transformation that relies on one of the object definitions above
-      // try to delete that object definition and validate 409
-      // delete transformation
-      // try to delete that object definition (just that one) and validate 200
-      // celebrate
+      //create a transformation
+      .then(r => cloud.post(`/organizations/elements/sfdc/transformations/ChurrosOrgObjectTestContact1235678`,transformation,true))
+      //run the delete test for organizations/objects/definitions, return false if anything but 409 is returned
+      .then(() => cloud.delete(test.api, (r) => {
+        if(r.statusCode===409){
+          return true;
+        } else {
+          return false;
+        }
+      }))
+      //delete the created transformation
+      .then(() => cloud.delete(`/organizations/elements/sfdc/transformations/ChurrosOrgObjectTestContact1235678`,true))
+      //clean up the created objects
       .then(() => deleteEm());
   });
 });
