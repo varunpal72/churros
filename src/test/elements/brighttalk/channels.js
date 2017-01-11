623698e79fe791d637e7e63240217dfc08f7fbbf
Tyler Toth <tyler@cloud-elements.com>
2017-01-10 20:07:52 -0600

    formatting updates

--- src/test/elements/brighttalk/channels.js
+++ src/test/elements/brighttalk/channels.js
@@ -3,40 +3,40 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
-suite.forElement('marketing', 'channels', null , (test) => {
-    let channelId;
-    it('should support GET,Pagination and CeqlSearch for /hubs/marketing/channels ', () => {
+suite.forElement('marketing', 'channels', null, (test) => {
+  let channelId;
+  it(`should support GET, pagination and Ceql search for ${test.api}`, () => {
     return cloud.get(test.api)
       .then(r => channelId = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
       .then(r => cloud.withOptions({ qs: { where: `id='${channelId}'` } }).get(test.api));
-    });
+  });
 
-    it('should support GET,Pagination and CeqlSearch for /hubs/marketing/channels/:channelId/activities ', () => {
+  it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/activities`, () => {
     let id;
     return cloud.get(`${test.api}/${channelId}/activities`)
       .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/activities`))
       .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/activities`));
-    });
+  });
 
-    it('should support GET,Pagination and CeqlSearch for /hubs/marketing/channels/:channelId/contacts ', () => {
+  it('should support GET, pagination and Ceql search for /hubs/marketing/channels/:channelId/contacts', () => {
     let id;
     return cloud.get(`${test.api}/${channelId}/contacts`)
       .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/contacts`))
       .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/contacts`));
-    });
+  });
 
-    it('should support GET,Pagination and CeqlSearch for /hubs/marketing/channels/:channelId/subscriberWebcastAttachmentAccessSummaries ', () => {
+  it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/subscriberWebcastAttachmentAccessSummaries`, () => {
     let id;
     return cloud.get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`)
       .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`))
       .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`${test.api}/${channelId}/subscriberWebcastAttachmentAccessSummaries`));
-    });
+  });
 
-    it('should support GET,Pagination and CeqlSearch for /hubs/marketing/channels/:channelId/surveys  and /hubs/marketing/surveys/:surveyId/responses ', () => {
+  it(`should support GET, pagination and Ceql search for ${test.api}/:channelId/surveys  and /hubs/marketing/surveys/:surveyId/responses`, () => {
     let surveyId, id;
     return cloud.get(`${test.api}/${channelId}/surveys`)
       .then(r => surveyId = r.body[0].id)
@@ -46,5 +46,5 @@ suite.forElement('marketing', 'channels', null , (test) => {
       .then(r => id = r.body[0].id)
       .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(`/hubs/marketing/surveys/${surveyId}/responses`))
       .then(r => cloud.withOptions({ qs: { where: `id='${id}'` } }).get(`/hubs/marketing/surveys/${surveyId}/responses`));
-    });
+  });
 });
