67ded8dd2fd5ca992afe1097568234cb77de48ab
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2017-01-13 18:48:19 +0530

    removed skip:true for subscriptiontypes API

--- src/test/elements/acton/subscriptiontypes.js
+++ src/test/elements/acton/subscriptiontypes.js
@@ -2,7 +2,7 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-  suite.forElement('marketing', 'subscription-types', {skip: true}, (test) => {
+  suite.forElement('marketing', 'subscription-types', null , (test) => {
     it('it should support Adding,Removing records from a subscription type by email address', () => {
        return cloud.get(test.api)
        .then(r => cloud.patch(`${test.api}/Newsletter/optin/test%40gmail.com`))
