00ab579a2a705e6708177f7d9b7ed7c4e4db5016
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2017-01-13 11:46:15 +0530

    Added churros for ping and products APIs

--- src/test/elements/servicemax/products.js
+++ src/test/elements/servicemax/products.js
@@ -6,20 +6,15 @@ const tools = require('core/tools');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const productsPayload = build({ Name: tools.random() });
 
-
 suite.forElement('fsa', 'products', { payload: productsPayload }, (test) => {
-
-
-        const options = {
-          churros: {
-            updatePayload: {
-              "Name": tools.random()
-            }
-          }
-        };
-        test.withOptions(options).should.supportCruds();
-        test.should.supportPagination();
-        test.should.supportCeqlSearch('id');
-
-
-  });
+  const options = {
+    churros: {
+      updatePayload: {
+        "Name": tools.random()
+      }
+    }
+  };
+  test.withOptions(options).should.supportCruds();
+  test.should.supportPagination();
+  test.should.supportCeqlSearch('id');
+});
