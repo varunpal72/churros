051f1bef9857aeb494884485ce9a04e5e6456bcd
Tyler Toth <tyler@cloud-elements.com>
2017-01-13 18:35:10 -0600

    small formatting updates

--- src/test/elements/freshbooks/customers.js
+++ src/test/elements/freshbooks/customers.js
@@ -6,5 +6,5 @@ const payload = require('./assets/customers');
 suite.forElement('finance', 'customers', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
-  test.withOptions({ qs: { where: 'email = \'claude@cloud-elements.com\'' } }).should.return200OnGet();
+  test.should.supportCeqlSearchForMultipleRecords('email');
 });
--- src/test/elements/freshbooks/hooks.js
+++ src/test/elements/freshbooks/hooks.js
@@ -5,5 +5,4 @@ const suite = require('core/suite');
 suite.forElement('finance', 'hooks', { payload: null }, (test) => {
   test.should.return200OnGet();
   test.should.supportPagination();
-
 });
