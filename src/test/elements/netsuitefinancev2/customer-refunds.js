b4e37185c3c911df7b5ce52ebba5e894f269dd36
yugesh <yugesh.bhosale@gslab.com>
2017-01-18 13:54:22 +0530

    Added churros for search hubs/finance/customer-refunds

--- src/test/elements/netsuitefinancev2/customer-refunds.js
+++ src/test/elements/netsuitefinancev2/customer-refunds.js
@@ -13,5 +13,5 @@ suite.forElement('finance', 'customer-refunds', { payload: payload }, (test) => 
   };
   test.withOptions(options).should.supportCrus();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
-  //test.withOptions({ qs: { where: 'lastModifiedDate >= 2016-08-05T09:35:38Z' } }).should.return200OnGet();
+  test.withOptions({ qs: { where: '\'lastModifiedDate\' >= \'2016-08-05T09:35:38Z\'' } }).should.return200OnGet();
 });
