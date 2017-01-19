70be308b635fd045671d02aa88a953dec9498b61
root <shital.patil@gslab.com>
2017-01-18 23:01:24 -0800

    Added churros for POST /jobss and GET /jobss:id for jobs

--- src/test/elements/sailthru/jobs.js
+++ src/test/elements/sailthru/jobs.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const payload = require('./assets/jobs');
 
-suite.forElement('marketing', 'jobs', { skip: true, payload: payload }, (test) => {
-  test.should.return200OnPost();
+suite.forElement('marketing', 'jobs', { payload: payload }, (test) => {
+
+  it('should support POST /jobss and GET /jobss:id for jobs', () => {
+    let jobId;
+    return cloud.post(test.api, payload)
+      .then(r => jobId = r.body.job_id)
+      .then(r => cloud.get(`${test.api}/${jobId}`))
+  });
 });
