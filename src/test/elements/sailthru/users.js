88e8cb5c8aee46a7934114cf59bb99ddc14c9338
root <shital.patil@gslab.com>
2017-01-19 01:08:55 -0800

    Added churros for jobs,users and ping for sailthru

--- src/test/elements/sailthru/users.js
+++ src/test/elements/sailthru/users.js
@@ -2,7 +2,23 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
+const userPayload = require('./assets/users');
 
-suite.forElement('marketing', 'users', { skip: true, payload: {'email': tools.randomEmail()}}, (test) => {
-  test.should.return200OnPost();
+suite.forElement('marketing', 'users', { payload: userPayload }, (test) => {
+  const build = (overrides) => Object.assign({}, userPayload, overrides);
+  const payload = build({ email: tools.randomEmail() });
+
+  const updatePayload = {
+    "email": "xyz@yahoo.com"
+  };
+
+  it('should support CRUS for users', () => {
+    let userSid;
+    return cloud.post(test.api, payload)
+      .then(r => userSid = r.body.keys.sid)
+      .then(r => cloud.withOptions({ qs: { where: `sid= \'${userSid}\'` } }).get(`${test.api}`))
+      .then(r => cloud.get(`${test.api}/${userSid}`))
+      .then(r => cloud.patch(`${test.api}/${userSid}`, updatePayload));
+  });
 });
