3e16914817a613b631f62bf0b48d85226c587bbb
Martin Brandt <martinfbrandt@gmail.com>
2017-01-19 10:29:11 -0700

    added Pipedrive test to confirm filter was deleted after created

--- src/test/elements/pipedrive/opportunities.js
+++ src/test/elements/pipedrive/opportunities.js
@@ -4,10 +4,25 @@ const suite = require('core/suite');
 const chakram = require('chakram');
 const payload = require('./assets/opportunities');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
 const build = (overrides) => Object.assign({}, payload, overrides);
+const expect = chakram.expect;
 const opportunitiesPayload = build({ title: tools.random(), value: tools.randomInt() });
 
+
+
 suite.forElement('crm', 'opportunities', { payload: opportunitiesPayload }, (test) => {
+  let elements_total_count;
+
+
+    it('should allow GET of Pipedrive filters', () => {
+        return cloud.get('/hubs/crm/filters')
+        .then(r => elements_total_count = r.response.headers['elements-returned-count']);
+
+    });
+
+
+
   const options = {
     churros: {
       updatePayload: {
@@ -19,4 +34,13 @@ suite.forElement('crm', 'opportunities', { payload: opportunitiesPayload }, (tes
   test.withOptions(options).should.supportCruds(chakram.put);
   test.should.supportPagination();
   test.withOptions({ qs: { where: 'title = \'Demo Deal NEW\'' } }).should.return200OnGet();
+
+  it('Count of Pipedrive filters should remain the same', () => {
+      return cloud.get('/hubs/crm/filters')
+      .then(r => expect(r.response.headers['elements-returned-count']).to.equal(elements_total_count));
+
+
+  });
+
+
 });
