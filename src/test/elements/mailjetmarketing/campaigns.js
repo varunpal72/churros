5fa4d67a8963792d0615de2b473d6cbd02149e39
BhushanWadichar-GsLab <bhushan.wadichar@gslab.com>
2017-01-20 15:55:33 +0530

    Updated campaigns.js file of mailjetmarketing

--- src/test/elements/mailjetmarketing/campaigns.js
+++ src/test/elements/mailjetmarketing/campaigns.js
@@ -6,15 +6,13 @@ const payload = require('./assets/campaigns');
 
 suite.forElement('marketing', 'campaigns', { payload: payload }, (test) => {
 
-  test.should.supportPagination();
-
   it('should support SRU for ' + test.api, () => {
     let campaignId;
     return cloud.get(test.api)
       .then(r => campaignId = r.body[0].ID)
+			.then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
+			.then(r => cloud.withOptions({ qs: { where: `updated_from ='2017-01-19'` } }).get(test.api))
       .then(r => cloud.get(`${test.api}/${campaignId}`))
       .then(r => cloud.patch(`/hubs/marketing/campaigns/1`, payload));
   });
-
-  test.withOptions({ qs: { where: `updated_from ='2017-01-19'` } }).should.return200OnGet();
 });
