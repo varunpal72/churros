6efc90f89c8e099ceb4322c74a33a2b26c6863ff
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-01-20 12:50:57 +0000

    Updated churros as per element changed

---
+++ src/test/elements/ciscospark/channels.js
@@ -0,0 +1,16 @@
+'use strict';
+const chakram = require('chakram');
+const suite = require('core/suite');
+const payload = require('./assets/channels');
+
+suite.forElement('collaboration', 'channels', { payload: payload }, (test) => {
+   const opts = {
+    churros: {
+      updatePayload: {
+        title: 'Churros Test Update'
+      }
+    }
+  };
+  test.withOptions(opts).should.supportCruds(chakram.put);
+
+});
---
+++ src/test/elements/ciscospark/users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('collaboration', 'users',null, (test) => {
+   
+ it(`should allow and SR for ${test.api} and GET account`, () => {
+    let userId,userEmail;
+    return cloud.get(`/hubs/collaboration/account`)
+      .then(r => userId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${userId}`))
+      .then(r =>  userEmail = r.body.emails[0])
+      .then(r => cloud.withOptions({ qs: { where :`email= '${userEmail}'`} }).get(test.api));
+  });
+});
