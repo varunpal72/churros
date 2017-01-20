975f2b48cd54113d75c62d01e2561fcd03c4c92c
root <shital.patil@gslab.com>
2017-01-19 22:21:10 -0800

    Added transformation for job_id

--- src/test/elements/sailthru/jobs.js
+++ src/test/elements/sailthru/jobs.js
@@ -9,7 +9,7 @@ suite.forElement('marketing', 'jobs', { payload: payload }, (test) => {
   it('should support POST /jobs and GET /jobs:id for jobs', () => {
     let jobId;
     return cloud.post(test.api, payload)
-      .then(r => jobId = r.body.job_id)
+      .then(r => jobId = r.body.id)
       .then(r => cloud.get(`${test.api}/${jobId}`));
   });
 });
