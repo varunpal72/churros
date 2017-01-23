d738c388f0315dd536939b5934b62d4fd8504bd9
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2017-01-23 17:53:08 +0530

    Added churros for PUT /campaigns/{id}/leads

--- src/test/elements/pardot/campaigns.js
+++ src/test/elements/pardot/campaigns.js
@@ -4,6 +4,7 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const payload = require('./assets/campaigns');
 const contactsPayload = require('./assets/contacts');
+const leadsPayload = require('./assets/leads');
 
 suite.forElement('marketing', 'campaigns', { payload: payload }, (test) => {
   test.should.supportCrus();
@@ -11,13 +12,17 @@ suite.forElement('marketing', 'campaigns', { payload: payload }, (test) => {
   test.withName('should support searching campaigns by created_after').withOptions({ qs: { where: 'created_after=\'2015-01-01\'' } }).should.return200OnGet();
 
   it('should allow PUT for /hubs/marketing/campaigns/:id/contacts', () => {
-    let campaignId, contactId, contactCampaignPayload;
+    let campaignId, contactId, leadId, contactCampaignPayload, leadCampaignPayload;
     return cloud.get(test.api)
       .then(r => campaignId = r.body[0].id)
       .then(r => cloud.post(`/hubs/marketing/contacts`, contactsPayload))
       .then(r => contactId = r.body.id)
       .then(r => contactCampaignPayload = [{ "id": contactId }])
       .then(r => cloud.put(`${test.api}/${campaignId}/contacts`, contactCampaignPayload))
+      .then(r => cloud.post(`/hubs/marketing/leads`, leadsPayload))
+      .then(r => leadId = r.body.id)
+      .then(r => leadCampaignPayload = [{ "id": leadId }])
+      .then(r => cloud.put(`${test.api}/${campaignId}/leads`, leadCampaignPayload))
       .then(r => cloud.delete(`/hubs/marketing/contacts/${contactId}`));
   });
 });
