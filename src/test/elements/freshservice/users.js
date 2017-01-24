c312872b7d7102d41d0eb8c231344e08975ce926
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2017-01-24 15:12:23 +0530

    Added churros for ping, incidents and users

--- src/test/elements/freshservice/users.js
+++ src/test/elements/freshservice/users.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/agents');
-
+const tools = require('core/tools');
+const payload = {
+  "name": "Super Man",
+  "email": tools.randomEmail(),
+  "department_users_attributes": {
+    "department_id": "2,3"
+  }
+};
 suite.forElement('helpdesk', 'users', { payload: payload }, (test) => {
-  test.should.supportSr();
+  test.should.supportCruds();
 });
