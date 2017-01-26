d35ba0cc5019abec42cbb6f1391f85ed5e95b7d6
Tyler Toth <tyler@cloud-elements.com>
2017-01-26 14:36:57 -0600

    update for transformations

--- src/test/elements/facebookleadads/pages.js
+++ src/test/elements/facebookleadads/pages.js
@@ -4,11 +4,11 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('marketing', 'pages', null, (test) => {
-  it('should allow READ and SEARCH for /hubs/marketing/pages', () => {
+  test.should.supportSr();
+  it('should allow R for /hubs/marketing/leads', () => {
     let pageId;
     return cloud.get(test.api)
       .then(r => pageId = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${pageId}`))
       .then(r => cloud.get(`/hubs/marketing/leads/${pageId}`));
   });
 });
