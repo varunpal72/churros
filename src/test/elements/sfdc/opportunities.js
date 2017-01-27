7502db7efbff12065ecbcadec7d5ee5fadb7f4a9
Tyler Toth <tyler@cloud-elements.com>
2017-01-27 17:39:50 -0600

    Added PATCH {objectName}/:id/notes/:id tests

--- src/test/elements/sfdc/opportunities.js
+++ src/test/elements/sfdc/opportunities.js
@@ -11,21 +11,22 @@ suite.forElement('crm', 'opportunities', { payload: payload }, (test) => {
   it('should allow CRUDS for /hubs/crm/opportunities', () => {
     let opportunityId, activityId, noteId;
     return cloud.post(test.api, payload)
-        .then(r => opportunityId = r.body.id)
-        .then(r => cloud.get(`${test.api}/${opportunityId}`))
-        .then(r => cloud.get(test.api))
-        .then(r => cloud.patch(`${test.api}/${opportunityId}`, payload))
-        .then(r => cloud.get(`${test.api}/${opportunityId}/activities`))
-        .then(r => cloud.post(`${test.api}/${opportunityId}/activities`,activities))
-        .then(r => activityId = r.body.Id)
-        .then(r => cloud.get(`${test.api}/${opportunityId}/activities/${activityId}`))
-        .then(r => cloud.patch(`${test.api}/${opportunityId}/activities/${activityId}`,activities))
-        .then(r => cloud.delete(`${test.api}/${opportunityId}/activities/${activityId}`))
-        .then(r => cloud.get(`${test.api}/${opportunityId}/notes`))
-        .then(r => cloud.post(`${test.api}/${opportunityId}/notes`,notes))
-        .then(r => noteId = r.body.Id)
-        .then(r => cloud.get(`${test.api}/${opportunityId}/notes/${noteId}`))
-        .then(r => cloud.delete(`${test.api}/${opportunityId}/notes/${noteId}`))
-        .then(r => cloud.delete(`${test.api}/${opportunityId}`));
+      .then(r => opportunityId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${opportunityId}`))
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.patch(`${test.api}/${opportunityId}`, payload))
+      .then(r => cloud.get(`${test.api}/${opportunityId}/activities`))
+      .then(r => cloud.post(`${test.api}/${opportunityId}/activities`, activities))
+      .then(r => activityId = r.body.Id)
+      .then(r => cloud.get(`${test.api}/${opportunityId}/activities/${activityId}`))
+      .then(r => cloud.patch(`${test.api}/${opportunityId}/activities/${activityId}`, activities))
+      .then(r => cloud.delete(`${test.api}/${opportunityId}/activities/${activityId}`))
+      .then(r => cloud.get(`${test.api}/${opportunityId}/notes`))
+      .then(r => cloud.post(`${test.api}/${opportunityId}/notes`, notes))
+      .then(r => noteId = r.body.Id)
+      .then(r => cloud.get(`${test.api}/${opportunityId}/notes/${noteId}`))
+      .then(r => cloud.patch(`${test.api}/${opportunityId}/notes/${noteId}`, notes))
+      .then(r => cloud.delete(`${test.api}/${opportunityId}/notes/${noteId}`))
+      .then(r => cloud.delete(`${test.api}/${opportunityId}`));
   });
 });
