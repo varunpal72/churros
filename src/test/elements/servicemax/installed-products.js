e211e1ca40484b292348fb64cf2469b39874cc9e
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2017-01-30 12:35:55 +0530

    Added churros for installed-products and work-orders

--- src/test/elements/servicemax/installed-products.js
+++ src/test/elements/servicemax/installed-products.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/order-parts');
+const payload = require('./assets/installed-products');
+const tools = require('core/tools');
 const build = (overrides) => Object.assign({}, payload, overrides);
-const productsPayload = build({ SVMXC__On_Hold__c: true });
+const productsPayload = build({ Name: tools.random() });
 
-suite.forElement('fsa', 'order-parts', { payload: productsPayload }, (test) => {
+suite.forElement('fsa', 'installed-products', { payload: productsPayload }, (test) => {
   const options = {
     churros: {
       updatePayload: {
-        "SVMXC__On_Hold__c": false
+        "Name": tools.random()
       }
     }
   };
   test.withOptions(options).should.supportCruds();
   test.should.supportPagination();
   test.should.supportCeqlSearch('id');
-
 });
