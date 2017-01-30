f9c5695f52cf92827ccb8faeb10ae0847923bd39
BhushanWadichar-GsLab <bhushan.wadichar@gslab.com>
2017-01-30 15:23:44 +0530

    Updated churros for abbyy

--- src/test/elements/abbyy/tasks.js
+++ src/test/elements/abbyy/tasks.js
@@ -13,18 +13,19 @@ suite.forElement('ocr', 'tasks', (test) => {
 
     return cloud.postFile(test.api, path)
       .then(r => taskId = r.body["@id"])
-      .then(r => cloud.get(test.api + '/' + taskId, (r) => expect(r).to.have.schemaAnd200(schema)));
+      .then(r => cloud.get(test.api + '/' + taskId, (r) => expect(r).to.have.schemaAnd200(schema)))
+      .then(r => cloud.delete(test.api + '/' + taskId, (r) => expect(r).to.have.schemaAnd200(schema)));
   });
 
   it('should allow creating a task to process text fields in an uploaded file, and should get the created task',
-          () => {
-    let taskId = "-1";
-    let path = __dirname + '/assets/brady.jpg';
+    () => {
+      let taskId = "-1";
+      let path = __dirname + '/assets/brady.jpg';
 
-    return cloud.postFile(test.api + '/textfields', path)
-      .then(r => taskId = r.body["@id"])
-      .then(r => cloud.get(test.api + '/' + taskId, (r) => expect(r).to.have.schemaAnd200(schema)));
-  });
+      return cloud.postFile(test.api + '/textfields', path)
+        .then(r => taskId = r.body["@id"])
+        .then(r => cloud.get(test.api + '/' + taskId, (r) => expect(r).to.have.schemaAnd200(schema)));
+    });
 
   test.withValidation(tasksSchema).should.return200OnGet();
   test.withApi(`${test.api}/finished`).withValidation(tasksSchema).should.return200OnGet();
