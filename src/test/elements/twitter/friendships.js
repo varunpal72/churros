c64e05e9b2e47600a7e10ae825431bcb1011436d
yugesh <yugesh.bhosale@gslab.com>
2017-02-01 20:41:04 +0530

    Updated the churros tests for the Twitter

--- src/test/elements/twitter/friendships.js
+++ src/test/elements/twitter/friendships.js
@@ -4,6 +4,10 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const payload = require('./assets/friendships');
 
+const updatePayload = {
+  "follow": "true"
+};
+
 suite.forElement('social', 'friendships', { payload: payload }, (test) => {
   it(`should support CDS for ${test.api}`, () => {
     let userId;
@@ -13,6 +17,7 @@ suite.forElement('social', 'friendships', { payload: payload }, (test) => {
       .then(r => sourceScreenName = r.body.screen_name)
       .then(r => cloud.withOptions({ qs: { where: `source_screen_name = \'${sourceScreenName}\' and target_screen_name = \'${payload.screen_name}\'` } }).get(test.api))
       .then(r => userId = r.body.id)
+      .then(r => cloud.put(`${test.api}/${userId}`, updatePayload))
       .then(r => cloud.delete(`${test.api}/${userId}`));
   });
   test.withApi(`${test.api}/incoming`).should.supportPagination();
