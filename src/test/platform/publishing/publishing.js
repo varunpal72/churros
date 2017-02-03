0152d7ca520e7619440d057a1b33aa8a045e6c2c
Venkat Ramana Lashkar <ramana.kathek@gmail.com>
2017-02-03 07:33:42 -0600

    AWS Auto publishing tests

--- src/test/platform/publishing/publishing.js
+++ src/test/platform/publishing/publishing.js
@@ -4,18 +4,33 @@ const suite = require('core/suite');
 const payload = require('./assets/publishing');
 const dbPayload = require('./assets/db-publishing');
 const cloud = require('core/cloud');
+const provisioner = require('core/provisioner');
 
 const getGenerateSdkPayload = () => ({
   type: 'awslambda-native-java'
 });
 
-suite.forPlatform('publishing', { payload: payload, skip: true }, (test) => {
-  let httpElement, dbElement;
+const getPublishSdkPayload = () => ({
+  type: 'awslambda-native-java',
+  testFlag: false,
+  generateSwagger: true,
+  "publishMetaData": {
+      "stackName": "ChurrosStack",
+      "overwriteExisting": false
+  }
+});
+
+suite.forPlatform('publishing', { payload: payload }, (test) => {
+  let httpElement, dbElement, s3InstanceId, awscfInstanceId;
   before(() => {
     return cloud.post('elements', payload)
       .then(r => httpElement = r.body)
       .then(r => cloud.post('elements', dbPayload))
-      .then(r => dbElement = r.body);
+      .then(r => dbElement = r.body)
+      .then(r => provisioner.create('amazons3'))
+      .then(r => s3InstanceId = r.body.id)
+      .then(r => provisioner.create('awscloudformation'))
+      .then(r => awscfInstanceId = r.body.id);
   });
 
   it('should allow AWS lambda autogenerate and download for the element ', () => {
@@ -36,8 +51,22 @@ suite.forPlatform('publishing', { payload: payload, skip: true }, (test) => {
       .then(() => cloud.withOptions({ 'headers': headers }).get(`elements/client-sdks/${packageId}`));
   });
 
+
+  it('should allow AWS lambda autogenerate and deploy to AWS ', () => {
+    let packageId;
+    const headers = { 'Accept': 'application/zip' };
+
+    let publishPayload = getPublishSdkPayload();
+    publishPayload.publishMetaData.amazons3InstanceId = s3InstanceId;
+    publishPayload.publishMetaData.cloudformationInstanceId = awscfInstanceId;
+
+    return cloud.post(`elements/${httpElement.id}/client-sdks/publish`, publishPayload);
+  });
+
   after(() => {
     return cloud.delete(`elements/${httpElement.id}`)
-      .then(r => cloud.delete(`elements/${dbElement.id}`));
+      .then(r => cloud.delete(`elements/${dbElement.id}`))
+      .then(r => provisioner.delete(s3InstanceId, 'elements/amazons3/instances'))
+      .then(r => provisioner.delete(awscfInstanceId, 'elements/awscloudformation/instances'));
   });
 });
