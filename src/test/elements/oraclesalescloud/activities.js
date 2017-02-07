408924acabecc7129e1ad6c1f2f8725cd231d25f
Luke Vance <lukevance90@gmail.com>
2017-02-07 16:47:07 -0700

    fixed tests and payload for activitie and added other transformations

--- src/test/elements/oraclesalescloud/activities.js
+++ src/test/elements/oraclesalescloud/activities.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/activities');
+const moment = require('moment');
+const payload = {
+   "ActivityFunctionCode" : "APPOINTMENT",
+    "ActivityStartDate" : "2017-02-09T13:00:00+07:00",
+    "ActivityEndDate" : "2017-02-09T14:00:00+07:00",
+    "Subject" : "Fancy 2 activity"
+};
+let now = moment().add(1, 'd').format('YYYY-MM-DD');
+
+payload.ActivityStartDate =  now + 'T' + payload.ActivityStartDate.split('T')[1];
+payload.ActivityEndDate = now + 'T' + payload.ActivityEndDate.split('T')[1];
 
 suite.forElement('crm', 'activities', { payload: payload }, (test) => {
   test.should.supportCruds();
--- src/test/elements/oraclesalescloud/deals.js
+++ src/test/elements/oraclesalescloud/deals.js
@@ -1,11 +1,31 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/deals');
+const cloud = require('core/cloud');
+const contactPayload = require('./assets/contacts.json');
+const accountPayload = require('./assets/accounts.json');
+const payload = {
+  "CloseDate": "2017-09-30",     // close date must be in future
+  "ContactId":300000000787157,   // contactId must be valid contact "PartyId"
+  "CustomerId":300000000786253,  // customerId must be valid account "PartyId"
+  "DealSize":1100,
+  "DealType": "ORA_EXISTING"
+};
 
 suite.forElement('crm', 'deals', { payload: payload }, (test) => {
-  // need to get correct contactId first
-  test.withOptions({skip:true}).should.supportCrus();
+  // get valid contactId and accountId first
+  before(() => cloud.post(`hubs/crm/accounts`, accountPayload)
+    .then(r => payload.CustomerId = r.body.PartyId)
+    .then(() => cloud.post(`hubs/crm/contacts`, contactPayload))
+    .then(r => payload.ContactId = r.body.PartyId)
+  );
+
+  test.withOptions({skip:false}).should.supportCrus();
   test.should.supportPagination();
   test.should.supportCeqlSearchForMultipleRecords('LastName');
+
+  // delete contact and account created for deal
+  after(() => cloud.delete(`hubs/crm/accounts/${payload.CustomerId}`)
+    .then(() => cloud.delete(`hubs/crm/contacts/${payload.ContactId}`))
+  );
 });
