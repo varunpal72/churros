a94c8638c7d81b5463667e69880f44162d0ab60a
root <hanumant.shinde@gslab.com>
2017-02-07 09:44:43 +0000

    Updated tests for autotaskcrm

--- src/test/elements/autotaskcrm/opportunities.js
+++ src/test/elements/autotaskcrm/opportunities.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const payload = require('./assets/opportunities');
 
-suite.forElement('crm', 'opportunities', { payload: payload, skip: true }, (test) => {
+suite.forElement('crm', 'opportunities', { payload: payload }, (test) => {
   test.should.supportCrus();
   test.withOptions({ qs: { where: 'title=\'ChurrosTestOpportunity\'' } }).should.return200OnGet();
   test.should.supportPagination();
--- src/test/elements/autotaskcrm/users.js
+++ src/test/elements/autotaskcrm/users.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
 const payload = require('./assets/users');
+const contactPayload = require('./assets/contacts');
+const tools = require('core/tools');
+const build = (overrides) => Object.assign({}, payload, overrides);
 
-suite.forElement('crm', 'users', { payload: payload, skip: true }, (test) => {
-  test.should.supportCrus();
-  test.withOptions({ qs: { where: 'userName=\'mrchurros@cloud-elements.com\'' } }).should.return200OnGet();
-  test.should.supportPagination();
+suite.forElement('crm', 'users', { payload: payload }, (test) => {
+  let userpayload;  
+  it(`should support CRUS for /hubs/crm/users`, () => {
+    let contactId,userId,userPayload;
+    return cloud.post('/hubs/crm/contacts', contactPayload)
+      .then(r => userPayload = build({ userName: tools.randomEmail(), contactID: r.body.id }))
+      .then(r => cloud.post(test.api, userPayload))
+      .then(r => userId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${userId}`))
+      .then(r => cloud.patch(`${test.api}/${userId}`, userPayload))
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
+      .then(r => cloud.withOptions({ qs: { where: 'userName=\'mrchurros@cloud-elements.com\'' } }).get(test.api));
+    });
 });
