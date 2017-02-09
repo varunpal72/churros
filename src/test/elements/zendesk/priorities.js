676c27fedb035dd38164a5f7e40aa6766acc4f23
CharuhasVidwnas-GSLab <charuhas.vidwans@gslab.com>
2017-02-09 19:27:28 +0530

    Added churros for /priority API's

---
+++ src/test/elements/zendesk/priorities.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const updatePayload = () => ({
+  "raw_description":"test update"
+});
+
+suite.forElement('helpdesk', 'priorities', null , (test) => {
+  test.should.supportPagination();
+  it(`should allow GET and PATCH for ${test.api}`, () => {
+    let priorityId;
+    return cloud.get(test.api)
+    .then(r => priorityId = r.body[0].id)
+    .then(r => cloud.get(`${test.api}/${priorityId}`))
+    .then(r => cloud.patch(`${test.api}/${priorityId}`,updatePayload()));
+  });
+});
