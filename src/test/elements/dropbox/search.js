5203b305a0c9710f39f564eeb5ab10420a00684f
Martin Brandt <martinfbrandt@gmail.com>
2017-02-10 12:52:42 -0700

    removed unnecessary requires

--- src/test/elements/dropbox/search.js
+++ src/test/elements/dropbox/search.js
@@ -1,6 +1,5 @@
 'use strict';
-const tools = require('core/tools');
-const cloud = require('core/cloud');
+
 const suite = require('core/suite');
 exports.search = () => {
   suite.forElement('documents', 'search', (test) => {
--- src/test/elements/dropboxv2/search.js
+++ src/test/elements/dropboxv2/search.js
@@ -1,6 +1,4 @@
 'use strict';
-const tools = require('core/tools');
-const cloud = require('core/cloud');
 const suite = require('core/suite');
 exports.search = () => {
   suite.forElement('documents', 'search', (test) => {
