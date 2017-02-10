44928ddd512289672ebd6d3746c05add810a8efa
Tyler Toth <tyler@cloud-elements.com>
2017-02-10 16:34:59 -0600

    incidentsUpdates

--- src/test/elements/helpscout/incidents.js
+++ src/test/elements/helpscout/incidents.js
@@ -7,55 +7,43 @@ const tools = require('core/tools');
 const payload = require('./assets/incident');
 const updatePayload = require('./assets/updatedIncident');
 const thread = require('./assets/thread');
-const attachmentsApi = '/hub/helpdesk/attachments';
+const attachmentsApi = '/hubs/helpdesk/attachments';
 const email = tools.randomEmail();
 
 suite.forElement('helpdesk', 'incidents', null, (test) => {
-  /*
-  let attachmentHash;
+
   it(`should allow allow C for ${attachmentsApi}`, () => {
     return cloud.postFile(attachmentsApi, __dirname + "/assets/attachment.txt")
-      .then(r => attachmentHash = r.body.hash);
+      .then(r => payload.threads[0].attachments[0].hash = r.body.hash);
   });
 
-  payload.threads[0].attachments[0].hash = attachmentHash;
-  */
   payload.customer.email = email;
   payload.threads[0].createdBy.email = email;
 
   it(`should allow allow CRUDS for ${test.api}, R for ${test.api}/{id}/comments and RD for ${attachmentsApi}`, () => {
-    let incidentId;
-    let threadId;
-    // let attachmentId;
+    let incidentId, threadId, attachmentId, incidentBody;
     return cloud.post(test.api, payload)
+      .then(r => incidentId = r.body.id)
       .then(r => cloud.get(test.api))
-      .then(r => incidentId = r.body.filter(function(incident) {
-        return incident.customer ? incident.customer.email ? incident.customer.email === email : false : false;
-      })[0].id)
       .then(r => cloud.get(`${test.api}/${incidentId}`))
-      .then(r => threadId = r.body.threads.filter(function(thread) {
-        return thread.body ? thread.body === payload.threads[0].body : false;
-      })[0].id)
-      /*
-      .then(r => attachmentId = r.body.threads[0].attachments[0].id)
-      .then(r => cloud.get(`${attachmentsApi}/${attachmentId}`))
-      .then(r => cloud.delete(`${attachmentsApi}/${attachmentId}`))
-      */
+      .then(r => incidentBody = r.body)
+      .then(r => cloud.get(`${attachmentsApi}/${incidentBody.threads[0].attachments[0].id}`))
+      .then(r => cloud.delete(`${attachmentsApi}/${incidentBody.threads[0].attachments[0].id}`))
       .then(r => cloud.patch(`${test.api}/${incidentId}`, updatePayload))
       .then(r => cloud.get(`${test.api}/${incidentId}`))
-      .then(r => cloud.put(`${test.api}/${incidentId}/comments/${threadId}`, thread))
-      .then(r => cloud.get(`${test.api}/${incidentId}`));
+      .then(r => cloud.put(`${test.api}/${incidentId}/comments/${incidentBody.threads[0].id}`, thread))
+      .then(r => cloud.delete(`${test.api}/${incidentId}`));
   });
 
   it(`should allow paginating with page ${test.api}`, () => {
     return cloud.withOptions({ qs: { page: 1 } }).get(test.api);
   });
 
-  it(`should support searching ${test.api} by firstName`, () => {
-    return cloud.withOptions({ qs: { where: `firstName='${updatePayload.firstName}'` } }).get(test.api);
+  it(`should support searching ${test.api} by modifiedSince`, () => {
+    return cloud.withOptions({ qs: { where: `modifiedSince='2016-09-30T17:21:00Z'` } }).get(test.api);
   });
 
-  it(`should allow paginating with page and searching ${test.api} by lastName`, () => {
-    return cloud.withOptions({ qs: { page: 1, where: `lastName='${payload.lastName}'` } }).get(test.api);
+  it(`should allow paginating with page and searching ${test.api} by modifiedSince`, () => {
+    return cloud.withOptions({ qs: { page: 1, where: `modifiedSince='2016-09-30T17:21:00Z'` } }).get(test.api);
   });
 });
