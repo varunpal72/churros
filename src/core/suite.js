ec1137ed30bf719c163d8c06a597c47133e89238
Tyler Toth <tyler@cloud-elements.com>
2017-02-10 17:01:41 -0600

    Updated to also pass page/pageSize in options

--- src/core/suite.js
+++ src/core/suite.js
@@ -75,9 +75,10 @@ const itCrs = (name, api, payload, validationCb, options) => {
 
 const itPagination = (name, api, options, validationCb) => {
   const n = name || `should allow paginating with page and pageSize ${api}`;
-  const limit = options ? options.qs ? options.qs.pageSize ? options.qs.pageSize : 1 : 1 : 1;
-  const newOptions = Object.assign({}, options, { qs: { page: 1, pageSize: limit } });
-  boomGoesTheDynamite(n, () => cloud.withOptions(newOptions).get(api, (r) => expect(r.body.length).to.equal(limit)), options ? options.skip : false);
+  const pageSize = options ? options.qs ? options.qs.pageSize ? options.qs.pageSize : 1 : 1 : 1;
+  const page = options ? options.qs ? options.qs.page ? options.qs.page : 1 : 1 : 1;
+  const newOptions = Object.assign({}, options, { qs: { page: page, pageSize: pageSize } });
+  boomGoesTheDynamite(n, () => cloud.withOptions(newOptions).get(api, (r) => expect(r.body.length).to.equal(pageSize)), options ? options.skip : false);
 };
 
 const paginate = (api, options, validationCb, nextPage, page, max, all) => {
