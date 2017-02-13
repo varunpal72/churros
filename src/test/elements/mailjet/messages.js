2c80276f8e6aaa7117bbf7645b0d76458031b100
Tyler Toth <tyler@cloud-elements.com>
2017-02-13 13:20:35 -0600

    updates per element changes

--- src/test/elements/mailjet/messages.js
+++ src/test/elements/mailjet/messages.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const suite = require('core/suite');
+const cloud = require('core/cloud');
+const tools = require('core/tools');
 const payload = require('./assets/messages');
 
 suite.forElement('messaging', 'messages', { payload: payload }, (test) => {
-	test.should.return200OnGet();
-	test.should.supportPagination();
-	test.should.supportCr();
+  it(`should allow CR for ${test.api}`, () => {
+    let messageId;
+    return cloud.post(test.api, payload)
+      .then(r => messageId = r.body.id)
+      .then(() => tools.sleep(10))		//takes some time for Mailjet to process the POST request
+      .then(r => cloud.get(`${test.api}/${messageId}`));
+  });
+  test.should.supportS();
+  test.should.supportPagination();
 });
