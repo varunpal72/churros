3dfb98a1bea9a2abe5376bdc1cd7f065fc401a72
charuhas <charuhas.vidwans@gslab.com>
2017-02-17 16:08:54 +0530

    Adding review comments changes

--- src/test/elements/hubspotcrm/contacts.js
+++ src/test/elements/hubspotcrm/contacts.js
@@ -4,6 +4,7 @@ const suite = require('core/suite');
 const payload = require('./assets/contacts');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+var contactsId = 396139;
 suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
   test.should.supportPagination();
   it('should test CRUD for /contacts and GET /contacts/{id}/activities', () => {
@@ -23,6 +24,10 @@ suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
       .then(r => options.qs.nextPage = r.response.headers['elements-next-page-token'])
       .then(r => cloud.withOptions(options).get(`${test.api}/${contactId}/activities`))
       .then(r => cloud.patch(`${test.api}/${contactId}`, updatePayload))
-      .then(r => cloud.delete(`${test.api}/${contactId}`));
+      .then(r => cloud.delete(`${test.api}/${contactId}`))
   });
+});
+
+suite.forElement('crm', `contacts/${contactsId}/activities`, { payload: payload }, (test) => {
+  test.should.supportNextPagePagination(1);
 });
