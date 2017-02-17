019b7236a6f0882a8cb806858ac8817c7efcecfb
Tyler Toth <tyler@cloud-elements.com>
2017-02-17 16:06:18 -0600

    Fresh desk v2 fixes

--- src/test/elements/freshdeskv2/agents.js
+++ src/test/elements/freshdeskv2/agents.js
@@ -4,4 +4,6 @@ const suite = require('core/suite');
 
 suite.forElement('helpdesk', 'agents', null, (test) => {
   test.should.supportSr();
+  test.withName(`should support searching ${test.api} by email`).withOptions({ qs: { where: 'email=\'freshdesk@cloud-elements.com\'' } }).should.return200OnGet();
+  test.should.supportPagination();
 });
--- src/test/elements/freshdeskv2/incidents.js
+++ src/test/elements/freshdeskv2/incidents.js
@@ -4,19 +4,20 @@ const suite = require('core/suite');
 const payload = require('./assets/incidents');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
-payload.email = tools.randomEmail();
+let email = tools.randomEmail();
+payload.email = email;
 payload.name = tools.random();
 var date = new Date();
 date.setFullYear(date.getFullYear() + tools.randomInt());
 payload.due_by = date.toISOString();
 payload.fr_due_by = date.toISOString();
-var types = ["Question", "Incident" , "Problem", "Feature Request", "Lead"];
-payload.type = types[Math.floor(Math.random()*types.length)];
+var types = ["Question", "Incident", "Problem", "Feature Request", "Lead"];
+payload.type = types[Math.floor(Math.random() * types.length)];
 
 suite.forElement('helpdesk', 'incidents', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
-
+  test.withName(`should support searching ${test.api} by updated_since`).withOptions({ qs: { where: 'updated_since=\'2016-01-01\'' } }).should.return200OnGet();
   it('should support GET /hubs/helpdesk/incidents/:id/comments ', () => {
     let id;
     return cloud.post(test.api, payload)
