8f214a0506c185e89653e9c74d78c02e3da9bec8
Madhuri Kadam <GS-1259@GSLAB.COM>
2017-02-21 23:38:20 +0530

    Formatted JSON

--- src/test/elements/zuorav2/subscriptions.js
+++ src/test/elements/zuorav2/subscriptions.js
@@ -31,14 +31,45 @@ suite.forElement('payment', 'subscriptions', { payload: payload }, (test) => {
   test.should.supportPagination();
   it(`should allow CRUDS ${test.api}`, () => {
     let id, customerId;
-    const previewPayload = { "contractEffectiveDate": "2015-1-15", "initialTerm": 12, "initialTermPeriodType": "Week", "previewAccountInfo": { "billCycleDay": 31, "billToContact": { "city": "Walnut Creek", "country": "United States", "county": "Contra Consta", "state": "California", "zipCode": "94549" }, "currency": "USD" }, "subscribeToRatePlans": [{ "chargeOverrides": [{ "productRatePlanChargeId": "c3ae26226860f5ef425eeafff8efd36a" }], "productRatePlanId": "c3ae2622721fc3a6c82eeebc7b386798" }], "termType": "TERMED" };
+    const previewPayload = {
+      "contractEffectiveDate": "2015-1-15",
+      "initialTerm": 12,
+      "initialTermPeriodType": "Week",
+      "previewAccountInfo": {
+        "billCycleDay": 31,
+        "billToContact": {
+          "city": "Walnut Creek",
+          "country": "United States",
+          "county": "Contra Consta",
+          "state": "California",
+          "zipCode": "94549"
+        },
+        "currency": "USD"
+      },
+      "subscribeToRatePlans": [{
+        "chargeOverrides": [{
+          "productRatePlanChargeId": "c3ae26226860f5ef425eeafff8efd36a"
+        }],
+        "productRatePlanId": "c3ae2622721fc3a6c82eeebc7b386798"
+      }],
+      "termType": "TERMED"
+    };
     previewPayload.subscribeToRatePlans[0].productRatePlanId = rateId;
     previewPayload.subscribeToRatePlans[0].chargeOverrides[0].productRatePlanChargeId = charge;
 
 
-    const renewUpdatePayload = { "collect": false, "invoice": true };
+    const renewUpdatePayload = {
+      "collect": false,
+      "invoice": true
+    };
 
-    const cancleUpdatePayload = { "cancellationEffectiveDate": "2015-01-31", "cancellationPolicy": "SpecificDate", "collect": false, "invoice": true };
+    const cancleUpdatePayload = {
+      "cancellationEffectiveDate": "2015-01-31",
+      "cancellationPolicy": "SpecificDate",
+      "collect": false,
+      "invoice": true
+    };
+
     return cloud.post(`/hubs/payment/customers`, customerPayload)
       .then(r => customerId = r.body.id)
       .then(r => payload.accountKey = customerId)
