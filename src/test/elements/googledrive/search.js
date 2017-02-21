1041c3c18f3e4c9a481808cb563557cdc234b7f8
Atul Barve <atul@cloud-elements.com>
2017-02-21 12:08:09 -0600

    Updated Churros Test for Search Paging.

--- src/test/elements/googledrive/search.js
+++ src/test/elements/googledrive/search.js
@@ -2,7 +2,16 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
+const chakram = require('chakram');
+const expect = chakram.expect;
 
-suite.forElement('documents', 'search',null, (test) => { 
+
+suite.forElement('documents', 'search',null, (test) => {
      test.withOptions({ qs: { text : tools.random() } }).should.return200OnGet();
+     test
+         .withName(`should not support searching more than 1000 records`)
+         .withOptions({ qs: { pageSize: 2,page : 501 } })
+         .withValidation((r) => expect(r).to.have.statusCode(400))
+         .should.return200OnGet();
+     test.should.supportPagination();
 });
