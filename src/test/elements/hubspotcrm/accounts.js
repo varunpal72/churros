c1a28ee70285e10823f473a660ed0d858498eb3f
charuhas <charuhas.vidwans@gslab.com>
2017-02-22 16:21:31 +0530

    Account activity churros added

--- src/test/elements/hubspotcrm/accounts.js
+++ src/test/elements/hubspotcrm/accounts.js
@@ -2,8 +2,33 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/accounts');
-
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+var accountsId = 285826161;
 suite.forElement('crm', 'accounts', { payload: payload }, (test) => {
-  test.should.supportCruds();
   test.should.supportPagination();
+  it('should test CRUD for /accounts and GET /accounts/{id}/activities', () => {
+    const updatePayload = {
+      "properties": {
+        "name": tools.random()
+      }
+    };
+    let accountId;
+    const options = { qs: { pageSize: 1 } };
+    return cloud.post(test.api, payload)
+      .then(r => accountId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${accountId}`))
+      .then(r => cloud.get(`${test.api}`))
+      .then(r => cloud.get(`${test.api}/${accountId}/activities`))
+      .then(r => cloud.withOptions(options).get(`${test.api}/${accountId}/activities`))
+      .then(r => options.qs.nextPage = r.response.headers['elements-next-page-token'])
+      .then(r => cloud.withOptions(options).get(`${test.api}/${accountId}/activities`))
+      .then(r => cloud.patch(`${test.api}/${accountId}`, updatePayload))
+      .then(r => cloud.delete(`${test.api}/${accountId}`));
+  });
 });
+
+suite.forElement('crm', `accounts/${accountsId}/activities`, { payload: payload }, (test) => {
+  test.should.supportNextPagePagination(1);
+});
+
