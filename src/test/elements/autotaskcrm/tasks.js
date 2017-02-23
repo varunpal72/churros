b9ec28df4cfa1ae6804feb507351cc1ea75be9a3
hanumant <hanumant.shinde@gslab.com>
2017-02-23 17:34:15 +0530

    Addressed the review comments

--- src/test/elements/autotaskcrm/tasks.js
+++ src/test/elements/autotaskcrm/tasks.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const payload = require('./assets/tasks');
 
-suite.forElement('crm', 'tasks', { payload: payload }, (test) => {
+suite.forElement('crm', 'tasks', { payload: payload, skip: true}, (test) => {
   test.should.supportCrus();
   test.withOptions({ qs: { where: 'title=\'ChurrosTestTask\'' } }).should.return200OnGet();
   test.should.supportPagination();
