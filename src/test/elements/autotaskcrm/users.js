cda88991ba425f9b7cfb87c468135106f3f45710
Luke Vance <lukevance90@gmail.com>
2017-03-02 13:56:08 -0700

    added test for PATCH users/{id}

--- src/test/elements/autotaskcrm/users.js
+++ src/test/elements/autotaskcrm/users.js
@@ -2,30 +2,33 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-// const payload = require('./assets/users');
-// const contactPayload = require('./assets/contacts');
-// const tools = require('core/tools');
-// const build = (overrides) => Object.assign({}, payload, overrides);
+const tools = require('core/tools');
+
+const confirmEmailProp = (users) => {
+  for (let i = 0; i < users.length; i++){
+    if (users[i].email){
+      return users[i];
+    } else if (i === users.length -1){
+      let user = users[i];
+      user.email = tools.randomEmail();
+      return user;
+    }
+  }
+};
 
 suite.forElement('crm', 'users', { skip: false}, (test) => {
     test.should.supportSr();
     test.should.supportPagination();
+    it('should support PATCH for /hubs/crm/users/{id}', () => {
+      let userPayload;
+      return cloud.get(test.api)
+        .then(r => userPayload = confirmEmailProp(r.body))
+        .then(r => cloud.patch(`${test.api}/${userPayload.id}`, userPayload));
+    });
     it('should support where for /hubs/crm/users', () => {
       let locationID;
       return cloud.get(test.api)
         .then(r => locationID = r.body[0].locationID)
         .then(r => cloud.withOptions({qs: {where: 'locationID=\'' + locationID + '\''}}).get(test.api));
     });
-    // it(`should support CRUS, pagination and where for /hubs/crm/users`, () => {
-    // let userId;
-    // return cloud.post(test.api, payload)
-    //   .then(r => {
-    //     console.log(r);
-    //     userId = r.body.id;
-    //   })
-    //   .then(r => cloud.get(`${test.api}/${userId}`))
-    //   .then(r => cloud.patch(`${test.api}/${userId}`, payload))
-    //   .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
-    //   .then(r => cloud.withOptions({ qs: { where: 'userName=\'mrchurros@cloud-elements.com\'' } }).get(test.api));
-    // });
 });
