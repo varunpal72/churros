3894ad60b504866569da2f955d414aec431dad71
Atul Barve <atul@cloud-elements.com>
2017-03-02 13:06:45 -0600

    Added Crud Churros For Payroll Items.

--- src/test/elements/netsuitehcv2/payroll-items.js
+++ src/test/elements/netsuitehcv2/payroll-items.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const suite = require('core/suite');
+const payload = require('./assets/payroll-items');
 
-suite.forElement('humancapital', 'payroll-items', null, (test) => {
-  test.should.supportSr();
+suite.forElement('humancapital', 'payroll-items', { payload: payload }, (test) => {
+  test.should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
 });
