4c3798594ad9baad0acec7c7f88d3e0513f5cbf7
Atul Barve <atul@cloud-elements.com>
2017-03-06 12:47:48 -0600

    fixed review comments

--- src/test/elements/netsuitecrmv2/accounts.js
+++ src/test/elements/netsuitecrmv2/accounts.js
@@ -3,6 +3,7 @@
 const suite = require('core/suite');
 const payload = require('./assets/accounts');
 
+// Sample Custom Where Clause for Reference
 //`custom.long.scriptId` = 'custentity_cust_priority' and `custom.long.value` = 50
 //`custom.multi.scriptId` = 'custentity1' and `custom.multi.value.internalId` = 1
 //`custom.boolean.scriptId` = 'custentity_2663_direct_debit' and `custom.boolean.value` = 'false'
@@ -12,6 +13,6 @@ suite.forElement('crm', 'accounts', { payload: payload }, (test) => {
   test.withOptions({ qs: { page: 1,
                            pageSize: 5,
                            where : "`custom.multi.scriptId` = 'custentity1' and `custom.multi.value.internalId` = 1"
-                         } }).should.return200OnGet();
+                         } }).should.supportPagination();
   test.should.supportCeqlSearch('id');
 });
