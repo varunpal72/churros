086a299aadd428c59208cb918f6eb7c4fea8f2df
Tyler Toth <tyler@cloud-elements.com>
2017-03-06 12:31:45 -0600

    adding comment to test

--- src/test/elements/paypalv2/payments.js
+++ src/test/elements/paypalv2/payments.js
@@ -5,6 +5,7 @@ const payload = require('./assets/payments');
 
 suite.forElement('payment', 'payments', { payload: payload }, (test) => {
   test.withOptions({ skip: true }).should.supportCrs();
+  // Other tests missing as they hinge on authorizating the payment via selenium 
   test.should.supportPagination();
   test.withName('should support Ceql search').withOptions({ qs: { where: `start_time='2016-03-06T11:00:00Z'` } }).should.return200OnGet();
 });
