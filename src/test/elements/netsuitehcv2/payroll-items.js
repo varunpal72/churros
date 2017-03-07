f35aa667d74e38119e74a50109b0cc615f16eb6a
Atul Barve <atul@cloud-elements.com>
2017-03-06 20:39:32 -0600

    Fixed Test

--- src/test/elements/netsuitehcv2/payroll-items.js
+++ src/test/elements/netsuitehcv2/payroll-items.js
@@ -5,5 +5,5 @@ const payload = require('./assets/payroll-items');
 
 suite.forElement('humancapital', 'payroll-items', { payload: payload }, (test) => {
   test.should.supportCruds();
-  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
+  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
 });
