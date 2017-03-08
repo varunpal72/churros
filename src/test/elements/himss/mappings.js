2d1abedc7698e6c50350f0f3a515a63b64c61ce6
Mithila Garud <mithila.garud@gslab.com>
2017-03-08 11:34:04 +0530

    Added skip on suite instead of the test case

--- src/test/elements/himss/mappings.js
+++ src/test/elements/himss/mappings.js
@@ -4,8 +4,8 @@ const suite = require('core/suite');
 const payload = require('./assets/mappings');
 const cloud = require('core/cloud');
 
-suite.forElement('general', 'mappings', { payload: payload }, (test) => {
-  it.skip('should allow POST and DELETE /mappings', () => {
+suite.forElement('general', 'mappings', { payload: payload, skip: true }, (test) => {
+  it('should allow POST and DELETE /mappings', () => {
     return cloud.post(test.api, payload)
       .then(r => cloud.delete(test.api));
   });
