913f6866fc0b376c2b535cc9a4a641c0f26f42ea
Austin Mahan <Austin@CE-MacBook-Pro-03.local>
2017-03-17 09:29:47 -0600

    commiting to change branch

--- src/test/elements/connectwisehd/contacts.js
+++ src/test/elements/connectwisehd/contacts.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const payload = require('./assets/contacts');
 
-suite.forElement('helpdesk', 'contacts', { payload: payload }, (test) => {
+suite.forElement('helpdesk', 'contacts', { payload: payload, skip:true }, (test) => {
   test.should.supportCruds();
   test.withOptions({ qs: { where: 'email=\'churroshd@connectWise.com\'' } }).should.return200OnGet();
   test.should.supportPagination();
