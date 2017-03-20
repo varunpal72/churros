0327753f01f4935e4ed3224874412bc1d8df3a89
Austin Mahan <Austin@CE-MacBook-Pro-03.local>
2017-03-20 11:41:47 -0600

    testing things

--- src/core/oauth.js
+++ src/core/oauth.js
@@ -184,12 +184,17 @@ const manipulateDom = (element, browser, r, username, password, config) => {
     case 'googledrive':
       browser.get(r.body.oauthUrl);
       browser.findElement(webdriver.By.id('Email')).sendKeys(username);
-      browser.findElement(webdriver.By.id('next')).click();
+      browser.findElement(webdriver.By.id('Email')).submit();
+      // browser.findElement(webdriver.By.id('next')).click();
       browser.sleep(2000);
       browser.findElement(webdriver.By.id('Passwd')).sendKeys(password);
-      browser.findElement(webdriver.By.id('signIn')).click();
+      browser.findElement(webdriver.By.id('Passwd')).submit();
+      // browser.findElement(webdriver.By.id('signIn')).click();
       browser.sleep(2000);
-      browser.findElement(webdriver.By.id('submit_approve_access')).click();
+      browser.findElement(webdriver.By.xpath('//*[@id="submit_approve_access"]'))
+      .then((element) => element.click(), (err) => {});
+      // browser.findElement(webdriver.By.id('submit_approve_access'))
+      // .then((element) => element.click(), (err) => {}); // ignore this
       browser.sleep(2000);
       return browser.getCurrentUrl();
     case 'gotowebinar':
