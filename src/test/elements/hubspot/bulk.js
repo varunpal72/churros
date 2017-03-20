d48d8cf350e34eb2467f46db9943f1a07f67b123
Austin Mahan <Austin@CE-MacBook-Pro-03.local>
2017-03-20 10:26:44 -0600

    changed hubspot bulk

--- src/test/elements/hubspot/bulk.js
+++ src/test/elements/hubspot/bulk.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const expect = require('chakram').expect;
 const suite = require('core/suite');
-const cloud = require('core/cloud');
-const tools = require('core/tools');
 
 suite.forElement('marketing', 'bulk', (test) => {
   test.should.supportBulkDownload({ qs: { q: 'select * from contacts where firstname = \'Kimberly\'' } });
