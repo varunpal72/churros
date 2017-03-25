f85a8fdbb8fddcea5d7d4a09e5e4395f86896f0a
Tyler Toth <tyler@cloud-elements.com>
2017-03-24 21:06:41 -0500

    npm test updates

--- src/test/elements/magentov20/orders.js
+++ src/test/elements/magentov20/orders.js
@@ -2,7 +2,6 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const tools = require('core/tools');
 const productPayload = require('./assets/products');
 const payload = require('./assets/orders');
 
@@ -28,6 +27,6 @@ suite.forElement('ecommerce', 'orders', (test) => {
       .then(r => cloud.get(test.api))
       // .then(r => cloud.put(`${test.api}/${orderId}`, payload)) @tylertoth - need to get payload
       .then(r => cloud.delete(`/hubs/ecommerce/products/${sku}`))
-      .then(r => cloud.delete(`${test.api}/${orderId}`));;
+      .then(r => cloud.delete(`${test.api}/${orderId}`));
   });
 });
