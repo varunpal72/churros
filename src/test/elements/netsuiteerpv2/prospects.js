19f644d1194818aa762e670a9bc1da7e23228862
Atul Barve <atul@cloud-elements.com>
2017-03-31 17:33:13 -0500

    Added netsuite tests for prospects

---
+++ src/test/elements/netsuiteerpv2/prospects.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/prospects');
+const tools = require('core/tools');
+
+payload.companyName += tools.random();
+
+suite.forElement('erp', 'prospects', { payload: payload }, (test) => {
+  test.should.supportCruds();
+  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
+  test.should.supportCeqlSearch('id');
+});
