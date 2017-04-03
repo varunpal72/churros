2ce5dc519b23045e1d97ea2e6cd0513238250edd
Gauri Bhutkar <gauri.bhutkar@gslab.com>
2017-04-03 16:06:46 +0530

    Added churros for organizations

---
+++ src/test/elements/connectwisecrmrest/organizations.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const suite = require('core/suite');
+const payload = require('./assets/organizations');
+const cloud = require('core/cloud');
+const organizationUpdate = () => ({
+  "identifier": "xyz10",
+  "name": "abc",
+  "billToCompany":{
+    "id":"644"
+  }
+});
+
+suite.forElement('crm', 'organizations', { payload: payload }, (test) => {
+  test.should.supportPagination();
+  it(`should support PUT for organizations`, () => {
+    let organizationId;
+    return cloud.post(test.api, payload)
+      .then(r => organizationId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${organizationId}`))
+      .then(r => cloud.get(`${test.api}/${organizationId}`))
+      .then(r => cloud.patch(`${test.api}/${organizationId}`, organizationUpdate()))
+      .then(r => cloud.put(`${test.api}/${organizationId}`, payload))
+      .then(r => cloud.delete(`${test.api}/${organizationId}`));
+  });
+});
