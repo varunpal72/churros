582e52b6f7ecbf8642baff640791aa22a0a5b731
Atul Barve <atul@cloud-elements.com>
2017-04-11 12:38:15 -0500

    Added Churros to fetch bookmarks

---
+++ src/test/elements/box/bookmarks.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const cloud = require('core/cloud');
+const suite = require('core/suite');
+
+
+
+suite.forElement('documents', 'folders/contents', (test) => {
+  test.withOptions({qs: { path: "/TestFolderDoNoDelete"}}).should.return200OnGet();
+});
