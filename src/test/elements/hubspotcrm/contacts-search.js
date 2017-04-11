1d8cdc106b4b94a0bea9e06ac783b986996b8530
Venkat Ramana Lashkar <ramana.kathek@gmail.com>
2017-04-11 12:58:36 -0500

    fixed `jshint` errors

--- src/test/elements/hubspotcrm/contacts-search.js
+++ src/test/elements/hubspotcrm/contacts-search.js
@@ -2,9 +2,7 @@
 
 const expect = require('chakram').expect;
 const suite = require('core/suite');
-const tools = require('core/tools');
 const cloud = require('core/cloud');
-const moment = require('moment');
 const newResource = require('./assets/newresource.json');
 
 // Test for extending hubspot crm and invoking the extended resource
@@ -17,7 +15,7 @@ suite.forElement('crm', 'contacts-search', {}, (test) => {
   //delete new/overide resource should work fine
   after(() => cloud.delete(`elements/hubspotcrm/resources/${newResourceId}`));
 
-  it('should test contacts-search', () => {
+  it('should test newly added account resource contacts-search', () => {
       const options = { qs: { where: "q='test'" } };
       return cloud.withOptions(options).get(`contacts-search`)
       .then(r => {
