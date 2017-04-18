a599d517b5ab35ed1e97d255fe036b109dd185a5
Jamie Thorson <jamiethorson@Jamies-MacBook-Pro.local>
2017-04-18 16:43:33 -0600

    added checks for endpoint /contacts/{contactId}/activities

--- src/test/elements/actessentialsoauth/contacts.js
+++ src/test/elements/actessentialsoauth/contacts.js
@@ -3,9 +3,18 @@
 const suite = require('core/suite');
 const payload = require('./assets/contacts');
 const options = { payload: payload };
+const cloud = require('core/cloud');
 
 suite.forElement('crm', 'contacts', options, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
   test.should.supportCeqlSearchForMultipleRecords('lastName');
+  it('should allow CRUDS for /hubs/crm/contacts/{contactId}/activities', () => {
+    let contactId;
+    return cloud.post(test.api, payload)
+      .then(r => contactId = r.body.id)
+      .then(console.log(contactId))
+      .then(r => cloud.get(`${test.api}/${contactId}/activities`))
+      .then(r => cloud.delete(`${test.api}/${contactId}`));
+  });
 });
