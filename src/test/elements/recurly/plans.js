5a392bf33a39df1a8c2ee72f300fe41832cbab98
Abhilash <abhilashreddy.guduru13@gmail.com>
2017-04-18 18:50:15 -0500

    Corrected npm tests.

--- src/test/elements/recurly/plans.js
+++ src/test/elements/recurly/plans.js
@@ -11,5 +11,5 @@ suite.forElement('billing', 'plans', { payload: plansPayload }, (test) => {
   test.should.supportCrs(); //not testing deleting as you can't delete a created plan
   test.should.supportNextPagePagination(2);
   test.withOptions({ qs: { where: `begin_time = '2017-04-10T16:02:04Z'` } }).should.return200OnGet();
-  test.withOptions({ qs: { orderBy: 'updated_at asc', pageSize: 5 } }).should.return200OnGet()
+  test.withOptions({ qs: { orderBy: 'updated_at asc', pageSize: 5 } }).should.return200OnGet();
 });
--- src/test/elements/recurly/subscriptions.js
+++ src/test/elements/recurly/subscriptions.js
@@ -11,7 +11,7 @@ const plansPayloadRandom = build({ plan_code: tools.randomInt() });
 suite.forElement('billing', 'subscriptions', { payload: payload }, (test) => {
   test.should.supportNextPagePagination(2);
   test.withOptions({ qs: { where: `begin_time = '2017-04-10T16:02:04Z' and state = 'active'` } }).should.return200OnGet();
-  test.withOptions({ qs: { orderBy: 'updated_at asc', pageSize: 5 } }).should.return200OnGet()
+  test.withOptions({ qs: { orderBy: 'updated_at asc', pageSize: 5 } }).should.return200OnGet();
 
   it(`should allow CRUS for ${test.api}`, () => {
     let planId, subscriptionId;
