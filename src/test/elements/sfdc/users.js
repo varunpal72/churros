df0c8946eade2eb795c7256de6cdf9ee7c174b1e
Austin Mahan <Amahan0424@gmail.com>
2017-04-24 09:32:18 -0600

    Update users.js

--- src/test/elements/sfdc/users.js
+++ src/test/elements/sfdc/users.js
@@ -5,6 +5,6 @@ const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/users.json`);
 
 suite.forElement('crm', 'users', { payload: payload }, (test) => {
-  test.withOptions(options).should.supportCrus();
+  test.should.supportCrus();
   test.should.supportPagination();
 });
