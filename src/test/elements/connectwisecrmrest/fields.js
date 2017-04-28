c1df3f2bc8b36d75f4d1c73423ac5eb586bcfbdf
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-04-28 11:40:08 +0000

    Added churros for fields API's

--- src/test/elements/connectwisecrmrest/fields.js
+++ src/test/elements/connectwisecrmrest/fields.js
@@ -3,7 +3,19 @@
 
 const suite = require('core/suite');
 const payload = require('./assets/fields');
+const chakram = require('chakram');
 
-suite.forElement('crm', 'fields', { payload: payload, skip: true }, (test) => {
-  test.should.supportCrs();
+const options = {
+  churros: {
+    updatePayload: {
+      "displayOnScreenFlag": true,
+    }
+  }
+};
+
+suite.forElement('crm', 'fields', { payload: payload }, (test) => {
+  test.should.supportCruds(chakram.put);            //test put
+  test.withOptions(options).should.supportCruds();  //test patch
+  test.should.supportPagination();
+  test.should.supportCeqlSearch('id');
 });
