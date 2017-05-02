7ddddcd4a1df7d099308f78b157166272f192d08
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-05-02 09:07:02 +0000

    Added the churros for comment and likes API

--- src/test/elements/facebooksocial/status.js
+++ src/test/elements/facebooksocial/status.js
@@ -3,10 +3,19 @@
 const suite = require('core/suite');
 const payload = require('./assets/status');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
 const build = (overrides) => Object.assign({}, payload, overrides);
 const statusPayload = build({ message: tools.random() });
 
 suite.forElement('social', 'status', { payload:statusPayload }, (test) => {
   test.should.supportCrds();
   test.should.supportPagination();
+it.skip(`should support US for ${test.api}/{id}/comments`, () => {
+   let id;
+   return cloud.post(test.api,statusPayload)
+      .then(r => id = r.body.id)
+      .then(r => cloud.post(`${test.api}/${id}/comments`, statusPayload))
+      .then(r => cloud.post(`${test.api}/${id}/like`))
+      .then(r => cloud.delete(`${test.api}/${id}`));
+  });
 });
