a8e1d93c0c2681a185a5ef4aacf44ff2957bd377
Mayur Dhande <mayur.dhande@gslab.com>
2017-05-03 17:17:42 +0530

    Made use of faker.js

--- src/test/elements/googlesheets/spreadsheets.js
+++ src/test/elements/googlesheets/spreadsheets.js
@@ -3,6 +3,7 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const tools = require('core/tools');
+const workSheetPayload = tools.requirePayload(`${__dirname}/assets/worksheets.json`);
 
 suite.forElement('general', 'spreadsheets', (test) => {
   let spreadsheetId;
@@ -10,7 +11,6 @@ suite.forElement('general', 'spreadsheets', (test) => {
 
   it('should allow CRD for /spreadsheets/worksheets', () => {
     let versionId;
-    let workSheetPayload = { 'title': 'New Sheet' + tools.random(), 'gs:rowCount': '881', 'gs:colCount': '15' };
     return cloud.get(test.api)
       .then(r => spreadsheetId = r.body[0].spreadsheetId)
       .then(r => cloud.post(`${test.api}/${spreadsheetId}/worksheet`, workSheetPayload))
@@ -19,7 +19,7 @@ suite.forElement('general', 'spreadsheets', (test) => {
         versionId = r.body.versionId;
       })
       .then(r => cloud.get(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}`))
-      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}?versionId=${versionId}`));
+      .then(r => cloud.withOptions({ qs: { versionId: versionId } }).delete(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}`));
   });
 
   it('should allow CRUD for /spreadsheets/worksheets/rows', () => {
@@ -27,17 +27,18 @@ suite.forElement('general', 'spreadsheets', (test) => {
     spreadsheetId = '1-dx_xl6COXSGpXsuoNhMc0KGuEkMnazBXqjSr4SyT5U';
     worksheetId = 'ohgdgle';
     let rowVersionId;
-    let rowPayload = { 'gsx:sal': '2', 'gsx:ipm': '1', 'gsx:items': '15', 'gsx:name': 'AVC' };
-    let rowUpdatePayload = { 'gsx:sal': '10', 'gsx:ipm': '1', 'gsx:items': '15', 'gsx:name': 'ZYZ' + tools.random() };
     let rowId;
+    let rowPayload = tools.requirePayload(`${__dirname}/assets/rows.json`);
+
     return cloud.get(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/rows`)
       .then(r => cloud.withOptions({ qs: { where: 'updated=\'2017-04-04T13:19:26.127Z\'', orderBy: 'updated' } }).get(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/rows`))
       .then(r => cloud.post(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/row`, rowPayload))
       .then(r => {
         rowId = r.body.rowId;
         rowVersionId = r.body.versionId;
+        rowPayload["gsx:name"] = tools.random();
       })
-      .then(r => cloud.put(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/row/${rowId}?versionId=${rowVersionId}`, rowUpdatePayload))
-      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/row/${rowId}?versionId=${rowVersionId}`));
+      .then(r => cloud.withOptions({ qs: { versionId: rowVersionId } }).put(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/row/${rowId}`, rowPayload))
+      .then(r => cloud.withOptions({ qs: { versionId: rowVersionId } }).delete(`${test.api}/${spreadsheetId}/worksheet/${worksheetId}/row/${rowId}`));
   });
 });
