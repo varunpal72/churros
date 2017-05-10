7b7491ee37111e7a241e53f48a262821e364f672
Mayur Dhande <mayur.dhande@gslab.com>
2017-05-10 14:11:40 +0530

    Updates

--- src/test/elements/hubspot/contacts.js
+++ src/test/elements/hubspot/contacts.js
@@ -2,11 +2,10 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const payload = require('./assets/contacts');
-const propertiesPayload = require('./assets/contactsProperties');
 const tools = require('core/tools');
+const payload = tools.requirePayload(`${__dirname}/assets/contacts.json`);
+const propertiesPayload = require('./assets/contactsProperties');
 propertiesPayload.name = "a"+tools.random().toLowerCase();
-payload.email = tools.randomEmail();
 const options = {
   churros: {
     updatePayload: {
