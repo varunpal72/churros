ae1296221592228e179c25791c9cb0f66a8fc91d
Mithila Garud <mithila.garud@gslab.com>
2017-05-15 15:39:05 +0530

    Corrected churros for the GET /sesrach API filtering and pagination on Salesforce Files element

--- src/test/elements/sfdcdocuments/search.js
+++ src/test/elements/sfdcdocuments/search.js
@@ -6,14 +6,15 @@ const tools = require('core/tools');
 suite.forElement('documents', 'search', null, (test) => {
   test.withOptions({ qs: { text: tools.random() } }).should.return200OnGet();
 
-  test.withOptions({ qs: { pageSize: 3, page: 1 } })
-    .should.return200OnGet()
-    .expect(r.response.headers['elements-returned-count']).to.be.below(3);
+  test.should.supportPagination();
 
   test.withOptions({ qs: { startDate: '2015-06-15T09:15:04Z', endDate: '2015-06-15T09:40:04Z' } })
     .should.return200OnGet()
     .expect(r).to.have.statusCode(200);
-    .expect(r.response.headers['elements-returned-count']).to.be.below(2);
+    .expect(r.response.headers['elements-returned-count']).to.equal(2);
+
+    test.withOptions({ qs: { path: '/' } })
+      .expect(r).to.have.statusCode(200);
 
   test.withOptions({ qs: { path: '/Test folder' } })
     .expect(r).to.have.statusCode(400);
