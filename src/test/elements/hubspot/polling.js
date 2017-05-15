e4654a054eccf67b1830add590303eb2ba49f1b8
Austin Mahan <Austin@CE-MacBook-Pro-03.local>
2017-05-15 12:10:26 -0600

    commit to change branch

--- src/test/elements/hubspot/polling.js
+++ src/test/elements/hubspot/polling.js
@@ -11,5 +11,9 @@ suite.forElement('marketing', 'polling', null, (test) => {
   test.withApi('/hubs/marketing/accounts').should.supportPolling(accountsPayload);
   test.withApi('/hubs/marketing/contacts').should.supportPolling(contactsPayload);
   test.withApi('/hubs/marketing/lists').should.supportPolling(listsPayload);
+  test.withApi('/hubs/marketing/lists').should.supportPolling(listsPayload);
+  test.withApi('/hubs/marketing/lists').should.supportPolling(listsPayload);
+  test.withApi('/hubs/marketing/workflows').should.supportPolling(workflowsPayload);
+  test.withApi('/hubs/marketing/workflows').should.supportPolling(workflowsPayload);
   test.withApi('/hubs/marketing/workflows').should.supportPolling(workflowsPayload);
 });
