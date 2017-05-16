f6d8f85ab2c5d63fd3b86212c5c53c16b455f6d8
Mithila Garud <mithila.garud@gslab.com>
2017-05-16 16:02:06 +0530

    Final test cases for Salesforce Files GET /search API

--- src/test/elements/sfdcdocuments/search.js
+++ src/test/elements/sfdcdocuments/search.js
@@ -3,32 +3,22 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const chakram = require('chakram');
+const expect = require('chakram').expect;
 
 suite.forElement('documents', 'search', null, (test) => {
   test.withOptions({ qs: { text: tools.random() } }).should.return200OnGet();
 
   test.should.supportPagination();
 
-  return cloud.GET(test.api)
-    .then(r => cloud.withOptions({ qs: { startDate: '2015-06-15T09:15:04Z', endDate: '2015-06-15T09:40:04Z' } }).get(test.api))
-    .expect(r).to.have.statusCode(200)
-    .expect(r.body.length).to.equal(2);
+  test.withOptions({ qs: { path: '/' } })
+    .should.return200OnGet();
 
-  /*  test.withOptions({ qs: { startDate: '2015-06-15T09:15:04Z', endDate: '2015-06-15T09:40:04Z' } })
-      .expect(r).to.have.statusCode(200)
-      .expect(r.response.headers['elements-returned-count']).to.equal(2); */
+  test.withOptions({ qs: { startDate: '2015-06-15T09:15:04Z', endDate: '2015-06-15T09:40:04Z' } })
+    .should.return200OnGet();
 
-  return cloud.GET(test.api)
-    .then(r => cloud.withOptions({ qs: { path: '/' } }).get(test.api))
-    .expect(r).to.have.statusCode(200);
-
-  /*  test.withOptions({ qs: { path: '/' } })
-      .expect(r).to.have.statusCode(200); */
-
-  return cloud.GET(test.api)
-    .then(r => cloud.withOptions({ qs: { path: '/Test folder' } }).get(test.api))
-    .expect(r).to.have.statusCode(400);
-
-  /* test.withOptions({ qs: { path: '/Test folder' } })
-    .expect(r).to.have.statusCode(400); */
+  test.withOptions({ qs: { path: '/Test folder' } })
+    .withValidation((r) => {
+      expect(r).to.have.statusCode(400);
+    })
 });
