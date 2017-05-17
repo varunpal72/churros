f74959f5160d5418dbc921399899da20d6d9acf2
garima agarwal <garima.agarwal@gslab.com>
2017-05-17 16:10:29 +0530

    Added payload for POST /contacts

--- src/test/elements/servicecloud/contacts.js
+++ src/test/elements/servicecloud/contacts.js
@@ -3,16 +3,44 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const payload = {
+  "attributes": {
+    "accountLocked": true,
+    "canModifyEmailSignature": true,
+    "forcePasswordChange": true,
+    "passwordNeverExpires": true,
+    "permanentlyDisabled": true,
+    "staffAssignmentDisabled": true,
+    "viewsReportsDisabled": true
+  },
+  "login": tools.random(),
+  "newPassword": tools.randomStr('abcdefghijklmnopqrstuvwxyz', 10) + "@Abc123",
+  "name": {
+    "last": tools.random(),
+    "first": tools.random()
+  },
+  "profile": {
+    "id": {
+      "id": 3
+    }
+  },
+  "staffGroup": {
+    "id": {
+      "id": 100395
+    }
+  }
+};
 
-suite.forElement('helpdesk', 'contacts', {skip: true}, (test) => {
+suite.forElement('helpdesk', 'contacts', (test) => {
   const updatePayload = {
     "login": tools.random()
   };
 
-  it('should allow RUDS for contacts', () => {
+  it('should allow CRUDS for contacts', () => {
     let contactID;
     return cloud.get(test.api)
-      .then(r => contactID = r.body[0].id.id)
+      .then(r => cloud.post(test.api, payload))
+      .then(r => contactID = r.body.id.id)
       .then(r => cloud.get(`${test.api}/${contactID}`))
       .then(r => cloud.patch(`${test.api}/${contactID}`, updatePayload))
       .then(r => cloud.delete(`${test.api}/${contactID}`))
