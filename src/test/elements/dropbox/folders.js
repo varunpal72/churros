28ba0a95a4007fe38c07e16fa91e725df90b84b0
Taylor Clark <taylor@cloud-elements.com>
2017-06-01 17:12:30 -0500

    Added tests for GET /folders/metadata by root id and path

---
+++ src/test/elements/dropbox/folders.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('documents', 'folders', null, (test) => {
+
+  it('should allow GET /folders/metadata for root folder by path & ID', () => {
+    let rootPath = "/";
+    let path = { path: rootPath };
+    return cloud.withOptions({qs: path}).get("/hubs/documents/folders/metadata")
+      .then(r => encodeURIComponent(r.body.id))
+      .then(r => cloud.get(`/hubs/documents/folders/${r}/metadata`))
+      .then(r => expect(r).to.have.statusCode(200))
+  });
+});
---
+++ src/test/elements/dropboxbusinessv2/folders.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+
+suite.forElement('documents', 'folders', null, (test) => {
+
+  it('should allow GET /folders/metadata for root folder by path & ID', () => {
+    let rootPath = "/";
+    let memberId = "developer@cloud-elements.com";
+    let query = { path: rootPath };
+    return cloud.withOptions({ qs: query, headers: { "Elements-As-Team-Member": memberId } }).get("/hubs/documents/folders/metadata")
+      .then(r => encodeURIComponent(r.body.id))
+      .then(r => cloud.get(`/hubs/documents/folders/${r}/metadata`))
+      .then(r => expect(r).to.have.statusCode(200))
+  });
+});
