ab43bff0b35a4abe5d2f16d9e44adc7a7f83a21f
Austin Mahan <Austin@CE-MacBook-Pro-03.local>
2017-06-02 12:46:09 -0600

    logic

--- src/core/oauth.js
+++ src/core/oauth.js
@@ -236,15 +236,14 @@ const manipulateDom = (element, browser, r, username, password, config) => {
       browser.findElement(webdriver.By.id('username')).sendKeys(username);
       browser.findElement(webdriver.By.id('password')).sendKeys(password);
       browser.findElement(webdriver.By.id('loginBtn')).click();
-      return browser.wait(() => browser.isElementPresent(webdriver.By.xpath('/html/body/div[2]/div/div[2]/div/table/tbody/tr[1]')), 5000)
-        .thenCatch(r => true) // ignore
+      return browser.sleep(2000)
       .then(() => Array(5).fill(0).map((e, i) => i+1).reduce((acc, cur) => {
         return acc.then(() => {
           return browser.findElement(webdriver.By.xpath(`/html/body/div[2]/div/div[2]/div/table/tbody/tr[${cur}]/td[1]/small`))
             .then((element) => element.getText().then(t => t.toLowerCase().includes('churros') ? element.click() : null), (err) => {}); // ignore this
         })
       }, Promise.resolve(null)))
-      .then(() => browser.sleep(5000))
+      .then(() => browser.sleep(3000))
       .then(() => browser.getCurrentUrl())
     case 'hubspotcrm':
       browser.get(r.body.oauthUrl);
@@ -261,11 +260,15 @@ const manipulateDom = (element, browser, r, username, password, config) => {
       browser.findElement(webdriver.By.id('username')).sendKeys(username);
       browser.findElement(webdriver.By.id('password')).sendKeys(password);
       browser.findElement(webdriver.By.id('loginBtn')).click();
-      browser.wait(() => browser.isElementPresent(webdriver.By.xpath('/html/body/div[2]/div/div[2]/div/table/tbody/tr[1]')), 5000)
-        .thenCatch(r => true); // ignore
-      browser.findElement(webdriver.By.xpath('/html/body/div[2]/div/div[2]/div/table/tbody/tr[1]'))
-        .then((element) => element.click(), (err) => {}); // ignore this
-      return browser.getCurrentUrl();
+      return browser.sleep(2000)
+      .then(() => Array(5).fill(0).map((e, i) => i+1).reduce((acc, cur) => {
+        return acc.then(() => {
+          return browser.findElement(webdriver.By.xpath(`/html/body/div[2]/div/div[2]/div/table/tbody/tr[${cur}]/td[1]/small`))
+            .then((element) => element.getText().then(t => t.toLowerCase().includes('churros') ? element.click() : null), (err) => {}); // ignore this
+        })
+      }, Promise.resolve(null)))
+      .then(() => browser.sleep(3000))
+      .then(() => browser.getCurrentUrl())
     case 'instagram':
       browser.get(r.body.oauthUrl);
       browser.findElement(webdriver.By.id('id_username')).clear();
