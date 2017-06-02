0a327cec931e51f7a9780c103890f5765c6ba994
Rick Rothenberg <rick@cloud-elements.com>
2017-06-02 13:25:21 -0600

    Ooops

--- src/test/platform/elements/instances.js
+++ src/test/platform/elements/instances.js
@@ -230,6 +230,7 @@ suite.forPlatform('elements/instances', opts, (test) => {
       .then(() => cloud.get(`/incidents`, r => expect(r).to.have.statusCode(403)))
       .then(() => cloud.get(`/objects`))
       .then(() =>cloud.put(`instances/${instanceId}/enabled`))
-      .then(() => cloud.get(`/incidents`));
+      .then(() => cloud.get(`/incidents`))
+      .then(() => provisioner.delete(instanceId));
   });
 });
