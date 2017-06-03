007404b9e26432ad26e07ab1a7d02327dff86d29
Taylor Clark <taylor@cloud-elements.com>
2017-06-03 16:22:53 -0500

    improved tests to verify payloads from both endpoints

--- src/test/elements/dropboxv2/folders.js
+++ src/test/elements/dropboxv2/folders.js
@@ -5,11 +5,10 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('documents', 'folders', (test) => {
+  let rootId = "%2F";
+  let rootPath = "/";
 
-  it('should allow GET /folders/metadata for root folder by path & ID', () => {
-    let id;
-    let rootId = "%2F";
-    let rootPath = "/";
+  it('should allow GET /folders/metadata for root folder', () => {
     let query = { path: rootPath };
     return cloud.withOptions({qs: query}).get("/hubs/documents/folders/metadata")
       .then(r => {
@@ -18,14 +17,16 @@ suite.forElement('documents', 'folders', (test) => {
         expect(r.body.path).to.equal(rootPath);
         expect(r.body.directory).to.equal(true);
       });
-
-    id = encodeURIComponent(rootId);
-    return cloud.get(`/hubs/documents/folders/${rootId}/metadata`)
-    .then(r => {
-      expect(r).to.have.statusCode(200);
-      expect(r.body.id).to.equal(rootId);
-      expect(r.body.path).to.equal(rootPath);
-      expect(r.body.directory).to.equal(true);
-    });
   });
+
+  it('should allow GET /folders/{id}/metadata for root folder', () => {
+    let folderId = encodeURIComponent(rootId);
+    return cloud.get(`/hubs/documents/folders/${folderId}/metadata`)
+      .then(r => {
+        expect(r).to.have.statusCode(200);
+        expect(r.body.id).to.equal(rootId);
+        expect(r.body.path).to.equal(rootPath);
+        expect(r.body.directory).to.equal(true);
+      });
+   });
 });
