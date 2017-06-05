dd84b4f0c291108d1f943d2381accdb718fba5b3
Atul Barve <atul@cloud-elements.com>
2017-06-05 15:09:59 -0500

    Enhanced Churros for the Additional where options

--- src/test/elements/autotaskcrm/contacts.js
+++ src/test/elements/autotaskcrm/contacts.js
@@ -6,6 +6,7 @@ const payload = require('./assets/contacts');
 suite.forElement('crm', 'contacts', { payload: payload }, (test) => {
   test.should.supportCrus();
   test.withOptions({ qs: { where: 'lastName=\'Churros\'' } }).should.return200OnGet();
+  test.withOptions({ qs: { where: 'lastActivityDate > \'2012-05-25T11:13:29-0500\' and  (lastName = \'Eckle\' or lastName = \'Schrage\') ' } }).should.return200OnGet();
   test.should.supportPagination();
   test.should.supportNextPagePagination(1);
 });
