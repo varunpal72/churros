22fab95d69af403a69ba75ca52abaeaecefd97c5
Vineet Joshi <vineet@cloud-elements.com>
2017-06-09 12:16:49 -0700

    Added apostrophe search tests for MS Dynamics and Shopify.

--- src/test/elements/dynamicscrmadfs/accounts.js
+++ src/test/elements/dynamicscrmadfs/accounts.js
@@ -19,23 +19,39 @@ suite.forElement('crm', 'accounts', { payload: accountsPayload }, (test) => {
     }
   };
   var accountFloatUpdate = {
-    attributes : {
-      address1_latitude : 4.4,
-      address1_city : ""
+    attributes: {
+      address1_latitude: 4.4,
+      address1_city: ""
     }
   };
   it('Float And Null Update Test for /hubs/crm/accounts', () => {
-      var accountId = null;
-      return cloud.post(test.api, accountsPayload)
-        .then(r => accountId = r.body.id)
-        .then(() => cloud.patch(`${test.api}/${accountId}`, accountFloatUpdate,(r) => {
-          expect(r.body.attributes.address1_latitude).to.equal(4.4);
-          expect(r.body.attributes.address1_city).to.equal(undefined);
-          })
-        )
-        .then(r => cloud.delete(`${test.api}/${accountId}`));
-      });
+    var accountId = null;
+    return cloud.post(test.api, accountsPayload)
+      .then(r => accountId = r.body.id)
+      .then(() => cloud.patch(`${test.api}/${accountId}`, accountFloatUpdate, (r) => {
+        expect(r.body.attributes.address1_latitude).to.equal(4.4);
+        expect(r.body.attributes.address1_city).to.equal(undefined);
+      }))
+      .then(r => cloud.delete(`${test.api}/${accountId}`));
+  });
   test.withOptions(options).should.supportCruds();
   test.should.supportPagination();
   test.should.supportCeqlSearch('id');
+  it('should find account with quote in name', () => {
+    let accountId;
+    let accountPayload = {
+      name: "Churro's Test Account",
+      address1_city: "TestCity"
+    };
+    let query = { where: "name='Churro''s Test Account'" };
+
+    return cloud.post(test.api, accountPayload)
+      .then(r => accountId = r.body.id)
+      .then(r => cloud.withOptions({ qs: query }).get(test.api))
+      .then(r => expect(r).to.have.statusCode(200) && expect(r.body).to.not.be.null &&
+        expect(r.body).to.be.a('array') && expect(r.body).to.have.length(1) &&
+        expect(r.body[0]).to.contain.key('name') &&
+        expect(r.body[0].name).to.equal("Churro's Test Account"))
+      .then(r => cloud.delete(`${test.api}/${accountId}`));
+  });
 });
--- src/test/elements/shopify/customers.js
+++ src/test/elements/shopify/customers.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const chakram = require('chakram');
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const expect = chakram.expect;
 
 const customer = (custom) => ({
   first_name: custom.firstName || 'Bill',
@@ -14,14 +16,31 @@ suite.forElement('ecommerce', 'customers', { payload: customer({}) }, (test) => 
   test.should.supportCruds();
   it(`should allow GET for /hubs/ecommerce/customers/{id}/order and /hubs/ecommerce/customers/{id}/abandoned-checkouts`, () => {
     let customertId;
-    return cloud.post(test.api,customer({}))
-    .then(r => customertId = r.body.id)
-    .then(r => cloud.get(`${test.api}/${customertId}/orders`))
-    .then(r => cloud.get(`${test.api}/${customertId}/abandoned-checkouts`))
-    .then(r => cloud.delete(`${test.api}/${customertId}`));
+    return cloud.post(test.api, customer({}))
+      .then(r => customertId = r.body.id)
+      .then(r => cloud.get(`${test.api}/${customertId}/orders`))
+      .then(r => cloud.get(`${test.api}/${customertId}/abandoned-checkouts`))
+      .then(r => cloud.delete(`${test.api}/${customertId}`));
   });
   it('should allow GET for /customers with use of the `orderBy` parameter', () => {
-    return cloud.withOptions({qs: {orderBy: 'updated_at'}}).get(test.api)
-      .then(r => cloud.withOptions({qs: {orderBy: 'last_order_date'}}).get(test.api));
+    return cloud.withOptions({ qs: { orderBy: 'updated_at' } }).get(test.api)
+      .then(r => cloud.withOptions({ qs: { orderBy: 'last_order_date' } }).get(test.api));
+  });
+  it('should find customer with quote in first name', () => {
+    let customerId;
+    let customerPayload = {
+      first_name: "Churro's",
+      last_name: "Test"
+    };
+    let query = { where: "first_name='Churro''s'" };
+
+    return cloud.post(test.api, customerPayload)
+      .then(r => customerId = r.body.id)
+      .then(r => cloud.withOptions({ qs: query }).get(test.api))
+      .then(r => expect(r).to.have.statusCode(200) && expect(r.body).to.not.be.null &&
+        expect(r.body).to.be.a('array') && expect(r.body).to.have.length(1) &&
+        expect(r.body[0]).to.contain.key('firstName') &&
+        expect(r.body[0].firstName).to.equal("Churro's"))
+      .then(r => cloud.delete(`${test.api}/${customerId}`));
   });
 });
