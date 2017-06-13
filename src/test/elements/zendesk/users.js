14f50b8d8831c90820b374521e375e8db810d113
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-06-13 09:20:08 +0000

    Added groups api

--- src/test/elements/zendesk/users.js
+++ src/test/elements/zendesk/users.js
@@ -2,8 +2,8 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
-const payload = tools.requirePayload(`${__dirname}/assets/users.json`);
-
+const payload = require('./assets/users');
+const cloud = require('core/cloud');
 const options = {
   churros: {
     updatePayload: {
@@ -12,9 +12,23 @@ const options = {
     }
   }
 };
+const updatePayload= {
+      "name": "Userguy2",
+      "email": tools.randomEmail()
+    };
 
+payload.email = tools.randomEmail();
 
-suite.forElement('helpdesk', 'users', { payload }, (test) => {
+suite.forElement('helpdesk', 'users', { payload:payload }, (test) => {
+var id;
   test.should.supportPagination();
   test.withOptions(options).should.supportCruds();
+  before(() => cloud.post(test.api, updatePayload)
+    .then(r => id = r.body.id)); 
+
+it(`should allow GET ${test.api}/groups`, () => {
+   return cloud.get(`${test.api}/${id}/groups`);
+});
+after(() => cloud.delete(`${test.api}/${id}`));
+
 });
