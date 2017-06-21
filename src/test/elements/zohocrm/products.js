bf53a07a26c72689f40416ccf002130e621bc08c
Hanumant <hanumant.shinde@gslab.com>
2017-06-21 14:34:37 +0530

    Added test for update related record API

--- src/test/elements/zohocrm/products.js
+++ src/test/elements/zohocrm/products.js
@@ -7,6 +7,10 @@ const expect = require('chakram').expect;
 const productsPayload = tools.requirePayload(`${__dirname}/assets/products.json`);
 
 suite.forElement('crm', 'products', { payload: productsPayload }, (test) => {
+
+const objectRelatedPayload = (productId) => ({
+  "PRODUCTID": productId.toString()
+  });
   const options = {
     churros: {
       updatePayload: {
@@ -24,4 +28,14 @@ suite.forElement('crm', 'products', { payload: productsPayload }, (test) => {
   const validValues = r.body.filter(obj => obj.modified_date >='2016-06-22 11:15:20');
   expect(validValues.length).to.equal(r.body.length);
   }).should.return200OnGet();
+
+let accountId,productId;
+it(`should update product related to account`, () => {
+  return cloud.get(test.api)
+  .then(r => productId= r.body[0].id)
+  .then(r => cloud.get('/hubs/crm/accounts'))
+  .then(r => accountId = r.body[0].id)
+  .then(r =>cloud.patch(`${test.api}/accounts/${accountId}`, objectRelatedPayload(productId)))
+  });
 });
+
