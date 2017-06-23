91b83df5a7085e8b9a9bc47c7b0eae1969902e95
tyler-cloud-elements <tyler@cloud-elements.com>
2017-06-23 11:51:42 -0500

    formatting

--- src/test/elements/zohocrm/products.js
+++ src/test/elements/zohocrm/products.js
@@ -7,9 +7,8 @@ const expect = require('chakram').expect;
 const productsPayload = tools.requirePayload(`${__dirname}/assets/products.json`);
 
 suite.forElement('crm', 'products', { payload: productsPayload }, (test) => {
-
-const objectRelatedPayload = (productId) => ({
-  "PRODUCTID": productId.toString()
+  const objectRelatedPayload = (productId) => ({
+    "PRODUCTID": productId.toString()
   });
   const options = {
     churros: {
@@ -18,24 +17,26 @@ const objectRelatedPayload = (productId) => ({
       }
     }
   };
+  
   test.withOptions(options).should.supportCruds();
   test.should.supportPagination();
+  
   test
-  .withName(`should support searching ${test.api} by Modified Time`)
-  .withOptions({ qs: { where: `'Modified Time'>'2016-06-22 11:15:20'` } })
-  .withValidation((r) => {
-  expect(r).to.have.statusCode(200);
-  const validValues = r.body.filter(obj => obj.modified_date >='2016-06-22 11:15:20');
-  expect(validValues.length).to.equal(r.body.length);
-  }).should.return200OnGet();
+    .withName(`should support searching ${test.api} by Modified Time`)
+    .withOptions({ qs: { where: `'Modified Time'>'2016-06-22 11:15:20'` } })
+    .withValidation((r) => {
+      expect(r).to.have.statusCode(200);
+      const validValues = r.body.filter(obj => obj.modified_date >='2016-06-22 11:15:20');
+      expect(validValues.length).to.equal(r.body.length);
+   }).should.return200OnGet();
 
-let accountId,productId;
+
 it(`should update product related to account`, () => {
+  let accountId, productId;
   return cloud.get(test.api)
-  .then(r => productId= r.body[0].id)
-  .then(r => cloud.get('/hubs/crm/accounts'))
-  .then(r => accountId = r.body[0].id)
-  .then(r =>cloud.patch(`${test.api}/accounts/${accountId}`, objectRelatedPayload(productId)));
+    .then(r => productId= r.body[0].id)
+    .then(r => cloud.get('/hubs/crm/accounts'))
+    .then(r => accountId = r.body[0].id)
+    .then(r => cloud.patch(`${test.api}/accounts/${accountId}`, objectRelatedPayload(productId)));
   });
 });
-
