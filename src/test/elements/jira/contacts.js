df0ffa704f7611caf59acbcd64a6930232a045fc
Taylor Clark <taylor@cloud-elements.com>
2017-07-11 14:01:08 -0500

    Added testing for special characters in contacts search

--- src/test/elements/jira/contacts.js
+++ src/test/elements/jira/contacts.js
@@ -3,16 +3,29 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const cloud = require('core/cloud');
+const expect = require('chakram').expect;
 const payload = tools.requirePayload(`${__dirname}/assets/contact.json`);
+const specialCharPayload = tools.requirePayload(`${__dirname}/assets/specialCharContact.json`);
 
 suite.forElement('helpdesk', 'contacts', {payload:payload}, (test) => {
-  it(`should allow CRUDS for ${test.api}`, () => {
+  it(`should allow CRUDS for ${test.api} with Ceql search`, () => {
     let contactId;
     return cloud.post(test.api, payload)
       .then(r => contactId = r.body.key)
       .then(r => cloud.get(`${test.api}/${contactId}`))
-      .then(r => cloud.withOptions({ qs: { where:`username='test'`}}).get(test.api))
-      .then(r => cloud.withOptions({ qs: { where: `username='test'`, page: 1, pageSize: 1 }}).get(test.api))
+      .then(r => cloud.withOptions({ qs: { where: `username='test'`}}).get(test.api))
       .then(r => cloud.delete(`${test.api}/${contactId}`));
    });
+
+   test.withOptions({ qs: { where: `username='test'` }}).should.supportPagination();
+
+   it(`should allow CRUDS for ${test.api} with special characters Ceql search`, () => {
+     let contactId;
+     return cloud.post(test.api, specialCharPayload)
+       .then(r => contactId = r.body.key)
+       .then(r => cloud.withOptions({ qs: { where: `username='${specialCharPayload.displayName}'`}}).get(test.api))
+       .then(r => expect(r.body.length).to.be.equal(1))
+       .then(r => cloud.delete(`${test.api}/${contactId}`));
+    });
+
  });
