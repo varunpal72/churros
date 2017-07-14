b6757304f015ed3cb7d5f85232fecf5f7b623efc
rsbarbo <rsbarbo@gmail.com>
2017-07-13 19:04:35 -0600

    added test for new sfdc endpoint /documents/{id}/data

---
+++ src/test/elements/sfdc/documents.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+
+const documentId = "0150H00000FRylLQAT";
+const newObject = {};
+
+suite.forElement('crm', 'documents', (test) => {
+  it('retrieves an id from GET /{objectName} with Document', () => {
+    return cloud.get(test.api + '/' + documentId + '/data')
+    .then(r => {
+      newObject.body = r.body;
+      expect(newObject == true);
+    })
+  });
+});
