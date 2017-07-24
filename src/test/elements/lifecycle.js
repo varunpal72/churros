c57924a3e0b77d2f9088830e539b9d9ba0f2a9eb
AustinMahan <amahan0424@gmail.com>
2017-07-24 12:57:06 -0600

    save functionality

--- src/test/elements/lifecycle.js
+++ src/test/elements/lifecycle.js
@@ -93,7 +93,7 @@ before(() => {
         }
       })
       .catch(r => {
-        return instanceId && !argv.instance ? provisioner.delete(instanceId).then(() => terminate(r)).catch(() => terminate(r)) : terminate(r);
+        return instanceId && !(argv.instance || argv.save) ? provisioner.delete(instanceId).then(() => terminate(r)).catch(() => terminate(r)) : terminate(r);
       });
     });
 });
@@ -135,7 +135,7 @@ it.skip('should not allow provisioning with bad credentials', () => {
 });
 after(done => {
   tools.resetCleanup();
-  instanceId && !argv.instance ? provisioner
+  instanceId && !(argv.instance || argv.save) ? provisioner
         .delete(instanceId)
         .then(() => done())
         .catch(r => logger.error('Failed to delete element instance: %s', r))
