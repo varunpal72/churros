344f4004f14c2538889ff4027941467032556e6f
Mithila Garud <mithila.garud@gslab.com>
2017-07-26 18:35:36 +0530

    Fixed circle CI

--- src/test/elements/linkedin/comments.js
+++ src/test/elements/linkedin/comments.js
@@ -7,7 +7,7 @@ suite.forElement('social', 'comments', {skip: true}, (test) => {
   it('should allow get all comments on an update by a company', () => {
     return cloud.get(`/hubs/social/companies/16238355/updates/comments?updateKey=UPDATE-c16238355-6283339617404096512`)
       .withApi(`/hubs/social/companies/16238355/updates/comments?updateKey=UPDATE-c16238355-6283339617404096512`)
-      .withOptions({ qs: { page: 1, pageSize: 1 } }).;
+      .withOptions({ qs: { page: 1, pageSize: 1 } });
   });
   //test.should.supportPagination();
 });
