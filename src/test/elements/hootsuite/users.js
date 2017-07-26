11ac6c290dcf4d5dc72a685cb9498f216f384c74
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2017-07-26 08:01:02 +0530

    Updated users.js

--- src/test/elements/hootsuite/users.js
+++ src/test/elements/hootsuite/users.js
@@ -4,11 +4,6 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('social', 'users', (test) => {
-  let id;
-  it(`should allow SR for ${test.api}`, () => {
-    return cloud.get(`${test.api}`)
-      .then(r => id = r.body[0].id)
-      .then(r => cloud.get(`${test.api}/${id}`));
-  });
+  test.should.supportSr()
   test.should.supportPagination();
 });
