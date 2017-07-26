33bce35ef13d318184f8fdabcea4ea26a0d4e10e
Afrin <AfrinChakure-GSLab.com>
2017-07-26 23:00:54 +0530

    resolved circle ci

--- src/test/elements/quickbooksonprem/bill-payment-cards.js
+++ src/test/elements/quickbooksonprem/bill-payment-cards.js
@@ -2,18 +2,19 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
+const chakram = require('chakram');
+const expect = chakram.expect;
 
 suite.forElement('finance', 'bill-payment-cards', null, (test) => {
-  let id;
   it.skip('should support SRD for /hubs/finance/bill-payment-cards', () => {
     let id;
     return cloud.get(test.api)
       .then(r => {
         expect(r.body).to.not.be.empty;
         id = r.body[0].TxnID;
-        .then(r => cloud.get(`${test.api}/${id}`));
-        .then(r => cloud.delete(`${test.api}/${id}`));
-      });
+      })
+      .then(r => cloud.get(`${test.api}/${id}`))
+      .then(r => cloud.delete(`${test.api}/${id}`));
   });
   test.should.supportPagination();
 });
