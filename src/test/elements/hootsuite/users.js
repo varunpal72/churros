730bb2b3382fcf603079a09d37759248bccad737
AfrinChakure-GSLab <afrin.chakure@gslab.com>
2017-07-26 08:14:34 +0530

    added semicolon

--- src/test/elements/hootsuite/users.js
+++ src/test/elements/hootsuite/users.js
@@ -3,6 +3,6 @@
 const suite = require('core/suite');
 
 suite.forElement('social', 'users', (test) => {
-  test.should.supportSr()
+  test.should.supportSr();
   test.should.supportPagination();
 });
