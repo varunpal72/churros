a09c376e63d7dbfc4ff499f72acdea3beb9e3894
Mithila Garud <mithila.garud@gslab.com>
2017-07-26 18:27:10 +0530

    Fixed test cases

--- src/test/elements/linkedin/companies.js
+++ src/test/elements/linkedin/companies.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
-suite.forElement('social', 'companies', { skip: true }, (test) => {
+suite.forElement('social', 'companies', (test) => {
   it('should allow get all companies', () => {
     return cloud.get(`/hubs/social/companies`);
   });
--- src/test/elements/linkedin/followers.js
+++ src/test/elements/linkedin/followers.js
@@ -3,7 +3,7 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
-suite.forElement('social', 'followers', { skip: true }, (test) => {
+suite.forElement('social', 'followers',{skip: true}, (test) => {
   it('should allow get all followers', () => {
     return cloud.get(`/hubs/social/companies/16238355/followers`);
   });
