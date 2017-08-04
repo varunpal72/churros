b07242ebfd1922ded490a45297f535eb83e31fee
Hanumant <hanumant.shinde@gslab.com>
2017-08-04 12:31:04 +0530

    Added churros for sfdcservicecloud swagger validation

--- src/test/platform/docs/docs.js
+++ src/test/platform/docs/docs.js
@@ -6,8 +6,7 @@ const swaggerParser = require('swagger-parser');
 const expect = require('chakram').expect;
 
 const elementKeys = ['hubspot', 'hubspotcrm', 'dynamicscrmadfs', 'quickbooks',
-'quickbooksonprem', 'netsuitecrmv2', 'netsuiteerpv2', 'netsuitefinancev2', 'marketo', 'zendesk', 'sfdc'];
-
+'quickbooksonprem', 'netsuitecrmv2', 'netsuiteerpv2', 'netsuitefinancev2', 'marketo', 'zendesk', 'sfdc','sfdcservicecloud'];
 suite.forPlatform('docs', {}, () => {
   let hubs, elementIds;
 
