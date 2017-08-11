755068ac23a56f9b2a9cb9c24d28c5c2ea17a486
Rick Rothenberg <rick@cloud-elements.com>
2017-08-11 06:22:14 -0600

    Add churros tests for handling quotes on boolean queries

--- src/test/elements/quickbooks/customers.js
+++ src/test/elements/quickbooks/customers.js
@@ -3,6 +3,7 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/customers.json`);
+const cloud = require('core/cloud');
 
 suite.forElement('finance', 'customers', { payload: payload }, (test) => {
   const options = {
@@ -16,4 +17,10 @@ suite.forElement('finance', 'customers', { payload: payload }, (test) => {
   test.withOptions(options).should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
   test.should.supportCeqlSearch('familyName');
+  it('should support CEQL style boolean queries with single quotes', () => {
+    return cloud.withOptions({ qs: { where: "active='true'" }}).get('/customers');
+  });
+  it('should support native style boolean queries without single quotes', () => {
+    return cloud.withOptions({ qs: { where: "active=true" }}).get('/customers');
+  });
 });
