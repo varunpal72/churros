1dd2f2ccdc3a0fb3fae2f0de57f29b13be6f73ca
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-08-17 17:31:37 +0530

    added skip

--- src/test/elements/netsuitefinancev2/subsidiaries.js
+++ src/test/elements/netsuitefinancev2/subsidiaries.js
@@ -4,7 +4,7 @@ const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/subsidiaries.json`);
 
-suite.forElement('finance', 'subsidiaries', { payload: payload }, (test) => {
+suite.forElement('finance', 'subsidiaries', { payload: payload , skip : true }, (test) => {
   	test.should.supportCruds();
   	test.should.supportCeqlSearch('email');
 });
