15d0265c1018538f55820195429a58ba5cac595e
Mithila Garud <mithila.garud@gslab.com>
2017-08-18 18:53:13 +0530

    incorporated review comments

--- src/test/elements/zendesk/users.js
+++ src/test/elements/zendesk/users.js
@@ -12,24 +12,29 @@ const options = {
     }
   }
 };
-const updatePayload= {
-      "name": "Userguy2",
-      "email": tools.randomEmail()
-    };
+const updatePayload = {
+  "name": "Userguy2",
+  "email": tools.randomEmail()
+};
 
 payload.email = tools.randomEmail();
 
-suite.forElement('helpdesk', 'users', { payload:payload }, (test) => {
-var id;
+suite.forElement('helpdesk', 'users', { payload: payload }, (test) => {
+  var id;
   test.should.supportPagination();
   test.withOptions(options).should.supportCruds();
   before(() => cloud.post(test.api, updatePayload)
     .then(r => id = r.body.id));
-
-test.withApi(`${test.api}/${id}/groups`).should.supportPagination();
-it(`should allow GET ${test.api}/groups`, () => {
-   return cloud.get(`${test.api}/${id}/groups`);
-});
-after(() => cloud.delete(`${test.api}/${id}`));
-
+  before(() =>
+    cloud.get(test.api, r => {
+      if (r.body.length > 0) {
+        id = r.body[0].id;
+      } else {}
+    })
+  );
+  it(`should allow GET ${test.api}/${id}/groups`, () => {
+    return cloud.get(`${test.api}/${id}/groups`)
+      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } })
+        .get(test.api + '/' + id + '/groups'))
+  });
 });
