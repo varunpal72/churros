e308257ecc0fb328d7a4cc6773ef07f47c03df12
Atul Barve <atul@cloud-elements.com>
2017-08-24 13:38:49 -0500

    Can u check if this is correct.

--- src/test/elements/quickbooksonprem/vendor-credits.js
+++ src/test/elements/quickbooksonprem/vendor-credits.js
@@ -11,10 +11,10 @@ suite.forElement('finance', 'vendor-credits', { payload: payload }, (test) => {
     return cloud.post(test.api, payload)
       .then(r => id = r.body.TxnID)
       .then(r => cloud.get(test.api))
-      .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 1 } }).get(test.api))
       .then(r => cloud.get(`${test.api}/${id}`))
       .then(r => updatePayload.EditSequence = r.body.EditSequence)
       .then(r => cloud.patch(`${test.api}/${id}`, updatePayload))
       .then(r => cloud.delete(`${test.api}/${id}`));
   });
+  test.should.supportPagination()
 });
