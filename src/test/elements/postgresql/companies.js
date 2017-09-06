4ab658ab31d748cb9b8d964e0c15bf3c288bec05
NeetaBirajdar-GSLab <neeta.birajdar@gslab.com>
2017-09-06 16:41:44 +0530

    Fix contact failure

--- src/test/elements/postgresql/companies.js
+++ src/test/elements/postgresql/companies.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const suite = require('core/suite');
-const payload = require('./assets/companies');
 const tools = require('core/tools');
-const build = (overrides) => Object.assign({}, payload, overrides);
-const companyPayload = build({ id: tools.randomInt(), name: tools.random() });
-suite.forElement('db', 'companies', { payload: companyPayload }, (test) => {
+const payload = tools.requirePayload(`${__dirname}/assets/companies.json`);
+
+suite.forElement('db', 'companies', { payload: payload }, (test) => {
   const options = {
     churros: {
       updatePayload: {
