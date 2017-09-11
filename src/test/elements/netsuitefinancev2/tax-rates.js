fc46beba8389b7697072cbdb1f3554381ca6f32b
NeetaBirajdar-GSLab <neeta.birajdar@gslab.com>
2017-09-11 13:24:05 +0530

    fixed tax-rate and tax-codes

--- src/test/elements/netsuitefinancev2/tax-rates.js
+++ src/test/elements/netsuitefinancev2/tax-rates.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const suite = require('core/suite');
-
+const cloud = require('core/cloud');
 suite.forElement('finance', 'tax-rates', (test) => {
-  test.should.supportSr();
+
+  it('should shupport GET /tax-rates and GET /tax-rates/:id', () => {
+    let taxId;
+    return cloud.get(test.api)
+      .then(r => taxId = r.body[0].internalId)
+      .then(r => cloud.get(`${test.api}/${taxId}`));
+  });
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
-  test.withOptions({ skip: true, qs: { where: 'preferred = \'true\'' } }).should.return200OnGet();
+  test.withOptions({ qs: { where: 'state = \'CA\'' } }).should.return200OnGet();
 });
