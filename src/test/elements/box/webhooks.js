519717f289428fcae8c5975fef3c2db157b2944b
root <root@example.com>
2017-09-12 18:16:08 +0530

    Added churros for webhooks

---
+++ src/test/elements/box/webhooks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const faker = require('faker');
+
+suite.forElement('documents', 'webhooks', null, (test) => {
+  it('should allow CRUDS for webhooks', () => {
+    let fileId,webhookId;
+    let path = __dirname + '/../assets/brady.jpg';
+    let query = { path: `/brady-${faker.random.number()}.jpg` };
+    return cloud.withOptions({ qs: query }).postFile('/hubs/documents/files', path)
+      .then(r => fileId = r.body.id)
+      .then(r => cloud.post(test.api,{"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED","METADATA_INSTANCE.UPDATED"                                     ,"METADATA_INSTANCE.DELETED"]}))
+      .then(r => webhookId = r.body.id)
+      .then(r => cloud.put(test.api+ '/' + webhookId,{"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED",
+                                                     "METADATA_INSTANCE.UPDATED","METADATA_INSTANCE.DELETED"]}))
+      .then(r => cloud.get(test.api))
+      .then(r => cloud.get(test.api + '/' + webhookId))
+      .then(r => cloud.delete(test.api + '/' + webhookId))
+      .then(r => cloud.delete('/hubs/documents/files/' + fileId));
+  });
+});
+
+
