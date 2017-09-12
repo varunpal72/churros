ea2fa8ce6820698813915b731f1922595b96f3e0
Travis McChesney <travis@travismcchesney.com>
2017-09-12 10:43:19 -0600

    New tests.

--- src/test/platform/accounts/accounts.js
+++ src/test/platform/accounts/accounts.js
@@ -9,6 +9,7 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forPlatform('accounts', { payload: account, schema: accountSchema }, (test) => {
+
   test.should.supportCrud();
   test.withOptions({ schema: accountsSchema }).should.supportS();
   test.should.supportCeqlSearch('name');
@@ -16,28 +17,39 @@ suite.forPlatform('accounts', { payload: account, schema: accountSchema }, (test
   test.should.return404OnPatch(-1);
   test.should.return404OnGet(-1);
 
-  let accountId;
-  let roleKey = 'sub-account-instances';
+  let roleKey = 'admin';
   let payload = {
-    name: 'Sub-account Instances',
-    key: roleKey,
-    description: 'Allow this admin account to view/use sub-account instances'
+    key: roleKey
   };
 
-  it('should support creating sub account instances role by admin account', () => {
-    return cloud.get('/accounts')
-      .then(r => {
-        expect(r.body.length).to.be.at.least(1) &&
-          expect(r.body[0]).to.contain.key('defaultAccount') &&
-          expect(r.body[0].defaultAccount).to.equal(true);
-        accountId = r.body[0].id;
-      })
-      .then(r => cloud.post(`/accounts/${accountId}/roles`, payload)
-        .then(r => expect(r).to.have.statusCode(200))
-        .then(r => cloud.delete(`/accounts/${accountId}/roles/${roleKey}`)));
+  describe('account roles', () => {
+    let accountId;
+    before(() => {
+      return cloud.post(`/accounts/`, account)
+        .then(r => {
+          accountId = r.body.id;
+          console.log(`Account ID: ${accountId}`);
+        });
+    });
+
+    test.withApi(`/accounts/${accountId}/roles`).should.supportCrd();
+
+    it.skip('should support granting admin role by admin account', () => {
+      return cloud.get('/accounts')
+        .then(r => {
+          expect(r.body.length).to.be.at.least(1) &&
+            expect(r.body[0]).to.contain.key('defaultAccount') &&
+            expect(r.body[0].defaultAccount).to.equal(true);
+          accountId = r.body[0].id;
+        })
+        .then(r => cloud.post(`/accounts/${accountId}/roles`, payload)
+          .then(r => expect(r).to.have.statusCode(200))
+          .then(r => cloud.delete(`/accounts/${accountId}/roles/${roleKey}`)));
+    });
   });
 
-  afterEach(() =>
+
+  after(() =>
     chakram.get('/accounts')
     .then(r => Promise.all(r.body.filter(a => !a.defaultAccount).map(a => chakram.delete(`/accounts/${a.id}`))))
   );
