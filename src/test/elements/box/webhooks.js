2b987c95a1dc2cb263779102a45dbb088fb95f76
root <root@example.com>
2017-09-13 15:40:01 +0530

    Used CRUDS function it cloud.js

--- src/test/elements/box/webhooks.js
+++ src/test/elements/box/webhooks.js
@@ -3,23 +3,20 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const faker = require('faker');
+const chakram = require('chakram');
 
+const webhookPayload = (fileId) => ({"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED","METADATA_INSTANCE.UPDATED","METADATA_INSTANCE.DELETED"]});
 suite.forElement('documents', 'webhooks', null, (test) => {
-  it('should allow CRUDS for webhooks', () => {
-    let fileId,webhookId;
-    let path = __dirname + '/../assets/brady.jpg';
-    let query = { path: `/brady-${faker.random.number()}.jpg` };
-    return cloud.withOptions({ qs: query }).postFile('/hubs/documents/files', path)
-      .then(r => fileId = r.body.id)
-      .then(r => cloud.post(test.api,{"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED","METADATA_INSTANCE.UPDATED"                                     ,"METADATA_INSTANCE.DELETED"]}))
-      .then(r => webhookId = r.body.id)
-      .then(r => cloud.put(test.api+ '/' + webhookId,{"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED",
-                                                     "METADATA_INSTANCE.UPDATED","METADATA_INSTANCE.DELETED"]}))
-      .then(r => cloud.get(test.api))
-      .then(r => cloud.get(test.api + '/' + webhookId))
-      .then(r => cloud.delete(test.api + '/' + webhookId))
-      .then(r => cloud.delete('/hubs/documents/files/' + fileId));
+  let fileId,path = __dirname + '/../assets/brady.jpg';
+  let query = { path: `/brady-${faker.random.number()}.jpg` };;
+  before(() => cloud.withOptions({ qs: query }).postFile('/hubs/documents/files', path)
+  .then(r => fileId = r.body.id)
+  );
+  it('should support CRUDS for webhooks', () => {
+    return cloud.cruds(test.api,webhookPayload(fileId),null,chakram.put);
   });
+  after(() => cloud.delete('/hubs/documents/files/' + fileId));
 });
+
 
 
