46dbb4e04646fa36d525fbdc51b82b2b0576e25b
wyselm <laura@cloud-elements.com>
2017-09-12 22:20:40 -0600

    clean up

--- src/test/platform/formulas/executions.js
+++ src/test/platform/formulas/executions.js
@@ -87,10 +87,9 @@ const isSkippedForBode = () => {
 suite.forPlatform('formulas', { name: 'formula executions' }, (test) => {
   let closeioId, dropboxId;
   before(() => {
-    return provisioner.create('closeio', { 'event.notification.enabled': true, 'event.vendor.type': 'polling', 'event.poller.refresh_interval': 999999999 })
-    // provisioner.create('dropbox')
-    //   .then(r => dropboxId = r.body.id)
-    //   .then(r => provisioner.create('closeio', { 'event.notification.enabled': true, 'event.vendor.type': 'polling', 'event.poller.refresh_interval': 999999999 }))
+    return provisioner.create('dropbox')
+      .then(r => dropboxId = r.body.id)
+      .then(r => provisioner.create('closeio', { 'event.notification.enabled': true, 'event.vendor.type': 'polling', 'event.poller.refresh_interval': 999999999 }))
       .then(r => closeioId = r.body.id)
       .catch(e => {
         console.log(`Failed to finish before()...${e}`);
@@ -101,7 +100,7 @@ suite.forPlatform('formulas', { name: 'formula executions' }, (test) => {
   after((done) => {
     if (!closeioId) done();
     provisioner.delete(closeioId)
-      // .then(r => provisioner.delete(dropboxId))
+      .then(r => provisioner.delete(dropboxId))
       .catch(e => {
         console.log(`Failed to finish after()...${e}`);
       })
