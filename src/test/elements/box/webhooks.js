dc785c24b6cce20fcd45ef4500ab3395c82ab388
root <root@example.com>
2017-09-13 15:46:17 +0530

    Resolved CircleCi fail

--- src/test/elements/box/webhooks.js
+++ src/test/elements/box/webhooks.js
@@ -8,7 +8,7 @@ const chakram = require('chakram');
 const webhookPayload = (fileId) => ({"target":{"id":fileId,"type":"file"},"triggers":["METADATA_INSTANCE.CREATED","METADATA_INSTANCE.UPDATED","METADATA_INSTANCE.DELETED"]});
 suite.forElement('documents', 'webhooks', null, (test) => {
   let fileId,path = __dirname + '/../assets/brady.jpg';
-  let query = { path: `/brady-${faker.random.number()}.jpg` };;
+  let query = { path: `/brady-${faker.random.number()}.jpg` };
   before(() => cloud.withOptions({ qs: query }).postFile('/hubs/documents/files', path)
   .then(r => fileId = r.body.id)
   );
