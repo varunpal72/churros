8a7ea5e24f0557c13424e0992ed4a4251cd271a2
Ross Caubarreaux <rcaubarreaux@gmail.com>
2017-09-14 12:27:31 -0600

    * Added more test cases for where clause in incidents.

--- src/test/elements/connectwisecrmrest/incidents.js
+++ src/test/elements/connectwisecrmrest/incidents.js
@@ -3,6 +3,7 @@
 const cloud = require('core/cloud');
 const suite = require('core/suite');
 const tools = require('core/tools');
+const expect = require('chakram').expect;
 const payload = require('./assets/incidents');
 const oPayload = require('./assets/organizations');
 const build = (overrides) => Object.assign({}, oPayload, overrides);
@@ -32,6 +33,46 @@ suite.forElement('crm', 'incidents', { payload: payload }, (test) => {
     });
 
     test.withOptions(options).should.supportCruds();
-    test.withOptions({contactPhoneNumber: '555-555-5555'}).should.supportCeqlSearch('contactPhoneNumber');
+    test.should.supportCeqlSearch('id');
+    test.should.supportCeqlSearch("summary");
+    test.withOptions({qs: {where: "contactPhoneNumber=\"555-5555\""}}).should.supportCeqlSearch('contactPhoneNumber');
     test.should.supportPagination();
+
+    it(`should support searching ${test.api} by field company.id`, () => {
+        let id, value;
+        return cloud.post(test.api, payload)
+            .then(r => {
+                id = r.body.id;
+                value = r.body['company'].id;
+                const myOptions = {qs: {where: `company.id=${organizationId}`}};
+                return cloud.withOptions(myOptions).get(test.api, (r) => {
+                    expect(r).to.have.statusCode(200);
+                    expect(r.body.filter(obj => obj['company'].id === value).length).to.equal(r.body.length);
+                });
+            })
+            .then(r => cloud.delete(test.api + '/' + id));
+    });
+
+    it(`should support searching ${test.api} by field lastUpdated`, () => {
+        let id, value;
+        return cloud.post(test.api, payload)
+            .then(r => {
+                id = r.body.id;
+                value = r.body['lastUpdated'];
+                const myOptions = {qs: {where: "lastUpdated>'2016-08-20T18:04:26Z'"}};
+                return cloud.withOptions(myOptions).get(test.api, (r) => {
+                    expect(r).to.have.statusCode(200);
+                    expect(r.body.filter(obj => obj['lastUpdated'] === value).length).to.equal(r.body.length);
+                });
+            })
+            .then(r => cloud.delete(test.api + '/' + id));
+    });
+
+    it(`should support retrieval of related time-entries via ${test.api}/{id}/time-entries`, () => {
+        let indicentId;
+        return cloud.post(test.api, payload)
+            .then(r => indicentId = r.body.id)
+            .then(() => cloud.get(`${test.api}/${indicentId}/time-entries`))
+            .then(r => cloud.delete(`${test.api}/${indicentId}`));
+    });
 });
