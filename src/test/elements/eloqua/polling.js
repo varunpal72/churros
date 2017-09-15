c2623f9d65849068003908663180d407be287892
Mithila Garud <mithila.garud@gslab.com>
2017-09-15 17:53:04 +0530

    Added churros test cases for new resource custom-objects on Eloqua

--- src/test/elements/eloqua/polling.js
+++ src/test/elements/eloqua/polling.js
@@ -3,7 +3,12 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const contactsPayload = tools.requirePayload(`${__dirname}/assets/contacts.json`);
+const customObjectsPayload =  {
+    "name": tools.randomStr('abcdefghijklmnopqrstuvwxyz11234567890', 10)
+};
+
 
 suite.forElement('marketing', 'polling', null, (test) => {
   test.withApi('/hubs/marketing/contacts').should.supportPolling(contactsPayload, 'contacts');
+  test.withApi('/hubs/marketing/custom-objects').should.supportPolling(customObjectsPayload, 'customObjects');
 });
