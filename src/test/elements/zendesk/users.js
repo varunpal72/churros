5f066e796eaf9023539e57ae37896a5537bde9d4
Rick Rothenberg <rick@cloud-elements.com>
2017-09-16 09:51:45 -0600

    Test for zendesk fix for handling unicode

--- src/test/elements/zendesk/users.js
+++ src/test/elements/zendesk/users.js
@@ -3,6 +3,8 @@
 const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/users.json`);
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
 
 const options = {
   churros: {
@@ -17,4 +19,8 @@ const options = {
 suite.forElement('helpdesk', 'users', { payload }, (test) => {
   test.should.supportPagination();
   test.withOptions(options).should.supportCruds();
+  it('should handle unicode', () => {
+    return cloud.withOptions({qs:{where: "name='%F0%9F%92%A9'"}}).get('/users')
+      .then(r => expect(r.body.length).to.equal(0));
+  });
 });
