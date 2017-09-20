d8d5904efedd99fee146fd3bb1e8354ef329bb99
MadhuriKadam-GSLab <madhuri.kadam@gslab.com>
2017-09-20 09:43:07 +0000

    add skip in vendor as delete operation just inactive the vendor object not deleting the object

--- src/test/elements/netsuitefinancev2/vendors.js
+++ src/test/elements/netsuitefinancev2/vendors.js
@@ -4,7 +4,7 @@ const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/vendors.json`);
 const payload1 = tools.requirePayload(`${__dirname}/assets/vendors.json`);
-suite.forElement('finance', 'vendors', {payload: payload }, (test) => {
+suite.forElement('finance', 'vendors', {skip: true, payload: payload}, (test) => {
   test.should.supportCruds();
 	test.withOptions({ qs: { page: 1, pageSize: 5}}).should.return200OnGet();
   test.should.supportCeqlSearch('id',payload1);
