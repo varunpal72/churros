92d8ae4e3b0fa9ddc2782b8190d8aa73ff651466
root <hanumant.shinde@gslab.com>
2017-09-25 18:37:13 +0530

    Added churros for extended resource

---
+++ src/test/elements/dropbox/extended-resource.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const expect = require('chakram').expect;
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const newResource = require('./assets/newResource.json');
+
+// Test for extending dropbox and invoking the extended resource
+suite.forElement('document', 'extended-resource', {}, (test) => {
+  let newResourceId;
+  // Add resource to
+  before(() => cloud.post(`elements/dropbox/resources`, newResource)
+    .then(r => newResourceId = r.body.id));
+
+  //delete new/overide resource should work fine
+  after(() => cloud.delete(`elements/dropbox/resources/${newResourceId}`));
+
+  it('should test newly added account resource', () => {
+      const options = { qs: { path: 'churros/myfile1.txt' } };
+      return cloud.withOptions(options).get(`extended-resource`)
+      .then(r => {
+        expect(r.body).to.not.be.empty;
+      });
+  });
+});
