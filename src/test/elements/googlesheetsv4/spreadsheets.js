3598bcf2a230d02a39e8ad51e1bb8ccb798340d1
seshuede <seshuede@gmail.com>
2017-09-26 14:34:08 -0500

    remove unused variables

--- src/test/elements/googlesheetsv4/spreadsheets.js
+++ src/test/elements/googlesheetsv4/spreadsheets.js
@@ -17,9 +17,7 @@ const rowUpdatePayload = tools.requirePayload(`${__dirname}/assets/rowupdate.jso
 
 suite.forElement('general', 'spreadsheets', (test) => {
 let spreadsheetId;
-let worksheetId;
 let sheetId;
-let sheetTitle;
 let destsheetId;
 let worksheetName;
 let range;
@@ -31,7 +29,7 @@ let rowId;
     return cloud.get(test.api)
     .then(r => cloud.post(test.api, spreadSheetPayload))
     .then(r => spreadsheetId = r.body.spreadsheetId)
-    .then(r => cloud.put(`${test.api}/${spreadsheetId}`, spreadSheetUpdatePayload))
+    .then(r => cloud.put(`${test.api}/${spreadsheetId}`, spreadSheetUpdatePayload));
   });
 
   it('should allow CRUD for /spreadsheets/{spreadsheetId}/worksheets',() =>{
@@ -39,7 +37,7 @@ let rowId;
     .then(r =>sheetId = r.body[0].sheetId)
     .then(r => cloud.put(`${test.api}/${spreadsheetId}/worksheets/${sheetId}`,workSheetUpdatePayload ))
     .then(r => cloud.get(`${test.api}/${spreadsheetId}/worksheets/${sheetId}`))
-    .then(r => worksheetName = r.body.title)
+    .then(r => worksheetName = r.body.title);
     //.then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheets/${sheetId}`))
    });
 
@@ -48,22 +46,22 @@ let rowId;
       return cloud.post(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/multiples`,multipleCellsPayload)
       .then(r => range = 'A1')
       .then(r => cloud.put(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/multiples/${range}`,multipleCellsUpdatePayload))
-      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/multiples/${range}`))
+      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/multiples/${range}`));
      });
 
      it('should allow CRUD for /spreadsheets/{spreadsheetId}/worksheets{worksheetName}/rows', () => {
       return cloud.post(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/rows`,rowPayload)
       .then(r => rowId = 'C3')
       .then(r => cloud.put(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/rows/${rowId}`,rowUpdatePayload))
-      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/rows/${rowId}`))
+      .then(r => cloud.delete(`${test.api}/${spreadsheetId}/worksheets/${worksheetName}/rows/${rowId}`));
      });
 
 
 
      it('should allow copy worksheet /spreadsheets/{spreadsheetId}/worksheets/{sheetId}:copyTo', () =>{
        return cloud.post(test.api, newspreadSheetPayload)
-       .then(r => {destsheetId = r.body.spreadsheetId})
-       .then(r => cloud.post(`${test.api}/${spreadsheetId}/worksheets/${sheetId}/copy`,destSpreadsheetPayload))
+       .then(r => {destsheetId = r.body.spreadsheetId});
+       .then(r => cloud.post(`${test.api}/${spreadsheetId}/worksheets/${sheetId}/copy`,destSpreadsheetPayload));
         });
 
 });
