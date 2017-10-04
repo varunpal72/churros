52f4f6364d965f8ce5418f49b550b6d882a92273
root <hanumant.shinde@gslab.com>
2017-10-04 15:10:52 +0530

    tests for dropbox and dropboxv2 extendable

--- src/test/elements/dropboxv2/extended-resource.js
+++ src/test/elements/dropboxv2/extended-resource.js
@@ -5,15 +5,15 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const newResource = require('./assets/newResource.json');
 
-// Test for extending dropbox and invoking the extended resource
+// Test for extending dropboxv2 and invoking the extended resource
 suite.forElement('document', 'extended-resource', {}, (test) => {
   let newResourceId;
   // Add resource to
-  before(() => cloud.post(`elements/dropbox/resources`, newResource)
+  before(() => cloud.post(`elements/dropboxv2/resources`, newResource)
     .then(r => newResourceId = r.body.id));
 
   //delete new/overide resource should work fine
-  after(() => cloud.delete(`elements/dropbox/resources/${newResourceId}`));
+  after(() => cloud.delete(`elements/dropboxv2/resources/${newResourceId}`));
 
   it('should test newly added account resource', () => {
       return cloud.get(`extended-resource`)
