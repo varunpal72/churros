197bb42d9f709e7dd58c0b238f81ccc4ecf5b454
Mithila Garud <mithila.garud@gslab.com>
2017-10-05 18:09:04 +0530

    Added comments on skipped test cases

--- src/test/elements/linkedin/users.js
+++ src/test/elements/linkedin/users.js
@@ -4,6 +4,7 @@ const suite = require('core/suite');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/shares.json`);
 
+//Need to skip as there is no delete API
 suite.forElement('social', 'user/me', { payload: payload, skip: true }, (test) => {
   test.withOptions({ qs: { language: `en_US` } }).should.return200OnGet();
   test.withApi(`${test.api}/detailed`).withOptions({ qs: { language: `en_US` } }).should.return200OnGet();
