73a4f15fabda93c383c5a9ccbad5248f76b50ef9
seshuede <seshuede@gmail.com>
2017-10-10 10:30:15 -0500

    Churros for CustomRecords and ExpenseReports

---
+++ src/test/elements/netsuitefinancev2/customrecords.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const tools = require('core/tools');
+const customRecordsPayload = require('./assets/customrecords');
+
+
+suite.forElement('finance', 'custom-record-types', (test) => {
+
+    it('should allow CRUDS /hubs/finance/customrecords', () => {
+      let customRecordTypeId = 478;
+      let customRecordId;
+      return cloud.post(`${test.api}/${customRecordTypeId}/custom-records`, customRecordsPayload)
+      .then(r => customRecordId = r.body.internalId)
+      .then(r => cloud.get(`${test.api}/${customRecordTypeId}/custom-records`))
+      .then(r => cloud.get(`${test.api}/${customRecordTypeId}/custom-records/${customRecordId}`))
+      .then(r => cloud.patch(`${test.api}/${customRecordTypeId}/custom-records/${customRecordId}`,customRecordsPayload))
+      .then(r => cloud.delete(`${test.api}/${customRecordTypeId}/custom-records/${customRecordId}`));
+
+    });
+});
