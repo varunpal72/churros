59a5862dc0029cc9b43e5e4116da146af6989962
seshuede <seshuede@gmail.com>
2017-10-10 10:25:11 -0500

    Churros ExpenseReports and CustomRecords  (Netsuite)

---
+++ src/test/elements/netsuitefinancev2/expense-reports.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const suite = require('core/suite');
+const cloud = require('core/cloud');
+const tools = require('core/tools');
+const expenseReportsPayload = require('./assets/expense-reports');
+
+
+suite.forElement('finance', 'expense-reports', (test) => {
+  	
+    it('should allow CRUDS /hubs/finance/expense-reports', () => {
+      let internalId;
+      return cloud.get(test.api)
+      .then(r => cloud.post(test.api, expenseReportsPayload))
+      .then(r => internalId = r.body.internalId)
+       .then(r => cloud.get(`${test.api}/${internalId}`))
+       .then(r => cloud.patch(`${test.api}/${internalId}`, {}))
+       .then(r => cloud.delete(`${test.api}/${internalId}`));
+    });
+});
