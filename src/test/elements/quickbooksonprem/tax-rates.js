22811ebc4479995440c03bacdd451fbc7c97ab77
Sonali <sonali.soni@gslab.com>
2017-10-13 08:30:13 -0400

    Update tax-rates.js
    Removed skip.

--- src/test/elements/quickbooksonprem/tax-rates.js
+++ src/test/elements/quickbooksonprem/tax-rates.js
@@ -3,9 +3,9 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 
-//skipping the test as pagination is not proper from element itself.
+//Pagination is not working for element
 suite.forElement('finance', 'tax-rates', null, (test) => {
-  it.skip('should support S for /hubs/finance/tax-rates', () => {
+  it('should support S for /hubs/finance/tax-rates', () => {
     return cloud.get(test.api);
   });
   test.should.supportPagination();
