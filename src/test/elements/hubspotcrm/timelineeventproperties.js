4f9e677362ecca653e7a2ad1609bd0d6fb26ae9c
seshuede <seshuede@gmail.com>
2017-10-17 16:51:53 -0500

    removed static id

--- src/test/elements/hubspotcrm/timelineeventproperties.js
+++ src/test/elements/hubspotcrm/timelineeventproperties.js
@@ -7,9 +7,10 @@ const payload = require('./assets/timelineeventproperties');
 suite.forElement('crm', 'timeline-event-types', (test) => {
 
   it('should test CUDS for /timelineeventtypes/{id}/properties ', () => {
-    let id = 19073,
-      propertyId;
-    return cloud.get(`${test.api}/${id}/properties`)
+    let id,propertyId;
+    return cloud.get(test.api)
+      .then(r => id = r.body[0].id)
+      .then(r => cloud.get(`${test.api}/${id}/properties`))
       .then(r => cloud.post(`${test.api}/${id}/properties`, payload))
       .then(r => propertyId = r.body.id)
       // In payload 'name' must be unique , so for every test make sure it is different
--- src/test/elements/hubspotcrm/timelineevents.js
+++ src/test/elements/hubspotcrm/timelineevents.js
@@ -7,9 +7,10 @@ const payload = require('./assets/timelineevents');
 suite.forElement('crm', 'timeline-event-types', (test) => {
 
   it('should test CRU for /timelineeventtypes/{id}/events ', () => {
-    let id = 19073,
-      eventId;
-    return cloud.post(`${test.api}/${id}/events`, payload)
+    let id,eventId;
+    return cloud.get(test.api)
+      .then(r => id = r.body[0].id)
+      .then(r => cloud.post(`${test.api}/${id}/events`, payload))
       .then(r => eventId = payload.id)
       .then(r => cloud.patch(`${test.api}/${id}/events/${eventId}`, payload))
       .then(r => cloud.get(`${test.api}/${id}/events/${eventId}`));
