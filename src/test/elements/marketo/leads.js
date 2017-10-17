089ed96e044fdac7cd1ec411011471e430df2ba6
sarahhealey <sarahdhealey@gmail.com>
2017-10-16 18:27:58 -0600

    fixed test allow C for programs/{programId}/leads

--- src/test/elements/marketo/leads.js
+++ src/test/elements/marketo/leads.js
@@ -2,16 +2,29 @@
 
 const suite = require('core/suite');
 const leadsPayload = require('./assets/leads');
-const programsPayload = require('./assets/programs');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
 
-suite.forElement('marketing', 'programs', { payload: programsPayload }, (test) => {
+const payload = {
+  "name": "Name for Program Test" + tools.random(),
+  "channel": "Email Send",
+  "type": "Email",
+  "folder": {
+    "type":"Folder",
+    "id":33
+  }
+};
+
+suite.forElement('marketing', 'programs', { payload: payload }, (test) => {
   it('should allow C for programs/{programId}/leads', () => {
-    let programId;
+    let program
+    // let programId;
+    // let programName;
     leadsPayload.email = "sampleTestName" + tools.random() + "@test.com";
     return cloud.post(test.api, payload)
-      .then(r => programId = r.body.id)
-      .then(r => cloud.post(`${test.api}/${programId}/leads`, leadsPayload()))
-      .then(r => cloud.delete(`${test.api}/${programId}`));
+      .then(r => program = r.body[0])
+      .then(r => cloud.post(`${test.api}/${program.name}/sync`, leadsPayload))
+      .then(r => cloud.delete(`${test.api}/${program.id}`));
   });
 });
 
