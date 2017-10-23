0f7b2d41cae326f9051e576800a4734664578e02
Jamie Thorson <jamie@cloud-elements.com>
2017-10-23 14:32:19 -0600

    made changes

--- src/test/elements/salesforcemarketingcloud/attribute-sets.js
+++ src/test/elements/salesforcemarketingcloud/attribute-sets.js
@@ -1,15 +1,12 @@
 'use strict';
 const cloud = require('core/cloud');
 const suite = require('core/suite');
-//
-// suite.forElement('marketing', 'attribute-sets', (test) => {
-//   test.should.return200OnGet()
-// });
 
 suite.forElement('marketing', 'attribute-sets', (test) => {
-it('should allow R for /data-events', () => {
-let name;
-  return cloud.get(test.api)
-  .then(r => cloud.get(`${test.api}/${name}`));
-});
+  it('should allow R for /attribute-sets', () => {
+    let name;
+    return cloud.get(test.api)
+      .then(r => name = r.body.items[0].name.value)
+      .then(r => cloud.get(`${test.api}/${name}`));
+  });
 });
--- src/test/elements/salesforcemarketingcloud/data-events.js
+++ src/test/elements/salesforcemarketingcloud/data-events.js
@@ -3,8 +3,7 @@ const cloud = require('core/cloud');
 const suite = require('core/suite');
 //Note: DataEventPayload does not generate values dynamically but it is techinically un upsert so it is okay To post repeate values
 const DataEventPayload = require('./assets/DataEvent');
-const updatePayload =
-  {
+const updatePayload = {
   "values": {
     "FirstName": "hollagin",
     "FollowerCount": 0,
@@ -14,18 +13,24 @@ const updatePayload =
   }
 };
 const incrementPayload = {
-  "step":3
+  "step": 3
 };
 
 //data-events are very account unique I set the primary key in our account to be First Name and added data that exists under that.
 suite.forElement('marketing', 'data-events', (test) => {
   it('should allow CU for /data-events', () => {
-    let eventId='70F95B23-E336-4B82-966F-C42B5AC44ABB';
-    let key= 'First Name:someone@example.com';
-    let columnName='Number';
-    return cloud.post(test.api,DataEventPayload)
- 	  .then(r => cloud.put(`${test.api}/${eventId}/rows/${key}`,updatePayload))
-    .then(r => cloud.put(`${test.api}/${eventId}/rows/${key}/column/${columnName}/increment`,incrementPayload));
+    let eventId = '70F95B23-E336-4B82-966F-C42B5AC44ABB';
+    let columnName = 'Number';
+    let key;
+    return cloud.post(test.api, DataEventPayload)
+      .then(r => {
+        let body = r.body[0].keys;
+        for (var keys in body) {
+          key = keys + ":" + body[keys];
+        }
+      })
+      .then(r => cloud.put(`${test.api}/${eventId}/rows/${key}`, updatePayload))
+      .then(r => cloud.put(`${test.api}/${eventId}/rows/${key}/column/${columnName}/increment`, incrementPayload));
 
- });
+  });
 });
