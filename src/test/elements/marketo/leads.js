e7cea0e3c55273f5c230b6e09161e6dbad7c862a
sarahhealey <sarahdhealey@gmail.com>
2017-10-23 15:55:33 -0600

    updated bulk to pass removed createleads test

--- src/test/elements/marketo/leads.js
+++ src/test/elements/marketo/leads.js
@@ -1,30 +1,14 @@
 'use strict';
 
 const suite = require('core/suite');
-const leadsPayload = require('./assets/leads');
-const tools = require('core/tools');
 const cloud = require('core/cloud');
 
-const payload = {
-  "name": "Name for Program Test" + tools.random(),
-  "channel": "Email Send",
-  "type": "Email",
-  "folder": {
-    "type":"Folder",
-    "id":33
-  }
-};
-
-suite.forElement('marketing', 'programs', { payload: payload }, (test) => {
-  it('should allow C for programs/{programId}/leads', () => {
+suite.forElement('marketing', 'programs', (test) => {
+  it('should allow R for programs/{id}/leads', () => {
     let program
-    // let programId;
-    // let programName;
-    leadsPayload.email = "sampleTestName" + tools.random() + "@test.com";
-    return cloud.post(test.api, payload)
+    return cloud.get(test.api)
       .then(r => program = r.body[0])
-      .then(r => cloud.post(`${test.api}/${program.name}/sync`, leadsPayload))
-      .then(r => cloud.delete(`${test.api}/${program.id}`));
+      .then(r => cloud.get(`${test.api}/${program.id}`));
   });
 });
 
