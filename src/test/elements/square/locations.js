066a1e683384a3cf75fd73d69197663d72ff297e
ChinnaBabuSadam <chinnapave@gmail.com>
2017-10-23 11:34:42 -0500

    Update locations.js

--- src/test/elements/square/locations.js
+++ src/test/elements/square/locations.js
@@ -7,6 +7,6 @@ suite.forElement('employee', 'locations', (test) => {
   test.should.supportPagination();
   test.withValidation(r => {
         expect(r).to.have.statusCode(200);
-        expect(r.body.filter(obj => obj.id != null)).to.not.be.empty;
+        expect(r.body.filter(obj => obj.id !== null)).to.not.be.empty;
   }).should.return200OnGet();
 });
