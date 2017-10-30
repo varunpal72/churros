e473a59945a04678813025801d16f7bead14a69f
tyler-cloud-elements <tyler@cloud-elements.com>
2017-10-30 18:46:12 -0500

    updated to suite tests

--- src/test/elements/salesforcemarketingcloud/attribute-sets.js
+++ src/test/elements/salesforcemarketingcloud/attribute-sets.js
@@ -1,10 +1,7 @@
 'use strict';
-const cloud = require('core/cloud');
 const suite = require('core/suite');
 
-
 suite.forElement('marketing', 'attribute-sets', (test) => {
-  it('should allow R for /attribute-sets', () => {
-    return cloud.get(`${test.api}`);
-  });
+    test.should.return200OnGet();
+    test.should.supportPagination();
 });
