424a83fb0a6f8cdfb809e861b86b2d9eab4b5187
Jamie Thorson <jamie@cloud-elements.com>
2017-10-30 15:12:52 -0600

    attribute-sets semicolon

--- src/test/elements/salesforcemarketingcloud/attribute-sets.js
+++ src/test/elements/salesforcemarketingcloud/attribute-sets.js
@@ -5,6 +5,6 @@ const suite = require('core/suite');
 
 suite.forElement('marketing', 'attribute-sets', (test) => {
   it('should allow R for /attribute-sets', () => {
-    return cloud.get(`${test.api}`)
+    return cloud.get(`${test.api}`);
   });
 });
