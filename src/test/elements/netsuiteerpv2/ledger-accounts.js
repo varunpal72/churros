c8dd781a351c241dd0c821c247a88241e5e034a5
MitilaGarud-GSLab <mithila.garud@gslab.com>
2017-11-04 10:29:43 +0530

    Added test cases for Netsuite ERP

--- src/test/elements/netsuiteerpv2/ledger-accounts.js
+++ src/test/elements/netsuiteerpv2/ledger-accounts.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 const payload = require('./assets/ledger-accounts');
 
-suite.forElement('erp', 'ledger-accounts', null, (test) => {
+suite.forElement('erp', 'ledger-accounts',{ payload: payload }, (test) => {
   test.should.supportCruds();
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
   test.withOptions({ qs: { where: 'isInactive = \'false\'' } }).should.return200OnGet();
