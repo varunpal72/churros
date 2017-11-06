cede0c02bce7df0aeb0deeddffa9836527eecbd7
seshuede <seshuede@gmail.com>
2017-11-06 10:19:12 -0600

    clear circle-ci error

--- src/test/elements/netsuitefinancev2/objects-tax-codes.js
+++ src/test/elements/netsuitefinancev2/objects-tax-codes.js
@@ -6,7 +6,7 @@ const cloud = require('core/cloud');
 suite.forElement('finance', 'objects', (test) => {
   let objectName = "customer";
   it('should support GET /objects-tax-codes', () => {
-    return  cloud.get(`${test.api}/${objectName}/tax-codes`)
+    return  cloud.get(`${test.api}/${objectName}/tax-codes`);
   });
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
 });
