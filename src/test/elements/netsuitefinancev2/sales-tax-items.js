d7b8572cf63a3f5bfe2382a4090d8460a1fc9b7e
seshuede <seshuede@gmail.com>
2017-11-08 19:27:22 -0600

    circleci error resolve

--- src/test/elements/netsuitefinancev2/sales-tax-items.js
+++ src/test/elements/netsuitefinancev2/sales-tax-items.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
 
 suite.forElement('finance', 'sales-tax-items', (test) => {
   test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
