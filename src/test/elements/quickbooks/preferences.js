781ac25703c8b73794f8356a19056bc2e56471ed
root <hanumant.shinde@gslab.com>
2017-11-09 14:32:14 +0530

    Incorporated review comments

--- src/test/elements/quickbooks/preferences.js
+++ src/test/elements/quickbooks/preferences.js
@@ -7,8 +7,18 @@ suite.forElement('finance', 'preferences', (test) => {
 
   test.should.supportPagination();
   it('should allow Patch for preferences', () => {
+    let id,syncToken; 
     return cloud.get(test.api)
-      .then(r => cloud.patch(`${test.api}/${r.body[0].Id}`, { "SyncToken": r.body[0].SyncToken, "sparse":false })); 
+      .then(r =>{
+	   if(r.body && r.body.length>0){
+            id=r.body[0].Id;
+	    syncToken=r.body[0].SyncToken;
+	   }
+      })
+      .then(r => {
+	   if(id && syncToken)
+	   return  cloud.patch(`${test.api}/${id}`, { "SyncToken": syncToken, "sparse":false }); 
+     });
   });
 });
 
