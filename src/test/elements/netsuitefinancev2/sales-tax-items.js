57177a3e4b01c82a89ae84b2d8bffc2902de9817
seshuede <seshuede@gmail.com>
2017-11-08 18:39:18 -0600

    works on PR comments

--- src/test/elements/netsuitefinancev2/sales-tax-items.js
+++ src/test/elements/netsuitefinancev2/sales-tax-items.js
@@ -5,7 +5,6 @@ const cloud = require('core/cloud');
 
 suite.forElement('finance', 'sales-tax-items', (test) => {
   it('should support GET /sales-tax-items', () => {
-    return  cloud.get(test.api);
+    cloud.withOptions({ qs: { page: 1, pageSize: 5 } }).get(test.api);
   });
-  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
 });
