42502bda390978f02b25b18fdd7aa5aeb4cd6a46
seshuede <seshuede@gmail.com>
2017-11-08 19:23:56 -0600

    updated comments

--- src/test/elements/netsuitefinancev2/sales-tax-items.js
+++ src/test/elements/netsuitefinancev2/sales-tax-items.js
@@ -4,7 +4,5 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 
 suite.forElement('finance', 'sales-tax-items', (test) => {
-  it('should support GET /sales-tax-items', () => {
-    cloud.withOptions({ qs: { page: 1, pageSize: 5 } }).get(test.api);
-  });
+  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
 });
