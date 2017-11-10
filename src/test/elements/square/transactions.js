ba755ae0e13941a595faca59c59b8e6257926a6c
seshuede <seshuede@gmail.com>
2017-11-10 16:29:07 -0600

    resolve circleci errors

--- src/test/elements/square/transactions.js
+++ src/test/elements/square/transactions.js
@@ -15,12 +15,12 @@ suite.forElement('employee', 'locations', (test) => {
     return cloud.withOptions({ qs: { where: "begin_time='2017-10-03T18:18:45Z'" } }).get(`${test.api}/${locId}/transactions`)
     .then(r => transactionPayload.idempotency_key = faker.random.uuid())
     .then(r => cloud.post(`${test.api}/${locId}/transactions`,transactionPayload))
-    .then(r => {transactionId = r.body.transaction.id})
+    .then(r => transactionId = r.body.transaction.id)
     .then(r => cloud.get(`${test.api}/${locId}/transactions/${transactionId}`))
     .then(r => cloud.delete(`${test.api}/${locId}/transactions/${transactionId}`))
     .then(r => transactionPayload.idempotency_key = faker.random.uuid())
     .then(r => cloud.post(`${test.api}/${locId}/transactions`,transactionPayload))
     .then(r => transactionId = r.body.transaction.id)
-    .then(r => cloud.post(`${test.api}/${locId}/transactions/${transactionId}/captures`,capturePayload))
+    .then(r => cloud.post(`${test.api}/${locId}/transactions/${transactionId}/captures`,capturePayload));
   });
 });
