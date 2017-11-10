b9462ca7cd05ead32c862254ee40c9a055c388c5
seshuede <seshuede@gmail.com>
2017-11-10 14:36:33 -0600

    PR modifications

--- src/test/elements/netsuitefinancev2/sales-tax-items.js
+++ src/test/elements/netsuitefinancev2/sales-tax-items.js
@@ -3,5 +3,6 @@
 const suite = require('core/suite');
 
 suite.forElement('finance', 'sales-tax-items', (test) => {
-  test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.return200OnGet();
+    test.withOptions({ qs: { page: 1, pageSize: 5 } }).should.supportPagination();
+    test.withOptions({ qs: { where: 'lastModifiedDate >= \'2016-08-05T09:35:38Z\'' } }).should.return200OnGet();
 });
