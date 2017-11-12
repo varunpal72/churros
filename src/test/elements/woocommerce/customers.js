c1da50008d85c9c16fcf5b193430ae61a3ccc0cc
Taylor Clark <taylor@cloud-elements.com>
2017-11-12 15:25:12 -0600

    replaced accidentally removed files

---
+++ src/test/elements/woocommerce/customers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+// const payload = require('./assets/customers');
+
+const customer = (custom) => ({
+    first_name: custom.firstName || 'Bill',
+    last_name: custom.lastName || 'Murray',
+    email: custom.email || tools.randomEmail(),
+    password: tools.random()
+});
+
+suite.forElement('ecommerce', 'customers', { payload: customer({}), skip: true }, (test) => {
+    test.should.supportCruds();
+    test.should.supportPagination();
+});
