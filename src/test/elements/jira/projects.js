0a1f67ff91a66041fa04095d77eab25e6c4336ef
root <hanumant.shinde@gslab.com>
2017-11-13 19:00:50 +0530

    Incorporated review comments

--- src/test/elements/jira/projects.js
+++ src/test/elements/jira/projects.js
@@ -7,5 +7,5 @@ const payload = tools.requirePayload(`${__dirname}/assets/projects.json`);
 suite.forElement('helpdesk', 'projects', { payload: payload }, (test) => {
   test.should.supportCruds();
   test.should.supportPagination();
-  test.withOptions({ qs: { where :`expand='lead'` }});
+  test.withOptions({ qs: { where :`expand='lead'` }}).should.return200OnGet();
  });
