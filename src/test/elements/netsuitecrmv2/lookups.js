8b0c9ecc0beb9745d972be00394e3bc64da8f7b3
AustinMahan <amahan0424@gmail.com>
2017-11-15 15:25:22 -0700

    one call

--- src/test/elements/netsuitecrmv2/lookups.js
+++ src/test/elements/netsuitecrmv2/lookups.js
@@ -6,7 +6,6 @@ const cloud = require('core/cloud');
 suite.forElement('crm', 'lookups', null, (test) => {
   it('should allow GET for /hubs/crm/lookups/:fieldName ', () => {
     let fieldName = 'currency';
-    return cloud.get(`${test.api}/${fieldName}`, (r) => console.log('res', r))
-      // .then(r => cloud.withOptions({ qs: { page: 1, pageSize: 5 } }).get(`${test.api}/${fieldName}`));
+    return cloud.withOptions({ qs: { pageSize: 5 } }).get(`${test.api}/${fieldName}`);
   });
 });
