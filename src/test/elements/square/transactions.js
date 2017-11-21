d43d8e7deb36936f119072213f08665856bf2d4f
seshuede <seshuede@gmail.com>
2017-11-21 15:45:17 -0600

    change label RDS as CRDS

--- src/test/elements/square/transactions.js
+++ src/test/elements/square/transactions.js
@@ -10,7 +10,7 @@ const transactionPayload = require('./assets/transactions');
 
 suite.forElement('employee', 'locations', (test) => {
   test.should.supportPagination();
-  it(`should allow RDS for /locations/:id/transactions`, () => {
+  it(`should allow CRDS for /locations/:id/transactions`, () => {
     let transactionId, locId = slocPayload.lid;
     return cloud.withOptions({ qs: { where: "begin_time='2017-10-03T18:18:45Z'" } }).get(`${test.api}/${locId}/transactions`)
     .then(transactionPayload.idempotency_key = faker.random.uuid())
