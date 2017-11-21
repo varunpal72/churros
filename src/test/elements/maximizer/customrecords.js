e25dc7df6664701c0e53d4a7526ab737774ad9e0
seshuede <seshuede@gmail.com>
2017-11-21 17:41:18 -0600

    added conditions for test casess

--- src/test/elements/maximizer/customrecords.js
+++ src/test/elements/maximizer/customrecords.js
@@ -3,18 +3,30 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const customrecordPayload = require('./assets/customrecords.json');
+const expect = require('chakram').expect;
 
 suite.forElement('crm', 'custom-records', (test) => {
   test.should.supportPagination('id');
   it('should allow CRUDS for /custom-records', () => {
     let customrecordId;
     return cloud.get(test.api)
-      .then(r => customrecordPayload.ApplicationId = r.body[0].ApplicationId)
+      .then(r => {
+        customrecordPayload.ApplicationId = r.body[0].ApplicationId;
+      })
       .then(r => cloud.post(test.api, customrecordPayload))
-      .then(r => customrecordId = r.body.Key)
-      .then(r => cloud.get(test.api))
+      .then(r => {
+        customrecordId = r.body.Key;
+        expect(r.body.Name === customrecordPayload.Name).to.not.be.empty;
+      })
+      .then(r => cloud.withOptions({ qs: { where: "Name='CustomRecordName'" } }).get(test.api))
+      .then(r => {
+        expect(r.body[0].Name === customrecordPayload.Name).to.not.be.empty;
+      })
       .then(r => cloud.get(`${test.api}/${customrecordId}`))
       .then(r => cloud.patch(`${test.api}/${customrecordId}`, customrecordPayload))
+      .then(r => {
+        expect(r.body.Name === customrecordPayload.Name).to.not.be.empty;
+      })
       .then(r => cloud.delete(`${test.api}/${customrecordId}`));
   });
 });
