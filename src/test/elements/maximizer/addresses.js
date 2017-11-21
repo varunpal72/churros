00bba02c149a1315bbaaec2a052d47f16001a89a
seshuede <seshuede@gmail.com>
2017-11-21 17:27:20 -0600

    add conditions for all test cases

--- src/test/elements/maximizer/addresses.js
+++ src/test/elements/maximizer/addresses.js
@@ -3,17 +3,29 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const addressPayload = require('./assets/addresses');
+const expect = require('chakram').expect;
+const addressbookentryPayload = require('./assets/addressbookentry');
 
 suite.forElement('crm', 'addresses', (test) => {
   test.should.supportPagination();
   it('should allow CRUD for /addresses', () => {
     let addressId;
-    return cloud.get(`/hubs/crm/addressbook-entries`)
-      .then(r => addressPayload.ParentKey = r.body[0].id)
+    return cloud.post(`/hubs/crm/addressbook-entries`,addressbookentryPayload)
+      .then(r => addressPayload.ParentKey = r.body.id)
       .then(r => cloud.post(test.api, addressPayload))
-      .then(r => addressId = r.body.Key)
-      .then(r => cloud.get(test.api))
+      .then(r => {
+        addressId = r.body.Key;
+        expect(r.body.City === addressPayload.City).to.not.be.empty;
+      })
+      .then(r => cloud.withOptions({ qs: { where: "CompanyName='CompanyName ABC'" } }).get(test.api))
+      .then(r => {
+        expect(r.body[0].Address.Default === true).to.not.be.empty;
+      })
       .then(r => cloud.patch(`${test.api}/${addressId}`, addressPayload))
-      .then(r => cloud.delete(`${test.api}/${addressId}`));
+      .then(r => {
+        expect(r.body.Key === addressId).to.not.be.empty;
+      })
+      .then(r => cloud.delete(`${test.api}/${addressId}`))
+      .then(r => cloud.delete(`/hubs/crm/addressbook-entries/${addressPayload.ParentKey}`));
   });
 });
--- src/test/elements/maximizer/opportunities.js
+++ src/test/elements/maximizer/opportunities.js
@@ -3,18 +3,30 @@
 const suite = require('core/suite');
 const cloud = require('core/cloud');
 const opportunityPayload = require('./assets/opportunities');
+const addressbookentryPayload = require('./assets/addressbookentry');
+const expect = require('chakram').expect;
 
 suite.forElement('crm', 'opportunities', (test) => {
   test.should.supportPagination('id');
   it('should allow CRUDS for /contacts', () => {
     let opportunityId;
-    return cloud.get(`/hubs/crm/addressbook-entries`)
-      .then(r => opportunityPayload.AbEntryKey = r.body[0].id)
+    return cloud.post(`/hubs/crm/addressbook-entries`,addressbookentryPayload)
+      .then(r => opportunityPayload.AbEntryKey = r.body.id)
       .then(r => cloud.post(test.api, opportunityPayload))
-      .then(r => opportunityId = r.body.Key)
-      .then(r => cloud.get(test.api))
+      .then(r => {
+        opportunityId = r.body.Key;
+        expect(r.body.Description === opportunityPayload.Description).to.not.be.empty;
+      })
+      .then(r => cloud.withOptions({ qs: { where: "Description='opportunity1'" } }).get(test.api))
+      .then(r => {
+        expect(r.body[0].Description === opportunityPayload.Description).to.not.be.empty;
+      })
       .then(r => cloud.get(`${test.api}/${opportunityId}`))
       .then(r => cloud.patch(`${test.api}/${opportunityId}`, opportunityPayload))
-      .then(r => cloud.delete(`${test.api}/${opportunityId}`));
+      .then(r => {
+        expect(r.body.Description === opportunityPayload.Description).to.not.be.empty;
+      })
+      .then(r => cloud.delete(`${test.api}/${opportunityId}`))
+      .then(r => cloud.delete(`/hubs/crm/addressbook-entries/${opportunityPayload.AbEntryKey}`));
   });
 });
