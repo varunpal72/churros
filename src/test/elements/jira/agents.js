b8488e940d3e43acb5abbec3c5824d12a99d67e9
root <jagruti.pardeshi@gslab.com>
2017-11-22 16:46:10 +0530

    fixing churros for Jira elements

--- src/test/elements/jira/agents.js
+++ src/test/elements/jira/agents.js
@@ -6,9 +6,10 @@ const cloud = require('core/cloud');
 suite.forElement('helpdesk', 'agents', (test) => {
   let agentId;
   it('should allow get for /agents', () => {
-    return cloud.withOptions({qs:{where:`username='test'`}}).get('/hubs/helpdesk/agents')
-    .then(r => cloud.withOptions({qs:{where:`username='test'`, page: 1, pageSize: 1 }}).get('/hubs/helpdesk/agents'))
-    .then(r => agentId = r.body[0].key)
+//"jiradev" is hardcoded as get all api has "username" constraint".
+    return cloud.withOptions({qs:{where:`username='jiradev'`}}).get('/hubs/helpdesk/agents')
+    .then(r => cloud.withOptions({qs:{where:`username='jiradev'`, page: 1, pageSize: 1 }}).get('/hubs/helpdesk/agents'))
+   .then(r => agentId = r.body[0].key)
     .then(r => cloud.get("/hubs/helpdesk/agents/" + agentId));
   });
 });
