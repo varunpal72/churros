e0832a61db79bdc198e10c486e198f48aee21937
Sonali Soni <sonali.soni@gslab.com>
2017-11-23 15:09:12 +0530

    Fixec CircleCi

--- src/test/elements/bullhorn/placements.js
+++ src/test/elements/bullhorn/placements.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const suite = require('core/suite');
-const cloud = require('core/cloud');
 const tools = require('core/tools');
 const payload = tools.requirePayload(`${__dirname}/assets/placments.json`);
 const updatePayload = {};
