eece66b9240be569733e4fb7989b58068df0969d
MitilaGarud-GSLab <mithila.garud@gslab.com>
2017-11-24 16:46:23 +0530

    Bills-payment test cases for Intacct

--- src/test/elements/intacct/bills-payments.js
+++ src/test/elements/intacct/bills-payments.js
@@ -2,10 +2,9 @@
 
 const suite = require('core/suite');
 const tools = require('core/tools');
-const cloud = require('core/cloud');
 const payload = tools.requirePayload(`${__dirname}/assets/billsPayments.json`);
 suite.forElement('finance', 'bills-payments', { payload: payload }, (test) => {
-  test.should.supportCruds();
+  test.should.supportSr();
   test.should.supportPagination();
   test.withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } }).should.return200OnGet();
 });
