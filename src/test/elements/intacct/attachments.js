e959b6814a4c1f797d4381e3f2613b7284e53d42
Gauri Bhutkar <gauri.bhutkar@gslab.com>
2017-11-28 16:33:46 +0530

    Added churros for attachment api's

---
+++ src/test/elements/intacct/attachments.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const suite = require('core/suite');
+const tools = require('core/tools');
+const cloud = require('core/cloud');
+const payload = require('./assets/attachment');
+const updatePayload = {
+    "discription":"testChurrosUpdated"
+};
+
+suite.forElement('finance', 'attachments', { payload: payload }, (test) => {
+  it(`should allow CRUDS for ${test.api}`, () => {
+    let docid;
+    return cloud.get(test.api)
+      .then(r => docid = r.body[1].id)
+      .then(r => cloud.post(`${test.api}`, payload))
+      .then(r => cloud.get(`${test.api}/${docid}`))
+      .then(r => cloud.patch(`${test.api}/${docid}`, payload))
+      .then(r => cloud.delete(`${test.api}/${docid}`))
+  });
+  test.should.supportPagination();
+  test.withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'creationdate>\'11/21/2017\'' } }).should.return200OnGet();
+});
