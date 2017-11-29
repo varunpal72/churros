b7c0a5a41deb254014aec20e6d26a26b824e418c
Gauri Bhutkar <gauri.bhutkar@gslab.com>
2017-11-29 17:33:14 +0530

    fixed build failure

--- src/test/elements/intacct/attachments.js
+++ src/test/elements/intacct/attachments.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const suite = require('core/suite');
-const tools = require('core/tools');
 const cloud = require('core/cloud');
 const payload = require('./assets/attachment');
-const updatePayload = {
-    "discription":"testChurrosUpdated"
-};
 
 suite.forElement('finance', 'attachments', { payload: payload }, (test) => {
   it(`should allow CRUDS for ${test.api}`, () => {
@@ -16,7 +12,7 @@ suite.forElement('finance', 'attachments', { payload: payload }, (test) => {
       .then(r => cloud.post(`${test.api}`, payload))
       .then(r => cloud.get(`${test.api}/${docid}`))
       .then(r => cloud.patch(`${test.api}/${docid}`, payload))
-      .then(r => cloud.delete(`${test.api}/${docid}`))
+      .then(r => cloud.delete(`${test.api}/${docid}`));
   });
   test.should.supportPagination();
   test.withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'creationdate>\'11/21/2017\'' } }).should.return200OnGet();
