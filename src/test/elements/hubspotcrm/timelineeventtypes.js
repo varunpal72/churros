7e02178163a5232f42c82b6b4285665801dd9fbc
seshuede <seshuede@gmail.com>
2017-11-29 00:45:06 -0600

    hubspot churros for timelineeventtypes

--- src/test/elements/hubspotcrm/timelineeventtypes.js
+++ src/test/elements/hubspotcrm/timelineeventtypes.js
@@ -2,17 +2,80 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const payload = require('./assets/timelineeventtypes');
+const tools = require('core/tools');
+const expect = require('chakram').expect;
+const faker = require('faker');
+const payload = tools.requirePayload(`${__dirname}/assets/timelineeventtypes.json`);
+const propertiesPayload = tools.requirePayload(`${__dirname}/assets/timelineeventproperties.json`);
+const eventsPayload = tools.requirePayload(`${__dirname}/assets/timelineevents.json`);
 
-suite.forElement('crm', 'timeline-event-types', {skip: true}, (test) => {
+suite.forElement('crm', 'timeline-event-types', {payload: payload}, (test) => {
 
-  it('should test CUDS for /timeline-event-types', () => {
-    let id;
-    return cloud.get(test.api)
-      .then(r => cloud.post(test.api, payload))
-      .then(r => id = r.body.id)
-      .then(r => cloud.patch(`${test.api}/${id}`, payload))
-      .then(r => cloud.delete(`${test.api}/${id}`));
+  it('should test CRUD for /timeline-event-types', () => {
+    test.should.supportPagination("id");
+    test.should.supportCrud();
   });
+
+  const eventTypesWrap = (cb) => {
+     let eventTypes;
+     return cloud.post(`${test.api}`, payload)
+       .then(r => eventTypes = r.body)
+       .then(r => cb(eventTypes))
+       .then(r => cloud.delete(`${test.api}/${eventTypes.id}`));
+   }
+
+   it('it should allow CUD for /timeline-event-types/:id/properties', () => {
+     const cb = (eventTypes) => {
+       let eventProperties;
+       return cloud.post(`${test.api}/${eventTypes.id}/properties`,propertiesPayload)
+         .then(r => {
+             expect(r).to.have.statusCode(200) &&
+             expect(r.body).to.not.be.null &&
+             expect(r.body).to.be.an('object') &&
+             expect(Object.keys(r.body)).to.have.length.above(0) &&
+             expect(r.body).to.contain.key('name');
+             eventProperties = r.body;
+             eventProperties.label = faker.random.word();
+         })
+         cloud.patch(`${test.api}/${eventTypes.id}/properties`,eventProperties)
+           .then(r => {
+               expect(r).to.have.statusCode(200) &&
+               expect(r.body).to.not.be.null &&
+               expect(r.body).to.be.an('object') &&
+               expect(Object.keys(r.body)).to.have.length.above(0) &&
+               expect(r.body).to.contain.key('label');
+           })
+         .then(r => cloud.delete(`${test.api}/${eventTypes.id}/properties/${eventProperties.id}`));
+     };
+     return eventTypesWrap(cb);
+   });
+
+   it('it should allow CRU for /timeline-event-types/:id/events', () => {
+     const cb = (eventTypes) => {
+       let events;
+       return cloud.post(`${test.api}/${eventTypes.id}/events`,eventsPayload)
+         .then(r => {
+                     expect(r).to.have.statusCode(200) &&
+                     expect(r.body).to.be.empty;
+                  })
+         .then(r => cloud.get(`${test.api}/${eventTypes.id}/events/${eventsPayload.id}`))
+         .then(r => {
+                    expect(r).to.have.statusCode(200) &&
+                    expect(r.body).to.not.be.null &&
+                    expect(r.body).to.be.an('object') &&
+                    expect(Object.keys(r.body)).to.have.length.above(0) &&
+                    expect(r.body).to.contain.key('email');
+                    expect(r.body).to.have.property('id', `${eventsPayload.id}`);
+                    events = r.body;
+                    events.email = faker.internet.email();
+                  })
+        .then(r => cloud.patch(`${test.api}/${eventTypes.id}/events/${eventsPayload.id}`,events))
+        .then(r => {
+                    expect(r).to.have.statusCode(200) &&
+                    expect(r.body).to.be.empty;
+                 })
+     };
+     return eventTypesWrap(cb);
+   });
 
 });
