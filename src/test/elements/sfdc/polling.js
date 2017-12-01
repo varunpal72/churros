f4cbef857a90febd7b98287bb3b7834e39c61ec6
Rick Rothenberg <rick@cloud-elements.com>
2017-12-01 16:30:15 -0700

    Test for SFDC delete unused event poller records

--- src/test/elements/sfdc/polling.js
+++ src/test/elements/sfdc/polling.js
@@ -1,8 +1,10 @@
 'use strict';
 
 //dependencies at the top
+const expect = require('chakram').expect;
 const suite = require('core/suite');
 const tools = require('core/tools');
+const cloud = require('core/cloud');
 //how to import payloads
 const accountsPayload = tools.requirePayload(`${__dirname}/assets/accounts.json`);
 const contactsPayload = tools.requirePayload(`${__dirname}/assets/contacts.json`);
@@ -14,4 +16,20 @@ suite.forElement('crm', 'polling', (test) => {
   test.withApi('/hubs/crm/contacts').should.supportPolling(contactsPayload, 'Contact');
   test.withApi('/hubs/crm/leads').should.supportPolling(leadsPayload, 'Lead');
   test.withApi('/hubs/crm/opportunities').should.supportPolling(opportunitiesPayload, 'Opportunity');
+
+  it('should delete event poller records for unused objects', () => {
+    return cloud.patch('/instances',
+      {
+        configuration:
+          {
+            'event.poller.refresh_interval':'1',
+            'event.objects':'Contact,Account',
+            'event.vendor.type':'polling',
+            'event.notification.enabled':'true'
+          }
+      })
+      .then(() => tools.wait.upTo(90000).for(() => cloud.get('/instances/event-poller-records', r => expect(r.body.length).to.equal(2))))
+      .then(() => cloud.patch('/instances',{configuration:{'event.objects':'Contact'}}))
+      .then(() => cloud.get('/instances/event-poller-records', r => expect(r.body.length).to.equal(1)));
+  });
 });
