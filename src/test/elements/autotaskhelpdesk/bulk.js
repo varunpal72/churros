29e87ca0b75b4f10a3f0e3088b51c8183c313eba
AustinMahan <amahan0424@gmail.com>
2017-12-04 13:55:48 -0700

    linter

--- src/test/elements/autotaskhelpdesk/bulk.js
+++ src/test/elements/autotaskhelpdesk/bulk.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const suite = require('core/suite');
-const cloud = require('core/cloud');
-const tools = require('core/tools');
-const expect = require('chakram').expect;
 
 suite.forElement('helpdesk', 'bulk', null, (test) => {
   const opts = {csv: true, json: true};
