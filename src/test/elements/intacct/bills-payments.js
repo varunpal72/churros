d30ea85ce58f18c9e48444067aa869c6896fbaea
MitilaGarud-GSLab <mithila.garud@gslab.com>
2017-12-06 19:01:27 +0530

    Incorporated review comments

--- src/test/elements/intacct/bills-payments.js
+++ src/test/elements/intacct/bills-payments.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const suite = require('core/suite');
-const tools = require('core/tools');
-const payload = tools.requirePayload(`${__dirname}/assets/billsPayments.json`);
-suite.forElement('finance', 'bills-payments', { payload: payload }, (test) => {
-  test.should.supportSr();
-  test.should.supportPagination();
-  test.withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } }).should.return200OnGet();
+const cloud = require('core/cloud');
+const expect = require('chakram').expect;
+
+
+suite.forElement('finance', 'bills-payments', null, (test) => {
+  it(`should allow RS for ${test.api}`, () => {
+    let recordNo;
+    return cloud.get(test.api)
+    .then(r => recordNo = r.body[0].RECORDNO)
+    .then(cloud.get(test.api + "/${recordNo}"))
+    .then(cloud.withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } })
+    .get(test.api)
+    .then((r) => {
+    expect(r).to.have.statusCode(200);
+    const validValues = r.body.filter(obj =>
+      obj.whenmodified>='08/13/2016 05:26:37');
+    expect(validValues.length).to.equal(r.body.length);
+  }));
+});
+  //test.should.supportSr();
+  //test.should.supportPagination();
+
 });
