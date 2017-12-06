0120e82775b089b9a11a6c02429ecdd5165a9636
MitilaGarud-GSLab <mithila.garud@gslab.com>
2017-12-06 19:02:25 +0530

    Incorporated review comments

--- src/test/elements/intacct/bills-payments.js
+++ src/test/elements/intacct/bills-payments.js
@@ -9,18 +9,15 @@ suite.forElement('finance', 'bills-payments', null, (test) => {
   it(`should allow RS for ${test.api}`, () => {
     let recordNo;
     return cloud.get(test.api)
-    .then(r => recordNo = r.body[0].RECORDNO)
-    .then(cloud.get(test.api + "/${recordNo}"))
-    .then(cloud.withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } })
-    .get(test.api)
-    .then((r) => {
-    expect(r).to.have.statusCode(200);
-    const validValues = r.body.filter(obj =>
-      obj.whenmodified>='08/13/2016 05:26:37');
-    expect(validValues.length).to.equal(r.body.length);
-  }));
-});
-  //test.should.supportSr();
-  //test.should.supportPagination();
-
+      .then(r => recordNo = r.body[0].RECORDNO)
+      .then(cloud.get(test.api + "/${recordNo}"))
+      .then(cloud.withOptions({ qs: { where: 'whenmodified>\'08/13/2016 05:26:37\'' } })
+        .get(test.api)
+        .then((r) => {
+          expect(r).to.have.statusCode(200);
+          const validValues = r.body.filter(obj =>
+            obj.whenmodified >= '08/13/2016 05:26:37');
+          expect(validValues.length).to.equal(r.body.length);
+        }));
+  });
 });
