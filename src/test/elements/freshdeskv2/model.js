e42cae693d096ff76235efed271d67ad42e0d32f
NeetaBirajdar-GSLab <you@example.com>
2017-12-06 17:43:19 +0530

    Added few test cases

--- src/test/elements/freshdeskv2/model.js
+++ src/test/elements/freshdeskv2/model.js
@@ -6,17 +6,34 @@ const cloud = require('core/cloud');
 const model = require('core/model');
 
 suite.forElement('helpdesk', 'model', (test) => {
+  let body;
   it.skip('should allow model vallidation for accounts', () => {
      return cloud.get("/hubs/helpdesk/accounts")
      .then(r => model.validateResponseModel(r, 'getAccounts'))
-   });
-
-   it('should allow model vallidation for incidents', () => {
-      return cloud.get("/hubs/helpdesk/incidents")
-      .then(r => model.validateResponseModel(r, 'getIncidents'))
+     .then(r => id = r.body[0].id )
+     .then(r =>cloud.get(`/hubs/helpdesk/accounts/${id}`))
+     .then(r => model.validateResponseModel(r, 'getIncidentByAccountId'));
     });
-    it('should allow model vallidation for contacts', () => {
+
+  //  it('should allow model vallidation for incidents', () => {
+  //     return cloud.withOptions({ qs: { pageSize: 1 } }).get(`/hubs/helpdesk/incidents/`)
+  //     .then(r => model.validateResponseModel(r, 'getIncidents'))
+  //     .then(r =>cloud.get(`/hubs/helpdesk/incidents/${r.body.id}`))
+  //     .then(r => model.validateResponseModel(r, 'getIncidentByIncidentsId'));
+  //   });
+
+    it('should allow model vallidation for incidents', () => {
+       return cloud.get(`/hubs/helpdesk/incidents`)
+       .then(r => model.validateResponseModel(r, 'getIncidents'))
+       .then(r =>cloud.get(`/hubs/helpdesk/incidents/${r.body[0].id}`))
+       .then(r => model.validateResponseModel(r, 'getIncidentById'));
+     });
+
+
+    it.skip('should allow model vallidation for contacts', () => {
        return cloud.get("/hubs/helpdesk/contacts")
        .then(r => model.validateResponseModel(r, 'getContacts'))
     });
+
+
 });
