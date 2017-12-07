8e3db8a30a5e21bb0495c548971aec0b1ead1e0a
seshuede <seshuede@gmail.com>
2017-12-06 23:16:46 -0600

    Make  use of <<random.uuid>>

--- src/test/elements/square/transactions.js
+++ src/test/elements/square/transactions.js
@@ -4,8 +4,9 @@ const suite = require('core/suite');
 const cloud = require('core/cloud');
 const faker = require('faker');
 const slocPayload = require('./assets/stransactionsPayload');
-const capturePayload = require('./assets/capturePayload');
-const transactionPayload = require('./assets/transactions');
+const tools = require('core/tools');
+const transactionPayload = tools.requirePayload(`${__dirname}/assets/transactions.json`);
+const transactionPayloadCapture = tools.requirePayload(`${__dirname}/assets/transactionsCapture.json`);
 
 
 suite.forElement('employee', 'locations', (test) => {
@@ -13,14 +14,12 @@ suite.forElement('employee', 'locations', (test) => {
   it(`should allow CRDS for /locations/:id/transactions`, () => {
     let transactionId, locId = slocPayload.lid;
     return cloud.withOptions({ qs: { where: "begin_time='2017-10-03T18:18:45Z'" } }).get(`${test.api}/${locId}/transactions`)
-    .then(transactionPayload.idempotency_key = faker.random.uuid())
-    .then(cloud.post(`${test.api}/${locId}/transactions`,transactionPayload))
+    .then(() => cloud.post(`${test.api}/${locId}/transactions`,transactionPayload))
     .then(r => transactionId = r.body.transaction.id)
-    .then(cloud.get(`${test.api}/${locId}/transactions/${transactionId}`))
-    .then(cloud.delete(`${test.api}/${locId}/transactions/${transactionId}`))
-    .then(transactionPayload.idempotency_key = faker.random.uuid())
-    .then(cloud.post(`${test.api}/${locId}/transactions`,transactionPayload))
+    .then(() => cloud.get(`${test.api}/${locId}/transactions/${transactionId}`))
+    .then(() => cloud.delete(`${test.api}/${locId}/transactions/${transactionId}`))
+    .then(() => cloud.post(`${test.api}/${locId}/transactions`,transactionPayloadCapture))
     .then(r => transactionId = r.body.transaction.id)
-    .then(cloud.post(`${test.api}/${locId}/transactions/${transactionId}/captures`,capturePayload));
+    .then(() => cloud.post(`${test.api}/${locId}/transactions/${transactionId}/captures`));
   });
 });
