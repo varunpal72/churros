30a64b4dd5fcefca6a5b5e6de133dd0dd2f2c786
rahul gupta <rahul.gupta@gslab.com>
2017-12-08 12:14:46 +0530

    Resolved conflicts

--- src/test/elements/jira/agents.js
+++ src/test/elements/jira/agents.js
@@ -2,31 +2,14 @@
 
 const suite = require('core/suite');
 const cloud = require('core/cloud');
-const model = require('core/model');
-
-// suite.forElement('helpdesk', 'agents', (test) => {
-//   let agentId;
-//   it('should allow get for /agents', () => {
-//     return cloud.withOptions({qs:{where:`username='test'`}}).get('/hubs/helpdesk/agents')
-//     .then(r => cloud.withOptions({qs:{where:`username='test'`, page: 1, pageSize: 1 }}).get('/hubs/helpdesk/agents'))
-//     .then(r => agentId = r.body[0].key)
-//     .then(r => cloud.get("/hubs/helpdesk/agents/" + agentId));
-//   });
-// });
 
 suite.forElement('helpdesk', 'agents', (test) => {
   let agentId;
   it('should allow get for /agents', () => {
-<<<<<<< HEAD
-    return cloud.withOptions({qs:{where:`username='jiradev'`}}).get('/hubs/helpdesk/agents')
-    .then(r => model.validateGetModel(r, '/agents'))
-    //return cloud.withOptions({qs:{where:`username='jiradev'`}}).validateGetModel('/hubs/helpdesk/agents', '/agents')    
-=======
 //"jiradev" is hardcoded as get all api has "username" constraint".
     return cloud.withOptions({qs:{where:`username='jiradev'`}}).get('/hubs/helpdesk/agents')
     .then(r => cloud.withOptions({qs:{where:`username='jiradev'`, page: 1, pageSize: 1 }}).get('/hubs/helpdesk/agents'))
    .then(r => agentId = r.body[0].key)
     .then(r => cloud.get("/hubs/helpdesk/agents/" + agentId));
->>>>>>> fd6b30305accb2879c4c0050f5731089c13922a6
   });
 });
