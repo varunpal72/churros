4561463283c92c9cb58f47231a54d215abe4efc9
Gauri Bhutkar <gauri.bhutkar@gslab.com>
2017-12-08 17:01:43 +0530

    Addressed review comments

--- src/test/elements/intacct/reporting-periods.js
+++ src/test/elements/intacct/reporting-periods.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const suite = require('core/suite');
+const schema = require('./assets/schema.json');
 
 suite.forElement('finance', 'reporting-periods', null , (test) => {
   test.should.return200OnGet();
   test.should.supportPagination();
-  test.withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'WHENCREATED>\'08/13/2016 05:26:37\'' } }).should.return200OnGet();
+  test.withValidation(schema).withName('should support updated > {date} Ceql search').withOptions({ qs: { where: 'RECORDNO=\'111\'' } }).should.return200OnGet();
 });
